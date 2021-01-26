7aae4cb7b49fb4a61b6c80f7fe3d9dd45c1ed8e2
new5558 <new17353@gmail.com>
2021-01-26 16:22:20 +0700

    feat/➕ add @storybook/addon-knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-knobs', '@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config) => {
-    config.resolve.alias["@"] = path.resolve("src")
+    config.resolve.alias['@'] = path.resolve('src')
     return config
-  }
+  },
 }

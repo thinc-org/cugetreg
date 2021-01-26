a16777792c0886607aa3a571c7b5f6cdf2b3145f
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-26 18:23:00 +0700

    :lipstick: change Flex to Grid for CourseList

--- src/components/ShoppingPanel/CourseList.tsx
+++ src/components/ShoppingPanel/CourseList.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core'
+import { Box, Grid } from '@material-ui/core'
 import { Delete } from '@material-ui/icons'
 import CourseLabel from '@/components/ShoppingPanel/CourseLabel'
 
@@ -14,20 +14,41 @@ interface PropsType {
 
 const CourseList = (props: PropsType) => {
   const { id, name, credit, color, category } = props.course
-  return (
-    <Box display="flex" alignItems="center" my={0.5} width={1}>
-      <Box display="flex" alignItems="center" justifyContent="space-between" width={3 / 4}>
-        <Box mr={0.5} color="gray">
-          <Delete />
-        </Box>
-        <Box mx={0.5}>{id}</Box>
-        <Box mx={0.5}>{name}</Box>
-        <Box mx={0.5}>{credit} Credits</Box>
-      </Box>
+  // return (
+  //   <Box display="flex" alignItems="center" my={0.5} width={1}>
+  //     <Box display="flex" alignItems="center" justifyContent="space-between" width={3 / 4}>
+  //       <Box mr={0.5} color="gray">
+  //         <Delete />
+  //       </Box>
+  //       <Box mx={0.5}>{id}</Box>
+  //       <Box mx={0.5}>{name}</Box>
+  //       <Box mx={0.5}>{credit} Credits</Box>
+  //     </Box>
 
-      <Box display="flex" justifyContent="center" width={1 / 4} ml={0.5}>
-        <CourseLabel color={color} category={category} />
-      </Box>
+  //     <Box display="flex" justifyContent="center" width={1 / 4} ml={0.5}>
+  //       <CourseLabel color={color} category={category} />
+  //     </Box>
+  //   </Box>
+  // )
+  return (
+    <Box my={1}>
+      <Grid container alignItems="center">
+        <Grid item xs={1} sm={1} color="gray">
+          <Delete />
+        </Grid>
+        <Grid item xs={5} sm={2}>
+          {id}
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          {name}
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          {credit} Credits
+        </Grid>
+        <Grid container xs={6} sm={2}>
+          <CourseLabel color={color} category={category} />
+        </Grid>
+      </Grid>
     </Box>
   )
 }

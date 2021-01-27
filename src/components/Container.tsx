16305861e9301d8fce078482a3d8d52478e70cd0
paphonb <paphonb@gmail.com>
2021-01-27 12:54:38 +0700

    ♻️ use Container from @material-ui/core

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,1 @@
-import { styled } from '@material-ui/core'
-
-export const Container = styled('div')(({ theme }) => ({
-  // TODO: add responsive size
-  width: '100%',
-  maxWidth: 1024,
-  paddingLeft: theme.spacing(1),
-  paddingRight: theme.spacing(1),
-  margin: '0 auto',
-}))
+export { Container } from '@material-ui/core'

34b86afd4fe6e4111f7caa9760bd076259ce6ba0
new5558 <new17353@gmail.com>
2021-01-28 12:33:14 +0700

    �  fix inner margin

--- src/components/AnnouncementCard/const.ts
+++ src/components/AnnouncementCard/const.ts
@@ -2,5 +2,5 @@ export const BOX_BORDER_RADIUS = '4px'
 export const BACKGROUND_COLOR = '#FFFFFF'
 export const IMAGE_SIZE = 196
 
-export const TAG_INNER_MARGIN = 3
+export const TAG_INNER_MARGIN = 2
 export const TAG_VERTICAL_MARGIN = 3

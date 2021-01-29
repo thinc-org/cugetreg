7be3f6b64e54c26436fcb50b1d48b146e6e7c150
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-29 11:45:16 +0700

    :recycle: remove React fragment

--- src/components/ShoppingPanel/CourseLabel.tsx
+++ src/components/ShoppingPanel/CourseLabel.tsx
@@ -6,22 +6,20 @@ interface PropsType {
 }
 const CourseLabel = ({ color, category }: PropsType) => {
   return (
-    <>
-      <Box
-        color={color}
-        border={1.5}
-        borderColor={color}
-        textAlign="center"
-        borderRadius={12}
-        px={1.5}
-        py={0.5}
-        fontWeight="fontWeightBold"
-        fontSize={12}
-        hidden={category === null}
-      >
-        {category !== null ? category : ''}
-      </Box>
-    </>
+    <Box
+      color={color}
+      border={1.5}
+      borderColor={color}
+      textAlign="center"
+      borderRadius={12}
+      px={1.5}
+      py={0.5}
+      fontWeight="fontWeightBold"
+      fontSize={12}
+      hidden={category === null}
+    >
+      {category !== null ? category : ''}
+    </Box>
   )
 }
 

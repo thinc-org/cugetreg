37e6bd963e5ecea94de81e1fdbc5b030e2380d00
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-29 15:26:47 +0700

    :art: implement useShoppingPanel custom hook

---
+++ src/hooks/useShoppingPanel.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useCallback } from 'react'
+
+interface Course {
+  id: number
+  name: string
+  credit: number
+  color: string
+  category: string | null
+}
+
+const useShoppingPanel = (data: Course[]) => {
+  const [courses, setCourses] = useState<Course[]>(data)
+
+  const credit: number = courses.reduce((acc: number, curr: Course) => acc + curr.credit, 0)
+
+  const deleteCourse = (id: number) => {
+    setCourses(courses.filter((item) => item.id !== id))
+  }
+
+  const makeSchedule = () => {
+    console.log(courses)
+  }
+
+  return { credit, courses, deleteCourse, makeSchedule }
+}
+
+export default useShoppingPanel

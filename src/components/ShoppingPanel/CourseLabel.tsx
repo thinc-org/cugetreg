48c4c66ceb37cf9f2c1c5ece071c488f74d2c97e
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-29 22:31:12 +0700

    Convert to useStyles

--- src/components/ShoppingPanel/CourseLabel.tsx
+++ src/components/ShoppingPanel/CourseLabel.tsx
@@ -1,26 +1,29 @@
-import { Box, Typography } from '@material-ui/core'
+import { Box, Typography, makeStyles, Theme } from '@material-ui/core'
 
 interface PropsType {
   color: string
   category: string | null
 }
-const CourseLabel = ({ color, category }: PropsType) => {
+
+const useStyles = makeStyles<Theme, PropsType>({
+  container: (props) => ({
+    borderRadius: 100,
+    padding: '2.5px 13px',
+    border: '1px solid',
+    color: props.color,
+    borderColor: props.color,
+    textAlign: 'center',
+    display: props.category === null ? 'none' : 'block',
+  }),
+})
+const CourseLabel = (props: PropsType) => {
+  const { category } = props
+  const classes = useStyles(props)
   return (
-    <Box
-      color={color}
-      border={1.5}
-      borderColor={color}
-      textAlign="center"
-      borderRadius={12}
-      px={1.5}
-      py={0.5}
-      hidden={category === null}
-    >
-      <Typography variant="caption">{category !== null ? category : ''}</Typography>
+    <Box className={classes.container}>
+      <Typography variant="overline">{category !== null ? category : ''}</Typography>
     </Box>
   )
 }
 
 export default CourseLabel
-
-//TODO: - Determine global border radius
--- src/components/ShoppingPanel/index.tsx
+++ src/components/ShoppingPanel/index.tsx
@@ -1,5 +1,5 @@
 import Course from './Course'
-import { Box, Button, Typography } from '@material-ui/core'
+import { Box, Button, Typography, makeStyles } from '@material-ui/core'
 import useShoppingPanel from '@/hooks/useShoppingPanel.ts'
 import TableChartIcon from '@material-ui/icons/TableChart'
 
@@ -9,7 +9,7 @@ const mockData = [
     name: 'URBAN ENVIRONMENT',
     credit: 2,
     color: 'green',
-    category: 'Social Studies',
+    category: 'Socials',
   },
   {
     id: 123445,
@@ -27,31 +27,62 @@ const mockData = [
   },
 ]
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: '50px 45px 30px 45px',
+    backgroundColor: 'white',
+    width: '100%',
+    maxWidth: 650,
+    borderRadius: 4,
+    boxShadow: theme.shadows[2],
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  course: {
+    marginTop: 25,
+    marginBottom: 17,
+  },
+  makeScheduleButton: {
+    marginTop: 70,
+  },
+  makeScheduleText: {
+    marginLeft: 10,
+  },
+}))
+
 const ShoppingPanel = () => {
   const { credit, courses, deleteCourse, makeSchedule } = useShoppingPanel(mockData)
 
+  const classes = useStyles()
   return (
-    <Box className="container" p={2} bgcolor="white" width="100%" maxWidth={646} borderRadius={4}>
-      <Box className="header" display="flex" alignItems="center" justifyContent="space-between">
+    <Box className={classes.container}>
+      <Box className={classes.header}>
         <Typography variant="h4">Selected Course</Typography>
         <Typography variant="h6">Total {credit} Credits</Typography>
       </Box>
-      <Box className="gened-course">
-        <Typography variant="h6">GenEd Courses</Typography>
+      <Box>
+        <Typography className={classes.course} variant="h6">
+          GenEd Courses
+        </Typography>
         {courses.map((course) => {
           return course.category && <Course course={course} key={course.id} deleteCourse={deleteCourse} />
         })}
       </Box>
-      <Box className="other-course">
-        <Typography variant="h6">Other Courses</Typography>
+      <Box>
+        <Typography className={classes.course} variant="h6">
+          Other Courses
+        </Typography>
         {courses.map((course) => {
           return !course.category && <Course course={course} key={course.id} deleteCourse={deleteCourse} />
         })}
       </Box>
-      <Box mt={6}>
+      <Box className={classes.makeScheduleButton}>
         <Button fullWidth variant="contained" color="primary" onClick={makeSchedule}>
           <TableChartIcon></TableChartIcon>
-          <Box ml={1}>
+          <Box className={classes.makeScheduleText}>
             <Typography variant="body1">Make Schedule</Typography>
           </Box>
         </Button>

c968fe77369349f4a7586692b9cc0888c4d00d97
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-30 21:32:16 +0700

    Change <Box /> to <div />  that only have className

--- src/components/ShoppingPanel/Course.tsx
+++ src/components/ShoppingPanel/Course.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, makeStyles, Typography } from '@material-ui/core'
+import { Grid, makeStyles, Typography } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 import { Delete } from '@material-ui/icons'
 import CourseLabel from '@/components/ShoppingPanel/CourseLabel'
@@ -37,12 +37,12 @@ const Course = ({ course: { id, name, credit, color, category }, deleteCourse }:
   const { t } = useTranslation()
 
   return (
-    <Box className={classes.wrapper}>
+    <div className={classes.wrapper}>
       <Grid className={classes.rootGrid} container>
         <Grid item xs={1} sm={1}>
-          <Box className={classes.deleteButtonWrapper}>
+          <div className={classes.deleteButtonWrapper}>
             <Delete onClick={() => deleteCourse(id)} className={classes.deleteButton} />
-          </Box>
+          </div>
         </Grid>
         <Grid item xs={5} sm={2}>
           <Typography variant="body1">{id}</Typography>
@@ -57,7 +57,7 @@ const Course = ({ course: { id, name, credit, color, category }, deleteCourse }:
           <CourseLabel color={color} category={category} />
         </Grid>
       </Grid>
-    </Box>
+    </div>
   )
 }
 

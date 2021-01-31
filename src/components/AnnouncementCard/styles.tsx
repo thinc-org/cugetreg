91c82bbb6aec7b4eb38be3e172ad76daaf83cf38
new5558 <new17353@gmail.com>
2021-01-31 20:56:55 +0700

    fix: some requested changes

--- src/components/AnnouncementCard/styles.tsx
+++ src/components/AnnouncementCard/styles.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@material-ui/core'
-import { makeStyles, styled } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
+import { styled } from '@material-ui/core/styles'
+import { IMAGE_SIZE } from './const'
 
 export const Image = styled('img')(({ theme }) => ({
   marginRight: theme.spacing(3),
@@ -7,12 +8,23 @@ export const Image = styled('img')(({ theme }) => ({
   objectFit: 'cover',
 }))
 
-export const BoxContainer = styled(Box)(({ theme }) => ({
+export const AnnouncementContainer = styled('div')(({ theme }) => ({
+  borderRadius: '4px',
+  backgroundColor: theme.palette.background.paper,
+  display: 'flex',
+  height: IMAGE_SIZE,
   padding: theme.spacing(3, 12, 3, 6),
 }))
 
-export const useStyles = makeStyles((theme) => ({
-  date: {
-    color: theme.palette.primaryRange['100'],
-  },
+export const TagContainer = styled('div')(({ theme }) => ({
+  margin: theme.spacing(3, 0),
+  display: 'flex',
+}))
+
+export const Tag = styled('div')(({ theme }) => ({
+  marginRight: theme.spacing(2),
+}))
+
+export const DateTitle = styled(Typography)(({ theme }) => ({
+  color: theme.palette.primaryRange['100'],
 }))

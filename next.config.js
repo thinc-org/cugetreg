770cad85eb05fd771eb63b88328d5927a47585cc
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-31 14:25:32 +0700

    :wrench: config next.config.js to support mdx page

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const withOptimizedImages = require('next-optimized-images')
 module.exports = withOptimizedImages({
   // other options here
   env: {
-    IS_PULL_REQUEST: process.env.IS_PULL_REQUEST
-  }
+    IS_PULL_REQUEST: process.env.IS_PULL_REQUEST,
+  },
 })
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
+module.exports = withOptimizedImages(
+  withMDX({
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  })
+)

c54a7abdf5ee0fb23ebab5ccd40f21d264eb7308
new5558 <new17353@gmail.com>
2021-01-31 21:02:48 +0700

    fix: add theme color to genedColorMapper

--- src/components/GenEdChip/utils.ts
+++ src/components/GenEdChip/utils.ts
@@ -3,9 +3,9 @@ import { Theme } from '@material-ui/core'
 
 export const genedColorMapper = (genEd: GenEd, theme: Theme) =>
   ({
-    [GenEd.HU]: '#C7117F',
-    [GenEd.IN]: '#681A83',
+    [GenEd.HU]: theme.palette.highlight.pink[700],
+    [GenEd.IN]: theme.palette.highlight.purple[700],
     [GenEd.SC]: theme.palette.secondaryRange[900],
-    [GenEd.SO]: '#4B991C',
+    [GenEd.SO]: theme.palette.highlight.green[700],
     [GenEd.NOT_GENED]: theme.palette.primary.main,
   }[genEd])

e71d1270976968b69dd417bc62f9e6d9c617ed9f
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-01 14:51:00 +0700

    :recycle: remove comments

--- src/hooks/useShoppingPanel.ts
+++ src/hooks/useShoppingPanel.ts
@@ -1,13 +1,5 @@
 import { useState } from 'react'
-import { GenEd } from '@/utils/types'
 import { Course } from '@thinc-org/chula-courses-types'
-
-// interface Course {
-//   id: number
-//   name: string
-//   credit: number
-//   category: GenEd | null
-// }
 
 const useShoppingPanel = (data: Course[]) => {
   const [courses, setCourses] = useState<Course[]>(data)

2cb0ec76a6bfb746df8da5f803d43592f58e21f3
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-01 14:47:18 +0700

    :bug: mapCourseToGenedType function to work

--- src/components/ShoppingPanel/mockData.ts
+++ src/components/ShoppingPanel/mockData.ts
@@ -7,7 +7,7 @@ export const mockData: Course[] = [
     academicYear: 2020,
 
     // Course info
-    courseNo: '12345',
+    courseNo: '123534645',
     abbrName: 'URBAN LIVING',
     courseNameTh: 'Thai name',
     courseNameEn: 'English Name',
@@ -16,6 +16,144 @@ export const mockData: Course[] = [
     creditHours: '4',
     courseCondition: 'open',
     genEdType: 'SO', // Non GenEd is undefied
+
+    // Exam
+    midterm: undefined,
+    final: undefined,
+
+    // Section
+    sections: [
+      {
+        sectionNo: 2,
+        closed: false,
+        capacity: {
+          current: 13,
+          max: 40,
+        },
+        note: 'No note',
+        classes: [
+          {
+            type: 'LECT',
+            dayOfweek: 'MO',
+            period: {
+              start: '12',
+              end: '14',
+            },
+            building: 'ENG 3',
+            room: '2',
+            teacher: 'AJ',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    studyProgram: 'S',
+    semester: 1,
+    academicYear: 2020,
+
+    // Course info
+    courseNo: '34124',
+    abbrName: 'URBAN LIVING',
+    courseNameTh: 'Thai name',
+    courseNameEn: 'English Name',
+    faculty: 'Engineer',
+    credit: 2,
+    creditHours: '4',
+    courseCondition: 'open',
+    genEdType: 'HU', // Non GenEd is undefied
+
+    // Exam
+    midterm: undefined,
+    final: undefined,
+
+    // Section
+    sections: [
+      {
+        sectionNo: 2,
+        closed: false,
+        capacity: {
+          current: 13,
+          max: 40,
+        },
+        note: 'No note',
+        classes: [
+          {
+            type: 'LECT',
+            dayOfweek: 'MO',
+            period: {
+              start: '12',
+              end: '14',
+            },
+            building: 'ENG 3',
+            room: '2',
+            teacher: 'AJ',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    studyProgram: 'S',
+    semester: 1,
+    academicYear: 2020,
+
+    // Course info
+    courseNo: '567345',
+    abbrName: 'URBAN LIVING',
+    courseNameTh: 'Thai name',
+    courseNameEn: 'English Name',
+    faculty: 'Engineer',
+    credit: 2,
+    creditHours: '4',
+    courseCondition: 'open',
+    genEdType: 'SO', // Non GenEd is undefied
+
+    // Exam
+    midterm: undefined,
+    final: undefined,
+
+    // Section
+    sections: [
+      {
+        sectionNo: 2,
+        closed: false,
+        capacity: {
+          current: 13,
+          max: 40,
+        },
+        note: 'No note',
+        classes: [
+          {
+            type: 'LECT',
+            dayOfweek: 'MO',
+            period: {
+              start: '12',
+              end: '14',
+            },
+            building: 'ENG 3',
+            room: '2',
+            teacher: 'AJ',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    studyProgram: 'S',
+    semester: 1,
+    academicYear: 2020,
+
+    // Course info
+    courseNo: '34543',
+    abbrName: 'URBAN LIVING',
+    courseNameTh: 'Thai name',
+    courseNameEn: 'English Name',
+    faculty: 'Engineer',
+    credit: 2,
+    creditHours: '4',
+    courseCondition: 'open',
+    genEdType: undefined, // Non GenEd is undefied
 
     // Exam
     midterm: undefined,

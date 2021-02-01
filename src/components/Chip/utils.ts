afa625d58f4e82c3aff7cbf718a0b9322c2e563e
new5558 <new17353@gmail.com>
2021-02-01 17:45:33 +0700

    docs: add JS docs

--- src/components/Chip/utils.ts
+++ src/components/Chip/utils.ts
@@ -1,6 +1,11 @@
 import { Theme } from '@material-ui/core'
 import { ChipShade } from './const'
 
+/**
+ *
+ * @param shade shade to map to textColor and backgroundColor
+ * @param theme theme material-ui
+ */
 export const shadeMapper = (shade: ChipShade, theme: Theme) => {
   const textColor = theme.palette.primaryRange[500]
   const backgroundMap = {

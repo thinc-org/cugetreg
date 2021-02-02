46595425ab14e80cc15afc4ef9ae7dd6f8cf5cb0
Pipat Saengow <pay2630@gmail.com>
2021-02-02 22:54:56 +0700

    :art: use env macro

--- src/utils/env/index.ts
+++ src/utils/env/index.ts
@@ -1,3 +1,5 @@
 export const isProduction = process.env.NODE_ENV === 'production'
 
 export const features_darkTheme = process.env.NEXT_PUBLIC_ENABLE_DARK_THEME === 'true'
+
+export const backend_uri = process.env.NEXT_PUBLIC_BACKEND_URL

5fdc6cee0a00129371d682007620383c0ad0cf37
SPK137 <o.sippakorn@gmail.com>
2021-02-02 09:40:03 +0700

    :sparkles: add backToTop button

---
+++ src/components/Footer/TopButton/index.tsx
@@ -0,0 +1,37 @@
+import { Button, makeStyles } from '@material-ui/core'
+import { FlexContainer } from '../FlexContainer'
+import chevronUp from '@/assets/images/chevronUp.svg'
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    ...theme.typography.subtitle1,
+    color: theme.palette.primaryRange['100'],
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: 0,
+    padding: '20px 40px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  logo: {
+    marginLeft: '16px',
+  },
+}))
+
+export function TopButton() {
+  const classes = useStyles()
+
+  return (
+    <FlexContainer className={classes.buttonContainer}>
+      <Button className={classes.button}>
+        กลับด้านบน
+        <a className={classes.logo}>
+          <img src={chevronUp} />
+        </a>
+      </Button>
+    </FlexContainer>
+  )
+}

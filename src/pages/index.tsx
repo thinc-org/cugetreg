f2f8c7a4344a5d24699ee21145e1039e7d19b402
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-03 20:16:34 +0700

    :fire: remove leftover code from mock show modal button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,6 @@
 import SampleComponent from '@/components/SampleComponent'
-import ShoppingPanel from '@/components/ShoppingPanel'
-import { mockData } from '@/components/ShoppingPanel/mockData'
-import { Button, Modal, Box } from '@material-ui/core'
-import { useState } from 'react'
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false)
-
-  function handleClose(event: React.MouseEvent<HTMLElement>) {
-    event.preventDefault()
-    if (event.target === event.currentTarget) {
-      setShowModal(false)
-    }
-  }
-
   return (
     <>
       <SampleComponent />

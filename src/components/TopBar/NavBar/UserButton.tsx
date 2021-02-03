ea64bec17b096912ba60a6985ff61d92d1cf34c5
Pipat Saengow <pay2630@gmail.com>
2021-02-04 00:41:17 +0700

    :sparkles: add google auth

---
+++ src/components/TopBar/NavBar/UserButton.tsx
@@ -0,0 +1,26 @@
+import { createRedirectUrl, GetAuthData, GET_AUTH_DATA, logout } from '@/utils/network/googleauth'
+import { useQuery } from '@apollo/client'
+import { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+import { NavBarItem } from './NavBarItem'
+import PersonIcon from '@material-ui/icons/Person'
+
+export default function UserButton() {
+  const { data } = useQuery<GetAuthData>(GET_AUTH_DATA)
+  const userName = data?.authData?.firstName
+  const onLogin = useCallback(() => {
+    const uri = createRedirectUrl()
+    location.replace(uri)
+  }, [])
+  const onLogout = useCallback(logout, [])
+
+  const { t } = useTranslation()
+
+  if (userName)
+    return (
+      <NavBarItem onClick={onLogout}>
+        <PersonIcon /> {userName}
+      </NavBarItem>
+    )
+  else return <NavBarItem onClick={onLogin}>{t('navBar:signin')}</NavBarItem>
+}
--- src/utils/network/apollo.ts
+++ src/utils/network/apollo.ts
@@ -3,15 +3,17 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import env from '@/utils/env/macro'
 import { authDataFieldPolicy } from './googleauth'
 
-export const client = new ApolloClient({
-  uri: env.backend.uri,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          ...authDataFieldPolicy,
-        },
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        ...authDataFieldPolicy,
       },
     },
-  }),
+  },
+})
+
+export const client = new ApolloClient({
+  uri: env.backend.uri,
+  cache: cache,
 })

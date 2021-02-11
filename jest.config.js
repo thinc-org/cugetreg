9f34b2fe0829320b0593b846b09d47d6a572bbb0
new5558 <new17353@gmail.com>
2021-02-11 16:51:43 +0700

    feat: add testing library

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}

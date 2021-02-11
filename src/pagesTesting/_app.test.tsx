bf1db881f73f673f49f22373f1083b25c9761a2e
new5558 <new17353@gmail.com>
2021-02-11 18:15:36 +0700

    test: add rough test for _app.tex

---
+++ src/pagesTesting/_app.test.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { render } from '@testing-library/react'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+// import * as AppType from '@/pages/_app'
+// import MyApp from '@/pages/_app'
+
+describe('Chip', () => {
+  const useAppSpy = jest.fn()
+
+  jest.doMock('@/hooks/useApp', () => useAppSpy)
+
+  const { default: App } = require('@/pages/_app')
+  const MOCK_COMPONENT = () => <div></div>
+  const MOCK_PROPS = { Component: MOCK_COMPONENT, pageProps: {}, forceDark: true }
+
+  it('shoulde be able to create', () => {
+    const chip = render(<App {...MOCK_PROPS} />)
+    expect(chip).toBeTruthy()
+  })
+})

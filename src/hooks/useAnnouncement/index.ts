9d0fd2b9be283b56d533d4e2c57c4064d6764120
new5558 <new17353@gmail.com>
2021-02-11 13:45:24 +0700

    feat: add announcement page logic

--- src/hooks/useAnnouncement/index.ts
+++ src/hooks/useAnnouncement/index.ts
@@ -1,21 +1,47 @@
-import { AnnouncementProps } from '@/pages/announcement'
-import { useState, useMemo } from 'react'
+import { OnSubmit } from '@/components/AnnouncementSearch'
+import { GenEdType } from '@thinc-org/chula-courses'
+import { useState } from 'react'
+import Fuse from 'fuse.js'
+import { ALL_CATEGORIES, ALL_FACULTIES, FACULTIES, CATAGORIES, mockAnnouncements } from './const'
+
+import differenceInCalendarDays from 'date-fns/differenceInCalendarDays'
+
+export type TagType = GenEdType | 'open' | 'close' | 'chula' | 'other'
+
+export interface Announcement {
+  _id: string
+  title: string
+  description: string
+  content: string
+  date: Date
+  tags: TagType[]
+  faculties: string[]
+  thumbnail: string
+}
 
 const useAnnouncement = () => {
-  const [announcements, setAnnouncements] = useState<AnnouncementProps[]>([])
+  const [filteredAnnouncements, setFilteredAnnouncements] = useState<Announcement[]>(mockAnnouncements)
 
-  const search = () => {
-    // searchKeyword
-    console.log('search')
-    const mockResult: AnnouncementProps[] = []
-    setAnnouncements(mockResult)
+  const search: OnSubmit = (keyword, date, category, faculty) => {
+    let filtered = mockAnnouncements.filter((announcement) => {
+      const dateMatch = date === null || differenceInCalendarDays(announcement.date, date) === 0
+      const categoryMatch = category == ALL_CATEGORIES || announcement.tags.includes(category)
+      const facultyMatch = faculty == ALL_FACULTIES || announcement.faculties.includes(faculty)
+      return dateMatch && categoryMatch && facultyMatch
+    })
+
+    if (keyword.length !== 0) {
+      const options = {
+        keys: ['title', 'description', 'content'],
+      }
+      const fuse = new Fuse(filtered, options)
+      filtered = fuse.search(keyword).map((item) => item.item)
+    }
+
+    setFilteredAnnouncements(filtered)
   }
 
-  const filteredAnnouncements = useMemo(() => {
-    return announcements
-  }, [announcements])
-
-  return { announcements: filteredAnnouncements, search }
+  return { announcements: filteredAnnouncements, search, categories: CATAGORIES, faculties: FACULTIES }
 }
 
 export default useAnnouncement
--- src/pages/announcement.tsx
+++ src/pages/announcement.tsx
@@ -1,23 +1,28 @@
 import { AnnouncementCard } from '@/components/AnnouncementCard'
 import useAnnouncement from '@/hooks/useAnnouncement'
-import { GenEdType } from '@thinc-org/chula-courses'
-
-export interface AnnouncementProps {
-  date: Date
-  imageURL: string
-  title: string
-  tags: string[]
-  genEds: GenEdType[]
-  body: string
-}
+import { AnnouncementSearch } from '@/components/AnnouncementSearch'
 
 export default function AnnouncementPage() {
-  //   const { announcements, search, searchKeyword, searchDate, searchCategory, searchFaculty } = useAnnouncement()
+  const { announcements, search, categories, faculties } = useAnnouncement()
 
-  //   const Announcements = announcements.map((announcement) => {
-  //     return <AnnouncementCard />
-  //   })
+  const Announcements = announcements.map((announcement) => {
+    return (
+      <AnnouncementCard
+        key={announcement._id}
+        date={announcement.date}
+        title={announcement.title}
+        faculties={announcement.faculties}
+        imageURL={announcement.thumbnail}
+        tags={announcement.tags}
+        body={announcement.description}
+      />
+    )
+  })
 
-  //   return <div>{Announcements}</div>
-  return <div></div>
+  return (
+    <div>
+      <AnnouncementSearch categories={categories} faculties={faculties} onSubmit={search} />
+      {Announcements}
+    </div>
+  )
 }

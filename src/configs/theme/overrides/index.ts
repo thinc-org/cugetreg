e461fc4fa39cfa5ba292055515b66439af98618d
Saenyakorn <saneyakorn@gmail.com>
2021-02-14 23:32:03 +0700

    🔧 move overrides.ts to overrides folder

--- src/configs/theme/overrides/index.ts
+++ src/configs/theme/overrides/index.ts
@@ -1,9 +1,11 @@
 import { Theme } from '@material-ui/core'
 import { Overrides } from '@material-ui/core/styles/overrides'
+import overrideChipStyles from './chip'
 
 const overrideMuiBaseComponent = (defaultTheme: Theme): Theme => {
   const overrides: Overrides = {
     // override here
+    MuiChip: overrideChipStyles(defaultTheme),
   }
 
   defaultTheme.overrides = overrides

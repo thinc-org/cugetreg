4c9dbb3406b2948f3673b8ad19d7874544a69d3e
Saenyakorn <saneyakorn@gmail.com>
2021-02-15 23:07:49 +0700

    🔥 remove duplicate function

--- src/components/Chips/utils.tsx
+++
@@ -1,6 +0,0 @@
-import { HighlightColor, HighlightColorRange } from '@/configs/theme/palette'
-import { Theme } from '@material-ui/core'
-
-export const highlightMapper = (color: HighlightColor, theme: Theme): HighlightColorRange => {
-  return theme.palette.highlight[color]
-}

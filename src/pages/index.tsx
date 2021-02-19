2791a23ec0ad45f49ed00ff714b68570ed7242e1
Saenyakorn <saneyakorn@gmail.com>
2021-02-19 14:55:02 +0700

    🔨 add simple usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,56 @@
 import SampleComponent from '@/components/SampleComponent'
+import { mockData } from '@/components/ShoppingPanel/mockData'
+import { useShoppingCartContext } from '@/contexts/shoppingCart'
+import { Box, Button } from '@material-ui/core'
+import { observer } from 'mobx-react'
+import { useState } from 'react'
 
-export default function Home() {
+const Home = observer(() => {
+  const shoppingCart = useShoppingCartContext()
+
+  const [index, setIndex] = useState(0)
+  const getMockCourse = () => {
+    setIndex((value) => value + 1)
+    return mockData[index]
+  }
+  const onAddItem = () => {
+    if (index < 4) {
+      const mockCourse = getMockCourse()
+      shoppingCart.addItem(mockCourse, mockCourse.sections[0].sectionNo)
+    }
+  }
+
+  const onSelectItem = (courseNo: string) => {
+    shoppingCart.selectItem(courseNo)
+  }
+
+  const onClearItems = () => {
+    shoppingCart.removeAllSelectedItem()
+  }
+
   return (
     <>
       <SampleComponent />
+      <Box display="flex" flexDirection="column">
+        {shoppingCart.allCourses.map(({ courseNo, abbrName }, index) => (
+          <Box mt={1} key={index}>
+            <Button
+              onClick={() => onSelectItem(courseNo)}
+              style={{ background: shoppingCart.shopItems[courseNo].isSelected ? 'red' : 'transparent' }}
+            >{`course: ${courseNo} ${abbrName}`}</Button>
+          </Box>
+        ))}
+      </Box>
+      {`shopping Cart State = ${shoppingCart.state}`}
+      <br />
+      <Button variant="outlined" onClick={onAddItem}>
+        Add Item
+      </Button>
+      <Button variant="outlined" onClick={onClearItems}>
+        Remove Selected Item
+      </Button>
     </>
   )
-}
+})
+
+export default Home

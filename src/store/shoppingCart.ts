fcc644a48f5df0574194db03d5b52f0f39867080
Saenyakorn <saneyakorn@gmail.com>
2021-03-30 10:23:46 +0700

    📝 add more jsdoc for shopping cart store

--- src/store/shoppingCart.ts
+++ src/store/shoppingCart.ts
@@ -33,8 +33,17 @@ export class ShoppingCartStore implements ShoppingCartProps {
     return { ...course, selectedSectionNo, isSelected: false }
   }
 
+  private removeItem(courseNo: string) {
+    if (this.shopItems[courseNo]) delete this.shopItems[courseNo]
+    if (this.selectedShopItems[courseNo]) delete this.selectedShopItems[courseNo]
+  }
+
+  /**
+   * this method used for toggling selection state of the shopItems
+   * @param {string} courseNo - the course number
+   */
   @action
-  selectItem(courseNo: string) {
+  toggleItemSelection(courseNo: string) {
     const shopItem = this.shopItems[courseNo]
     shopItem.isSelected = !shopItem.isSelected
     if (shopItem.isSelected) {
@@ -46,24 +55,31 @@ export class ShoppingCartStore implements ShoppingCartProps {
     }
   }
 
+  /**
+   * This method used for adding interested courses into the shopping cart
+   * @param {string} course - the course number
+   * @param {string} [selectedSectionNo=1] - the selected section of the course, default value is 1
+   */
   @action
-  addItem(course: Course, selectedSectionNo: string) {
+  addItem(course: Course, selectedSectionNo = '1') {
+    // TODO create a function that find the first course's section number
     const shopItem = this.createNewItem(course, selectedSectionNo)
     this.shopItems[shopItem.courseNo] = shopItem
   }
 
+  /**
+   * This method used for removing the selected courses
+   */
   @action
-  removeItem(courseNo: string) {
-    if (this.shopItems[courseNo]) delete this.shopItems[courseNo]
-    if (this.selectedShopItems[courseNo]) delete this.selectedShopItems[courseNo]
-  }
-
-  @action
-  removeAllSelectedItem() {
+  removeSelectedItems() {
     Object.keys(this.selectedShopItems).forEach((courseNo) => this.removeItem(courseNo))
     this.selectedShopItems = {}
   }
 
+  /**
+   * This method used for getting one course from the shopping cart
+   * @param {string} course - the course number
+   */
   course = computedFn((courseNo: string): Course | undefined => {
     const shopItem = this.shopItems[courseNo]
     if (!shopItem) return undefined
@@ -77,6 +93,10 @@ export class ShoppingCartStore implements ShoppingCartProps {
     }, {}) as Course
   })
 
+  /**
+   * This method used for getting all courses from the shopping cart
+   * The courses is sorted by `courseNo`
+   */
   @computed
   get allCourses(): Course[] {
     const allCourses = Object.keys(this.shopItems).reduce((accumulator, currentCourseNo) => {

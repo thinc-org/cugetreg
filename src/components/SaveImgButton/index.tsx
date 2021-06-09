0112b36759b64f1b1fbb53685f147810c28ab61c
SPK137 <o.sippakorn@gmail.com>
2021-06-09 14:56:07 +0700

    feat: add save image button

---
+++ src/components/SaveImgButton/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@material-ui/core'
+import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded'
+import React, { MutableRefObject, useCallback } from 'react'
+import { useScreenshot } from 'use-react-screenshot'
+
+const SaveImgButton: React.FC<{ imageRef: MutableRefObject<null> }> = ({ imageRef }) => {
+  const [image, takeScreenshot] = useScreenshot()
+
+  const saveImage = useCallback(() => {
+    takeScreenshot(imageRef.current)
+    console.log(image)
+    const link = document.createElement('a')
+    link.href = image
+    link.download = 'schedule.png'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }, [image, takeScreenshot, imageRef])
+
+  return (
+    <Button variant="outlined" onClick={saveImage}>
+      <GetAppRoundedIcon />
+      PNG
+    </Button>
+  )
+}
+
+export default SaveImgButton

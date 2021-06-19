a0d968dfde27e6ebc44d52969b225d74920ba2f6
Saenyakorn <saneyakorn@gmail.com>
2021-06-19 19:11:59 +0700

    ✨ update filter bar

--- src/components/FilterBar/index.tsx
+++ src/components/FilterBar/index.tsx
@@ -5,21 +5,25 @@ import {
   createDayOfWeekCheckboxes,
   createSpecialCheckboxes,
 } from '@/components/FilterBar/constants'
-import { Stack } from '@material-ui/core'
-import { CheckboxGroup } from './components/CheckboxGroup'
+import { Paper, Stack } from '@material-ui/core'
+import { useStyles } from '@/components/FilterBar/styled'
+import { CheckboxGroup } from '@/components/FilterBar/components/CheckboxGroup'
 
 export interface FilterBarProps {}
 
 export function FilterBar() {
+  const classes = useStyles()
   const { checkboxes: genEdCheckboxes } = useFilterBar(createGenEdCheckboxes)
   const { checkboxes: dayOfWeekCheckboxes } = useFilterBar(createDayOfWeekCheckboxes)
   const { checkboxes: specialCheckboxes } = useFilterBar(createSpecialCheckboxes)
 
   return (
-    <Stack spacing={4}>
-      <CheckboxGroup title="หมวดหมู่ GenEd" checkboxes={genEdCheckboxes} />
-      <CheckboxGroup title="วันในสัปดาห์" checkboxes={dayOfWeekCheckboxes} />
-      <CheckboxGroup title="แสดงผลพิเศษ" checkboxes={specialCheckboxes} />
-    </Stack>
+    <Paper className={classes.paper}>
+      <Stack spacing={4}>
+        <CheckboxGroup title="หมวดหมู่ GenEd" checkboxes={genEdCheckboxes} />
+        <CheckboxGroup title="วันในสัปดาห์" checkboxes={dayOfWeekCheckboxes} />
+        <CheckboxGroup title="แสดงผลพิเศษ" checkboxes={specialCheckboxes} />
+      </Stack>
+    </Paper>
   )
 }

7093f1f57f07047db039d16731033256d2b86d82
Saenyakorn <saneyakorn@gmail.com>
2021-06-20 18:58:05 +0700

    feat: unfinished course search page

--- src/pages/[studyProgram]/courses/index.tsx
+++ src/pages/[studyProgram]/courses/index.tsx
@@ -1,19 +1,34 @@
-import { Box, Stack, Typography } from '@material-ui/core'
+import { useContext } from 'react'
+import { Box, Stack as MuiStack, Typography } from '@material-ui/core'
 import { CourseList } from '@/components/CourseList'
 import { SearchField } from '@/components/SearchField'
 import { FilterIconButton } from '@/components/FilterIconButton'
 import { SelectedCoursesButton } from '@/components/SelectedCoursesButton'
+import { CourseSearchContext } from '@/context/CourseSearch'
+import { FilterBar } from '@/components/FilterBar'
+import styled from '@emotion/styled'
+
+const Stack = styled(MuiStack)`
+  margin-bottom: ${({ theme }) => theme.spacing(4)};
+`
 
 function CourseSearchPage() {
+  const { openFilterBar } = useContext(CourseSearchContext)
+
   return (
     <Box padding="2em">
-      <Typography variant="h1">ค้นหาวิชาเรียน</Typography>
+      <Stack spacing={2} direction="row">
+        <Typography variant="h2">ค้นหาวิชาเรียน</Typography>
+      </Stack>
       <Stack spacing={2} direction="row">
         <SearchField />
         <FilterIconButton />
         <SelectedCoursesButton />
       </Stack>
-      <CourseList />
+      <Stack spacing={3} direction="row">
+        <CourseList />
+        {openFilterBar && <FilterBar />}
+      </Stack>
     </Box>
   )
 }

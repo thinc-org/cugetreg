eb22dc94207a99082b719f387a6fde5e0637b810
new5558 <new17353@gmail.com>
2021-06-20 17:21:32 +0700

    feat: add schedule apge without description

--- src/components/CR11/index.tsx
+++ src/components/CR11/index.tsx
@@ -1,43 +1,5 @@
-import { withTypography } from '@/hoc/withTypography'
-import { makeStyles } from '@material-ui/core'
 import { Course } from '@thinc-org/chula-courses'
-
-const useStyles = makeStyles((theme) => ({
-  header: {
-    background: theme.palette.primaryRange['30'],
-    borderRadius: theme.shape.borderRadius,
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    borderSpacing: '0',
-    '& td, th': {
-      width: '142px',
-      textAlign: 'center',
-      boxSizing: 'content-box',
-    },
-    '& th': {
-      padding: theme.spacing(1, 4),
-    },
-    '& td': {
-      padding: theme.spacing(0.5, 4),
-    },
-    '& td:nth-child(3)': {
-      width: '316px',
-      textAlign: 'left',
-    },
-    '& tr:nth-child(2) td': {
-      paddingTop: theme.spacing(2),
-    },
-  },
-  totalCredit: {
-    '& td': {
-      paddingTop: theme.spacing(3),
-    },
-  },
-}))
-
-const Column = withTypography((props) => <td {...props} />)
-const ColumnHeader = withTypography((props) => <th {...props} />)
+import { useStyles, Column, ColumnHeader } from './styles'
 
 interface PropTypes {
   courses?: Course[]
@@ -47,7 +9,6 @@ export function CR11({ courses }: PropTypes) {
   const classes = useStyles()
   const totalCredit = courses?.reduce((accumulator, course) => accumulator + course.credit, 0)
   const Items = courses?.map((course, i) => {
-    console.log(course.sections[0].sectionNo, 'course')
     return (
       <tr key={course.courseNo}>
         <Column variant="body1">{i + 1}</Column>
@@ -63,10 +24,16 @@ export function CR11({ courses }: PropTypes) {
       <table className={classes.table}>
         <tr className={classes.header}>
           <ColumnHeader variant="h6">ลำดับที่</ColumnHeader>
-          <ColumnHeader variant="h6">รหัสรายวิชา</ColumnHeader>
+          <ColumnHeader variant="h6">
+            <span className={classes.desktop}>รหัสรายวิชา</span>
+            <span className={classes.mobile}>รหัสวิชา</span>
+          </ColumnHeader>
           <ColumnHeader variant="h6">ชื่อย่อรายวิชา</ColumnHeader>
           <ColumnHeader variant="h6">ตอนเรียน</ColumnHeader>
-          <ColumnHeader variant="h6">หน่วยกิต</ColumnHeader>
+          <ColumnHeader variant="h6">
+            <span className={classes.desktop}>หน่วยกิต</span>
+            <span className={classes.mobile}>หน่วย</span>
+          </ColumnHeader>
         </tr>
         {Items}
         <tr className={classes.totalCredit}>
--- src/pages/[studyProgram]/schedule/cr11/index.tsx
+++ src/pages/[studyProgram]/schedule/cr11/index.tsx
@@ -1,12 +1,66 @@
 import { mockData } from '@/components/ShoppingPanel/mockData'
 import { courseCartStore } from '@/store/shoppingCart'
-import { Box, Button } from '@material-ui/core'
+import { Button, Typography, IconButton, makeStyles } from '@material-ui/core'
 import { observer } from 'mobx-react'
 import React, { useEffect } from 'react'
 import { CR11 } from '@/components/CR11'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+
+const useStyles = makeStyles((theme) => ({
+  buttonMobile: {
+    border: '1px solid #2A2D48',
+    boxSizing: 'border-box',
+    borderRadius: '4px',
+    textAlign: 'right',
+    padding: '5px 8px',
+    color: theme.palette.primaryRange[400],
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  semester: {
+    fontWeight: 500,
+  },
+  main: {
+    lineHeight: theme.spacing(4),
+  },
+  subtitle: {
+    color: theme.palette.primaryRange['100'],
+    fontWeight: 500,
+    [theme.breakpoints.down('sm')]: {
+      marginTop: theme.spacing(3),
+    },
+  },
+  desktop: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  mobile: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: theme.spacing(3, 0, 6, 0),
+    [theme.breakpoints.down('sm')]: {
+      margin: theme.spacing(3, 0, 4, 0),
+    },
+  },
+  top: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+}))
 
 const Home = observer(() => {
   const shoppingCart = courseCartStore
+  const classes = useStyles()
 
   useEffect(() => {
     // get mock data
@@ -21,17 +75,39 @@ const Home = observer(() => {
 
   return (
     <>
-      {/* <Box display="flex" flexDirection="column">
-        {shoppingCart.courses.map(({ courseNo, abbrName }, index) => (
-          <Box mt={1} key={index}>
-            <Button
-              style={{ background: shoppingCart.item(courseNo)?.isSelected ? 'red' : 'transparent' }}
-            >{`course: ${courseNo} ${abbrName}`}</Button>
-          </Box>
-        ))}
-      </Box> */}
-      <CR11 courses={shoppingCart.courses} />
-      <br />
+      <div className={classes.container}>
+        <div className={classes.top}>
+          <Button
+            className={classes.desktop}
+            startIcon={<ArrowBackIosIcon />}
+            color="primary"
+            variant="outlined"
+            disableElevation
+          >
+            กลับ
+          </Button>
+          <IconButton className={`${classes.buttonMobile} ${classes.mobile}`} aria-label="back">
+            <ArrowBackIosIcon />
+          </IconButton>
+          <Typography className={`${classes.semester} ${classes.mobile}`} variant="subtitle1">
+            ปีการศึกษา 2563/2 หลักสูตรนานาชาติ
+          </Typography>
+        </div>
+        <div className={classes.title}>
+          <div>
+            <Typography className={classes.main} variant="h3">
+              รายวิชาที่ต้องการลงทะเบียนเรียน
+            </Typography>
+            <Typography className={classes.subtitle} variant="subtitle1">
+              จำลองเอกสาร แสดงความจำนงขอลงทะเบียนเรียน (จท11)
+            </Typography>
+          </div>
+          <Typography className={`${classes.semester} ${classes.desktop}`} variant="subtitle1">
+            ปีการศึกษา 2563/2 หลักสูตรนานาชาติ
+          </Typography>
+        </div>
+        <CR11 courses={shoppingCart.courses} />
+      </div>
     </>
   )
 })

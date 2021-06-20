4c4598f5a42f21fee64afe0ecb656584ba5407c6
SPK137 <o.sippakorn@gmail.com>
2021-06-20 20:13:41 +0700

    fix: no file downloaded bug

--- src/components/SaveImgButton/index.tsx
+++ src/components/SaveImgButton/index.tsx
@@ -1,20 +1,25 @@
 import { Button } from '@material-ui/core'
 import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded'
-import React, { MutableRefObject, useCallback } from 'react'
-import { useScreenshot } from 'use-react-screenshot'
+import React, { MutableRefObject, useEffect } from 'react'
+import { useScreenshot, createFileName } from 'use-react-screenshot'
 
 const SaveImgButton: React.FC<{ imageRef: MutableRefObject<null> }> = ({ imageRef }) => {
   const [image, takeScreenshot] = useScreenshot()
 
-  const saveImage = useCallback(() => {
-    takeScreenshot(imageRef.current)
-    const link = document.createElement('a')
-    link.href = image
-    link.download = 'schedule.png'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-  }, [image, takeScreenshot, imageRef])
+  const download = (image: string, { name = 'Schedule', extension = 'png' } = {}) => {
+    const a = document.createElement('a')
+    a.href = image
+    a.download = createFileName(extension, name)
+    a.click()
+  }
+
+  const saveImage = () => takeScreenshot(imageRef.current)
+
+  useEffect(() => {
+    if (image) {
+      download(image, { name: 'Schedule', extension: 'png' })
+    }
+  }, [image])
 
   return (
     <Button variant="outlined" onClick={saveImage}>

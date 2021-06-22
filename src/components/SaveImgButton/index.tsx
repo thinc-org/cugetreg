7d404b76be3a0100c4ae0bacee83ec87728e60af
SPK137 <o.sippakorn@gmail.com>
2021-06-22 21:32:28 +0700

    fix: remove no picture and incomplete picture bug

--- src/components/SaveImgButton/index.tsx
+++ src/components/SaveImgButton/index.tsx
@@ -1,24 +1,20 @@
 import { Button } from '@material-ui/core'
 import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded'
-import React, { RefObject, useCallback, useEffect } from 'react'
+import React, { RefObject, useCallback } from 'react'
 import { useScreenshot } from 'use-react-screenshot'
 
 const SaveImgButton: React.FC<{ imageRef: RefObject<HTMLDivElement> }> = ({ imageRef }) => {
-  const [image, takeScreenshot] = useScreenshot()
+  const [_, takeScreenshot] = useScreenshot()
 
   const saveImage = useCallback(async () => {
-    if (!image) await takeScreenshot(imageRef.current)
+    const image = await takeScreenshot(imageRef.current)
     const link = document.createElement('a')
     link.href = image
     link.download = 'schedule.png'
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
-  }, [image, takeScreenshot, imageRef])
-
-  useEffect(() => {
-    takeScreenshot(imageRef.current)
-  }, [imageRef.current])
+  }, [takeScreenshot, imageRef])
 
   return (
     <Button variant="outlined" onClick={saveImage}>

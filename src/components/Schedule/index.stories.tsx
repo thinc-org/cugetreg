eadedcefe97902d1e3a1970427d6165dec141a03
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-06-23 22:39:38 +0700

    fix: schedule story build fail

--- src/components/Schedule/index.stories.tsx
+++ src/components/Schedule/index.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Meta } from '@storybook/react/types-6-0'
 import { Schedule } from '.'
 import { mockClasses } from './mockClasses'
+import { useScheduleClass } from './utils'
 
 export default {
   title: 'Component/Schedule',
@@ -9,5 +10,6 @@ export default {
 } as Meta
 
 export const ScheduleStory = () => {
-  return <Schedule classes={mockClasses} />
+  const scheduleClasses = useScheduleClass(mockClasses)
+  return <Schedule classes={scheduleClasses} />
 }

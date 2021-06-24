d767af0114bfe5ef1a836e1cc2d68a15daca8ee1
Saenyakorn <saneyakorn@gmail.com>
2021-06-24 22:54:32 +0700

    feat: responsive filter section

--- src/components/FilterSection/index.tsx
+++ src/components/FilterSection/index.tsx
@@ -5,29 +5,78 @@ import {
   createDayOfWeekCheckboxes,
   createSpecialCheckboxes,
 } from '@/components/FilterSection/constants'
-import { Hidden, Paper, Stack } from '@material-ui/core'
+import { Button as MuiButton, Dialog, DialogContent as MuiDialogContent, Hidden, Paper, Stack } from '@material-ui/core'
 import { useStyles } from '@/components/FilterSection/styled'
 import { CheckboxGroup } from '@/components/FilterSection/components/CheckboxGroup'
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 
-export interface FilterSectionProps {}
+const DialogContent = styled(MuiDialogContent)`
+  padding: ${({ theme }) => theme.spacing(4)};
+`
 
-export function FilterSection() {
+const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  div:first-child {
+    ${({ theme }) => {
+      return css`
+        margin-right: ${theme.spacing(12)};
+        ${theme.breakpoints.down('sm')} {
+          margin-right: ${theme.spacing(2)};
+        }
+      `
+    }};
+  }
+`
+
+const Button = styled(MuiButton)`
+  margin-top: ${({ theme }) => theme.spacing(2)}; ;
+`
+export interface FilterSectionProps {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const FilterSection: React.FC<FilterSectionProps> = ({ open, setOpen }) => {
   const classes = useStyles()
   const { checkboxes: genEdCheckboxes } = useFilterBar(createGenEdCheckboxes, 'genEdTypes')
   const { checkboxes: dayOfWeekCheckboxes } = useFilterBar(createDayOfWeekCheckboxes, 'dayOfWeeks')
   const { checkboxes: specialCheckboxes } = useFilterBar(createSpecialCheckboxes)
 
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  if (!open) return null
+
   return (
-    <Hidden mdDown>
-      <div>
-        <Paper className={classes.paper} variant="outlined">
-          <Stack spacing={4}>
-            <CheckboxGroup title="หมวดหมู่ GenEd" checkboxes={genEdCheckboxes} />
-            <CheckboxGroup title="วันในสัปดาห์" checkboxes={dayOfWeekCheckboxes} />
+    <>
+      <Hidden mdDown>
+        <div>
+          <Paper className={classes.paper} variant="outlined">
+            <Stack spacing={4}>
+              <CheckboxGroup title="หมวดหมู่ GenEd" checkboxes={genEdCheckboxes} />
+              <CheckboxGroup title="วันในสัปดาห์" checkboxes={dayOfWeekCheckboxes} />
+              <CheckboxGroup title="แสดงผลพิเศษ" checkboxes={specialCheckboxes} />
+            </Stack>
+          </Paper>
+        </div>
+      </Hidden>
+      <Hidden mdUp>
+        <Dialog open onClose={handleClose} maxWidth="xl">
+          <DialogContent>
+            <Box>
+              <CheckboxGroup title="หมวดหมู่ GenEd" checkboxes={genEdCheckboxes} />
+              <CheckboxGroup title="วันในสัปดาห์" checkboxes={dayOfWeekCheckboxes} />
+            </Box>
             <CheckboxGroup title="แสดงผลพิเศษ" checkboxes={specialCheckboxes} />
-          </Stack>
-        </Paper>
-      </div>
-    </Hidden>
+            <Button color="primary" variant="outlined" fullWidth onClick={handleClose}>
+              เลือกตัวกรอง
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </Hidden>
+    </>
   )
 }

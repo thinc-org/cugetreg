4c04bde4d424dda42841f523b0ed8d43fb33171f
new5558 <new17353@gmail.com>
2021-06-26 21:40:48 +0700

    feat: reconfig font to improve performance

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,14 +10,16 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;700&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;500;700&display=swap"
+            as="style"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+            as="style"
           />
         </Head>
         <body>

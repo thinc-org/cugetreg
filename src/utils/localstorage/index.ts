a59ce4e4c0ea450e8c1f56fed332eeee153d5887
new5558 <new17353@gmail.com>
2021-06-27 15:07:27 +0700

    feat: implement local storage for non-login users

---
+++ src/utils/localstorage/index.ts
@@ -0,0 +1,20 @@
+import { CourseCartItem, courseCartStore } from '@/store'
+import { runInAction } from 'mobx'
+
+const SHOPPING_CART_KEY = 'SHOPPING_CART_KEY'
+
+export function syncWithLocalStorage(items: CourseCartItem[], isInitializedLocal: boolean) {
+  const shoppingCart = localStorage.getItem(SHOPPING_CART_KEY)
+  if (!shoppingCart) {
+    const courses = JSON.stringify(items)
+    localStorage.setItem(SHOPPING_CART_KEY, courses)
+  } else if (!isInitializedLocal) {
+    runInAction(() => {
+      courseCartStore.isInitializedLocal = true
+      courseCartStore.shopItems = JSON.parse(shoppingCart)
+    })
+  } else {
+    const courses = JSON.stringify(items)
+    localStorage.setItem(SHOPPING_CART_KEY, courses)
+  }
+}

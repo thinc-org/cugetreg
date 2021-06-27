33a42dccf6239b7120ca3420a13adf72c97e09fb
Saenyakorn <saneyakorn@gmail.com>
2021-06-27 12:36:09 +0700

    fix: sticky search field with tag lsit

---
+++ src/components/TagList/styled.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+import { Stack } from '@material-ui/core'
+
+export const StyledStack = styled(Stack)`
+  height: 24px;
+  display: flex;
+  justify-content: center;
+`

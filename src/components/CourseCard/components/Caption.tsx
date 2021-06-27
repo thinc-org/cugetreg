bbf795e08c56e7d5aaaa9b279964c7d5b4e207b1
Saenyakorn <saneyakorn@gmail.com>
2021-06-27 20:31:17 +0700

    fix: card ui

--- src/components/CourseCard/components/Caption.tsx
+++ src/components/CourseCard/components/Caption.tsx
@@ -1,5 +1,10 @@
-import { Typography } from '@material-ui/core'
+import styled from '@emotion/styled'
+import { Typography as MuiTypography } from '@material-ui/core'
 import { PropsWithChildren } from 'react'
+
+const Typography = styled(MuiTypography)`
+  min-width: 80;
+`
 
 export const Caption = ({ children }: PropsWithChildren<{}>) => (
   <Typography variant="caption" color="primaryRange.100">

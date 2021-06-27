13c47b1c9f241326454915ae341006555c4f0f21
new5558 <new17353@gmail.com>
2021-06-28 00:32:42 +0700

    fix: prevent refresh on click shoppingcart link

--- src/components/ShoppingPanel/components/ActionButton.tsx
+++ src/components/ShoppingPanel/components/ActionButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from '@emotion/styled'
 import { Button, ButtonProps } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
@@ -6,6 +6,9 @@ import TableChartIcon from '@material-ui/icons/TableChart'
 import { useTranslation } from 'react-i18next'
 import { ShoppingState } from '@/components/ShoppingPanel/hooks'
 import { useCourseGroup } from '@/utils/hooks/useCourseGroup'
+import Link from 'next/link'
+
+import { ShoppingCartModalContext } from '@/context/ShoppingCartModal'
 
 const ErrorButton = styled(Button)`
   color: ${({ theme }) => theme.palette.error.main};
@@ -27,6 +30,7 @@ export const ActionButton: React.FC<ActionButtonProps> = ({
   const { t } = useTranslation('shoppingPanel')
   const { studyProgram } = useCourseGroup()
   const href = `/${studyProgram}/schedule`
+  const { onClose } = useContext(ShoppingCartModalContext)
 
   const defaultButtonProps: ButtonProps = {
     ...props,
@@ -37,9 +41,11 @@ export const ActionButton: React.FC<ActionButtonProps> = ({
 
   if (status === ShoppingState.Default) {
     return (
-      <Button {...defaultButtonProps} startIcon={<TableChartIcon />} href={href}>
-        {t('makeSchedule.default')}
-      </Button>
+      <Link href={href} passHref>
+        <Button {...defaultButtonProps} startIcon={<TableChartIcon />} onClick={onClose}>
+          {t('makeSchedule.default')}
+        </Button>
+      </Link>
     )
   }
 

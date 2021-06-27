83f525de60589a13447e43870e3c5fa647182bc9
Saenyakorn <saneyakorn@gmail.com>
2021-06-27 22:46:55 +0700

    fix: loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { CircularProgress } from '@material-ui/core'
 
 export interface LoadingProps {
   loading: boolean
@@ -7,7 +8,7 @@ export interface LoadingProps {
 const Container = styled.div`
   margin: ${({ theme }) => theme.spacing(2, 'auto', 0, 'auto')};
   width: 100%;
-  min-height: 60px;
+  min-height: 80px;
   height: 100%;
   display: flex;
   justify-content: center;
@@ -18,5 +19,9 @@ const Container = styled.div`
 export const Loading: React.FC<LoadingProps> = ({ loading }) => {
   if (!loading) return <Container />
 
-  return <Container>Loading</Container>
+  return (
+    <Container>
+      <CircularProgress color="primary" />
+    </Container>
+  )
 }

858a1c2a5d430594c5d38a0c06665f4816bcdc53
new5558 <new17353@gmail.com>
2021-06-27 17:13:45 +0700

    fix: google fonts not working

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,16 +10,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            rel="preload"
-            href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;500;700&display=swap"
-            as="style"
-          />
-          <link
-            rel="preload"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
-            as="style"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Prompt:wght@400;500;700&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>

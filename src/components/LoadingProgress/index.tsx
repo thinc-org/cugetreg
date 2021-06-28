3fd6e3513e330a054ac68787688aeac1d544beda
new5558 <new17353@gmail.com>
2021-06-28 22:08:49 +0700

    perf: remove unused css on spinner

--- src/components/LoadingProgress/index.tsx
+++ src/components/LoadingProgress/index.tsx
@@ -47,47 +47,12 @@ export function LoadingProgress() {
         -ms-transform: rotate(3deg) translate(0px, -4px);
         transform: rotate(3deg) translate(0px, -4px);
       }
-      #nprogress .spinner {
-        display: 'block';
-        position: fixed;
-        z-index: 1031;
-        top: 15px;
-        right: 15px;
-      }
-      #nprogress .spinner-icon {
-        width: 18px;
-        height: 18px;
-        box-sizing: border-box;
-        border: solid 2px transparent;
-        border-top-color: ${theme.palette.secondary.main};
-        border-left-color: ${theme.palette.secondary.main};
-        border-radius: 50%;
-        -webkit-animation: nprogresss-spinner 400ms linear infinite;
-        animation: nprogress-spinner 400ms linear infinite;
-      }
       .nprogress-custom-parent {
         overflow: hidden;
         position: relative;
       }
-      .nprogress-custom-parent #nprogress .spinner,
       .nprogress-custom-parent #nprogress .bar {
         position: absolute;
-      }
-      @-webkit-keyframes nprogress-spinner {
-        0% {
-          -webkit-transform: rotate(0deg);
-        }
-        100% {
-          -webkit-transform: rotate(360deg);
-        }
-      }
-      @keyframes nprogress-spinner {
-        0% {
-          transform: rotate(0deg);
-        }
-        100% {
-          transform: rotate(360deg);
-        }
       }
     `}</style>
   )

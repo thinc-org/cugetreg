8d69a6d7d49361fcba2d09ace0bf68371d682fb7
new5558 <new17353@gmail.com>
2021-06-28 22:40:37 +0700

    perf: move inline css to emotion

--- src/components/LoadingProgress/index.tsx
+++ src/components/LoadingProgress/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import nprogress from 'nprogress'
 import { Router } from 'next/router'
 import { useTheme } from '@material-ui/core'
+import { css, Global } from '@emotion/react'
 
 export function LoadingProgress() {
   const theme = useTheme()
@@ -25,39 +26,39 @@ export function LoadingProgress() {
     Router.events.on('routeChangeError', stop)
   }, [])
 
-  return (
-    <style jsx global>{`
-      #nprogress {
-        pointer-events: none;
-      }
-      #nprogress .bar {
-        background: ${theme.palette.secondary.main};
-        position: fixed;
-        z-index: 1031;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: ${height}px;
-      }
-      #nprogress .peg {
-        display: block;
-        position: absolute;
-        right: 0px;
-        width: 100px;
-        height: 100%;
-        box-shadow: 0 0 10px ${theme.palette.secondary.main}, 0 0 5px ${theme.palette.secondary.main};
-        opacity: 1;
-        -webkit-transform: rotate(3deg) translate(0px, -4px);
-        -ms-transform: rotate(3deg) translate(0px, -4px);
-        transform: rotate(3deg) translate(0px, -4px);
-      }
-      .nprogress-custom-parent {
-        overflow: hidden;
-        position: relative;
-      }
-      .nprogress-custom-parent #nprogress .bar {
-        position: absolute;
-      }
-    `}</style>
-  )
+  const cssCode = css`
+    #nprogress {
+      pointer-events: none;
+    }
+    #nprogress .bar {
+      background: ${theme.palette.secondary.main};
+      position: fixed;
+      z-index: 1031;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: ${height}px;
+    }
+    #nprogress .peg {
+      display: block;
+      position: absolute;
+      right: 0px;
+      width: 100px;
+      height: 100%;
+      box-shadow: 0 0 10px ${theme.palette.secondary.main}, 0 0 5px ${theme.palette.secondary.main};
+      opacity: 1;
+      -webkit-transform: rotate(3deg) translate(0px, -4px);
+      -ms-transform: rotate(3deg) translate(0px, -4px);
+      transform: rotate(3deg) translate(0px, -4px);
+    }
+    .nprogress-custom-parent {
+      overflow: hidden;
+      position: relative;
+    }
+    .nprogress-custom-parent #nprogress .bar {
+      position: absolute;
+    }
+  `
+
+  return <Global styles={cssCode} />
 }

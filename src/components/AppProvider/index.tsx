50bfd3662308755962182b88c3798e411f3c6739
new5558 <new17353@gmail.com>
2021-06-28 15:47:14 +0700

    refactor: move provider to other components

---
+++ src/components/AppProvider/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@/utils/network/apollo'
+import { AnalyticsProvider } from '@/context/analytics'
+import { ShoppingCartModalProvider } from '@/context/ShoppingCartModal'
+import { SnackbarContextProvider } from '@/context/Snackbar'
+import { ModalProps } from '@/context/ShoppingCartModal/types'
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns'
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider'
+import { ThemeProvider, useMediaQuery } from '@material-ui/core'
+
+import { useAnalytics } from '@/context/analytics/hooks'
+
+import env from '@/utils/env/macro'
+import { darkTheme, lightTheme } from '@/configs/theme'
+import { SnackbarProps } from '@/context/Snackbar/types'
+
+interface AppProviderProps {
+  children: React.ReactNode
+  snackBarContextValue: SnackbarProps
+  forceDark: boolean
+  disclosureValue: ModalProps
+}
+
+export function AppProvider({ children, snackBarContextValue, forceDark, disclosureValue }: AppProviderProps) {
+  const prefersDarkMode =
+    env.features.darkTheme &&
+    // features.darkTheme is a constant
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useMediaQuery('(prefers-color-scheme: dark)', {
+      noSsr: true,
+    })
+
+  const { addEvent } = useAnalytics()
+
+  return (
+    <ApolloProvider client={client}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <AnalyticsProvider value={{ addEvent }}>
+          <SnackbarContextProvider value={snackBarContextValue}>
+            <ShoppingCartModalProvider value={disclosureValue}>
+              <ThemeProvider theme={prefersDarkMode || forceDark ? darkTheme : lightTheme}>{children}</ThemeProvider>
+            </ShoppingCartModalProvider>
+          </SnackbarContextProvider>
+        </AnalyticsProvider>
+      </LocalizationProvider>
+    </ApolloProvider>
+  )
+}

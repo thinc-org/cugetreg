cff440e3254fda31fd5174b24370ae7a3271a143
Pipat Saengow <pay2630@gmail.com>
2021-06-28 19:57:47 +0700

    add cache injection

--- src/context/CourseSearch/index.tsx
+++ src/context/CourseSearch/index.tsx
@@ -2,16 +2,30 @@ import React, { createContext, useState, useEffect } from 'react'
 import { SearchCourseResponse, SearchCourseVars, SEARCH_COURSE } from '@/utils/network/BackendGQLQueries'
 import { useSearchCourseQueryParams } from '@/utils/hooks/useSearchCourseQueryParams'
 import { DEFAULT_COURSE_SEARCH_CONTEXT_VALUE, LIMIT_QUERY_CONSTANT } from '@/context/CourseSearch/constants'
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
+import { SearchPagePrefetchData } from '@/pages/[studyProgram]/courses'
 
 export const CourseSearchContext = createContext(DEFAULT_COURSE_SEARCH_CONTEXT_VALUE)
 
-export const CourseSearchProvider: React.FC = (props) => {
+export const CourseSearchProvider: React.FC<{ cache?: SearchPagePrefetchData }> = (props) => {
   const router = useRouter()
   const [offset, setOffset] = useState(0)
 
   const { searchCourseQueryParams } = useSearchCourseQueryParams()
+
+  const [cacheInjected, setCacheInjected] = useState(false)
+  const client = useApolloClient()
+  const cache: SearchPagePrefetchData | undefined = props.cache
+
+  if (!cacheInjected && cache) {
+    client.writeQuery({
+      query: SEARCH_COURSE,
+      data: cache.data,
+      variables: cache.vars,
+    })
+    setCacheInjected(true)
+  }
 
   const courseSearchQuery = useQuery<SearchCourseResponse, SearchCourseVars>(SEARCH_COURSE, {
     notifyOnNetworkStatusChange: true,

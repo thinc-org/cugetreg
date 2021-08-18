08b910ae53173240cbb5a7719b0c79cf6971cab7
new5558 <new17353@gmail.com>
2021-08-18 22:43:39 +0700

    fix: enable console warn and error on production

--- babel.config.js
+++ babel.config.js
@@ -31,7 +31,7 @@ module.exports = {
   ],
   env: {
     production: {
-      plugins: ['transform-remove-console'],
+      plugins: ['transform-remove-console', { "exclude": ["error", "warn"] }],
     },
   },
 }

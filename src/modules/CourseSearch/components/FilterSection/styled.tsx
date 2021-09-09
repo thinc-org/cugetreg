1581f341e006011ec9abf74d3cc555527b871103
Anos Posidos <anonjin44@gmail.com>
2021-09-09 16:26:07 +0700

    fix: fix height of the filter section

--- src/modules/CourseSearch/components/FilterSection/styled.tsx
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-export const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(4),
-    overflowY: 'scroll',
-    height: '100%',
-  },
-}))
---
+++ src/modules/CourseSearch/components/FilterSection/types.ts
@@ -0,0 +1,4 @@
+export interface FilterSectionProps {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}

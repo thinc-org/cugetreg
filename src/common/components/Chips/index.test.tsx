c6c0fcc19beba737cd6b056dc83664d9cc53b66a
new5558 <new17353@gmail.com>
2021-09-11 13:56:57 +0700

    test: convert dom rendering to shallow rendering

--- src/common/components/Chips/index.test.tsx
+++ src/common/components/Chips/index.test.tsx
@@ -1,20 +1,11 @@
-import { ThemeProvider } from '@material-ui/core'
-import { render } from '@testing-library/react'
+import Chip from '@material-ui/core/Chip'
 import { GenEdTypeEnum, DayOfWeekEnum } from '@thinc-org/chula-courses'
-
-import { mockAndShallowSpy } from '@/common/testing/mockAndShallowSpy'
-import { lightTheme } from '@/configs/theme'
+import { shallow } from 'enzyme'
 
 import { GeneralChipProps } from '.'
 import { GeneralChipKey } from './config'
 
 describe('Chip', () => {
-  const [muiChipSpy, resetMuiChipSpy] = mockAndShallowSpy('@material-ui/core/Chip')
-
-  afterEach(() => {
-    resetMuiChipSpy()
-  })
-
   it.each`
     value
     ${DayOfWeekEnum.Monday}
@@ -27,14 +18,10 @@ describe('Chip', () => {
   `(`should return chips with filled color, $value.label`, async ({ value }: { value: GeneralChipProps['type'] }) => {
     const { GeneralChip } = await import('.')
 
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <GeneralChip type={value} />
-      </ThemeProvider>
-    )
+    const wrapper = shallow(<GeneralChip type={value} />)
 
-    expect(muiChipSpy).toBeCalledTimes(1)
-    expect(muiChipSpy.mock.calls[0][0].variant).not.toBeDefined()
+    expect(wrapper.find(Chip)).toBeDefined()
+    expect(wrapper.find(Chip).prop('variant')).not.toBeDefined()
   })
 
   it.each`
@@ -46,13 +33,9 @@ describe('Chip', () => {
   `(`should return chips with outlined color, $value.label`, async ({ value }: { value: GeneralChipKey }) => {
     const { GeneralChip } = await import('.')
 
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <GeneralChip type={value} />
-      </ThemeProvider>
-    )
+    const wrapper = shallow(<GeneralChip type={value} />)
 
-    expect(muiChipSpy).toBeCalledTimes(1)
-    expect(muiChipSpy.mock.calls[0][0].variant).toEqual('outlined')
+    expect(wrapper.find(Chip)).toBeDefined()
+    expect(wrapper.find(Chip).prop('variant')).toEqual('outlined')
   })
 })

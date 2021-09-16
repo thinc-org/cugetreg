72d7ad18444ca616c4961295ebfbdde2f63a79d8
new5558 <new17353@gmail.com>
2021-09-16 20:11:07 +0700

    fix: eslint warning

--- src/common/testing/mockAndShallowSpy/index.tsx
+++ src/common/testing/mockAndShallowSpy/index.tsx
@@ -1,9 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /**
  * @deprecated please use shallow rendering instead
  */
 export function mockAndShallowSpy(modulePath: string): [jest.Mock<JSX.Element, [props: any]>, () => void] {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const componentSpy = jest.fn((props: any) => {
     const children = props?.children || modulePath
 

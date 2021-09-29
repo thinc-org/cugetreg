d4c9a925b5ebb086c1fb52bfef04d4f2f8b43c67
new5558 <new17353@gmail.com>
2021-09-29 17:44:59 +0700

    fix: yurl error

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,7 @@ module.exports = {
             },
             numberOfRuns: 3,
             url: [
-                "https://127.0.0.1:3000/",
+                "http://127.0.0.1:3000/",
             ]
         },
         // upload: {

1cc151540f8838f68e5189d5719266c80337ec43
new5558 <new17353@gmail.com>
2021-09-29 17:42:36 +0700

    build: run lighthouse ci

---
+++ lighthouserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    ci: {
+        collect: {
+            settings: {
+                chromeFlags: "--ignore-certificate-errors"
+            },
+            numberOfRuns: 3,
+            url: [
+                "https://127.0.0.1:3000/",
+            ]
+        },
+        // upload: {
+        //     target: "lhci",
+        //     serverBaseUrl: "https://lit-ocean-90649.herokuapp.com/",
+        //     token: process.env.LHCI_SERVER_TOKEN
+        // }
+    }
+};

4bdf26927efd7404d6a52e7362ceabd398047e47
new5558 <new17353@gmail.com>
2021-09-29 18:00:29 +0700

    fix: skip http2 audit

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,17 +2,19 @@ module.exports = {
     ci: {
         collect: {
             settings: {
-                chromeFlags: "--ignore-certificate-errors"
+                chromeFlags: "--ignore-certificate-errors",
+                skipAudits: ['uses-http2']
             },
             numberOfRuns: 3,
             url: [
                 "http://127.0.0.1:3000/",
             ]
         },
-        // upload: {
-        //     target: "lhci",
-        //     serverBaseUrl: "https://lit-ocean-90649.herokuapp.com/",
-        //     token: process.env.LHCI_SERVER_TOKEN
-        // }
+        upload: {
+            //     target: "lhci",
+            //     serverBaseUrl: "https://lit-ocean-90649.herokuapp.com/",
+            //     token: process.env.LHCI_SERVER_TOKEN
+            target: 'temporary-public-storage',
+        }
     }
 };

1de18b046d8908da504d7acc4990b0ec8ea152b6
Saenyakorn <saneyakorn@gmail.com>
2021-10-10 17:45:59 +0700

    update: snapshot

--- src/components/PageMeta.tsx
+++ src/components/PageMeta.tsx
@@ -3,5 +3,5 @@ import { NextSeo, NextSeoProps } from 'next-seo'
 interface PageMetaProps extends NextSeoProps {}
 
 export function PageMeta(props: PageMetaProps) {
-  return <NextSeo titleTemplate="%s | CU Get Reg`" {...props} />
+  return <NextSeo titleTemplate="%s | CU Get Reg" {...props} />
 }

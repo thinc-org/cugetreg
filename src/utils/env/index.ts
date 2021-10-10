c36e54d6aa7500e20cc0a3a4f25e9741c9fa3b51
Saenyakorn <saneyakorn@gmail.com>
2021-10-10 16:55:36 +0700

    fix: env name

--- src/utils/env/index.ts
+++ src/utils/env/index.ts
@@ -11,4 +11,4 @@ export const googleauth_coderedirector = process.env.NEXT_PUBLIC_GOOGLEAUTH_CODE
 export const google_tag_manager_container_id = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER_ID || ''
 export const google_optimize = process.env.NEXT_PUBLIC_GOOGLE_OPTIMIZE || ''
 
-export const site_url = process.env.SITE_URL || 'http://localhost:3000'
+export const site_url = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'

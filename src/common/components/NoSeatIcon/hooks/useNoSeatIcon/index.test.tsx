8ebfc5238576f71e755d20d28b7f19f383852417
new5558 <new17353@gmail.com>
2021-10-21 22:34:40 +0700

    refactor: add more robost tets for no seat action

--- src/common/components/NoSeatIcon/hooks/useNoSeatIcon/index.test.tsx
+++ src/common/components/NoSeatIcon/hooks/useNoSeatIcon/index.test.tsx
@@ -2,6 +2,10 @@ import { renderHook, act } from '@testing-library/react-hooks'
 import { MouseEvent } from 'react'
 
 describe('UseNoSeatNotice', () => {
+  const I18N_NAME = 'regWarNotice'
+  const LOG_NAME = 'regwarnotice'
+  const LOG_ARGS = 'see notice'
+
   const mockTranslate = jest.fn()
   const mockUseTranslation = jest.fn(() => ({ t: mockTranslate }))
   const mockLog = jest.fn()
@@ -19,12 +23,12 @@ describe('UseNoSeatNotice', () => {
     const { useNoSeatIcon } = await import('.')
     const { result } = renderHook(() => useNoSeatIcon())
 
-    expect(mockUseTranslation).toBeCalledWith('regWarNotice')
+    expect(mockUseTranslation).toBeCalledWith(I18N_NAME)
     expect(mockUseTranslation).toBeCalledTimes(1)
-    expect(mockUseLog).toBeCalledWith('regwarnotice')
+    expect(mockUseLog).toBeCalledWith(LOG_NAME)
     expect(mockUseLog).toBeCalledTimes(1)
-
     expect(result.current.anchorEl).toBe(null)
+    expect(result.current.open).toBe(false)
 
     const mockElement = ((<div></div>) as any) as Element
     const mockMouseEvent = { currentTarget: mockElement }
@@ -33,14 +37,16 @@ describe('UseNoSeatNotice', () => {
       result.current.onHover(mockMouseEvent as MouseEvent<HTMLButtonElement>)
     })
 
-    expect(mockLog).toBeCalledWith(null, 'see notice')
+    expect(mockLog).toBeCalledWith(null, LOG_ARGS)
     expect(mockLog).toBeCalledTimes(1)
     expect(result.current.anchorEl).toBe(mockElement)
+    expect(result.current.open).toBe(true)
 
     act(() => {
       result.current.onLeave()
     })
 
     expect(result.current.anchorEl).toBe(null)
+    expect(result.current.open).toBe(false)
   })
 })

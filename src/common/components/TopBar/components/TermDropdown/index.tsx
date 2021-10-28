f3e514710ad6ebe176faf3a5fb7e159d88850893
Saenyakorn <saneyakorn@gmail.com>
2021-10-28 11:38:47 +0700

    add analytic to term dropdown

--- src/common/components/TopBar/components/TermDropdown/index.tsx
+++ src/common/components/TopBar/components/TermDropdown/index.tsx
@@ -1,21 +1,20 @@
 import { ChangeEvent } from 'react'
 
+import { TERM_DROPDOWN } from '@/common/context/Analytics/constants'
+import { useLog } from '@/common/context/Analytics/hooks/useLog'
 import { useCourseGroup } from '@/common/hooks/useCourseGroup'
 
 import { ConfigBarSelect } from '../ConfigSelect'
 
-interface TermDropdownProps {
-  log?: (_: unknown, value: string) => void
-}
-
 // TODO: clearify the changing term flow
-export function TermDropdown({ log }: TermDropdownProps) {
+export function TermDropdown() {
   const { academicYear, semester, setTerm } = useCourseGroup()
+  const { log } = useLog(TERM_DROPDOWN)
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const term = e.target.value as string
     setTerm(term)
-    log?.(null, term)
+    log(null, term)
   }
 
   return (

0c616b08c091a1c737ce1d4ed44934fbc0e026bc
new5558 <new17353@gmail.com>
2021-10-30 22:33:14 +0700

    fix: remove useMemo (no need)

--- src/modules/CourseSearch/context/CourseSearch/hooks/useCourseSearchProvider/index.tsx
+++ src/modules/CourseSearch/context/CourseSearch/hooks/useCourseSearchProvider/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 
 import { LIMIT_QUERY_CONSTANT } from '@/modules/CourseSearch/context/CourseSearch/constants'
 import { useSearchCourseQueryParams } from '@/modules/CourseSearch/hooks/useSearchCourseQueryParams'
@@ -14,22 +14,18 @@ export const useCourseSearchProvider = () => {
   const [offset, setOffset] = useState(0)
   const { searchCourseQueryParams } = useSearchCourseQueryParams()
 
-  const variables = useMemo(
-    () => ({
+  const courseSearchQuery = useQuery<SearchCourseResponse, SearchCourseVars>(SEARCH_COURSE, {
+    notifyOnNetworkStatusChange: true,
+    variables: {
       ...searchCourseQueryParams,
       filter: {
         ...searchCourseQueryParams.filter,
         limit: LIMIT_QUERY_CONSTANT,
         offset: 0,
       },
-    }),
-    [searchCourseQueryParams]
-  )
-
-  const courseSearchQuery = useQuery<SearchCourseResponse, SearchCourseVars>(SEARCH_COURSE, {
-    notifyOnNetworkStatusChange: true,
-    variables,
+    },
   })
+
   useEffect(() => {
     courseSearchQuery.refetch()
     // eslint-disable-next-line react-hooks/exhaustive-deps

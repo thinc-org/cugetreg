d9c61ba052b365004155e9a94f8c24b178eb555d
new5558 <new17353@gmail.com>
2021-11-02 04:52:39 +0700

    fix: add proper timeout for puppeteer

--- __e2e__/pilot.ts
+++ __e2e__/pilot.ts
@@ -1,6 +1,7 @@
 import 'expect-puppeteer'
 
 describe('Google', () => {
+    jest.setTimeout(20000)
     beforeAll(async () => {
         await page.goto('https://beta.cugetreg.com')
     })
@@ -13,10 +14,10 @@ describe('Google', () => {
             () => document.querySelectorAll('select')[1].id = 'blank_id'
         );
         await page.select('#blank_id', "2564/1");
+        await page.waitForTimeout(500)
         await expect(page).toMatch('0201172 SELF/CAREER MGT')
 
         await page.click("input[name='หมวดวิทย์']");
-        await page.waitForTimeout(500)
         await expect(page).not.toMatch('0201172 SELF/CAREER MGT')
         await expect(page).toMatch('0201287 MAP APPLN')
 
@@ -36,6 +37,7 @@ describe('Google', () => {
 
         await page.waitForTimeout(500)
         await page.click("input[name='ไม่ใช่ GenEd']");
+        await page.waitForTimeout(500)
         await expect(page).toMatch('2145490 AERO ENG SEM III')
         await expect(page).not.toMatch('0201172 SELF/CAREER MGT')
         await expect(page).not.toMatch('0201287 MAP APPLN')
@@ -45,15 +47,16 @@ describe('Google', () => {
         await page.click("input[name='วันพฤหัสบดี']");
         await page.click("input[name='วันอังคาร']");
         await page.click("input[name='หมวดวิทย์']");
-        await expect(page).not.toMatch('2145290 AERO ENG SEM I')
+        await page.waitForTimeout(500)
+        await expect(page).toMatch('2145490 AERO ENG SEM II')
         await expect(page).not.toMatch('0201172 SELF/CAREER MGT')
         await expect(page).not.toMatch('0201287 MAP APPLN')
         await expect(page).toMatch('2145217 SCI PROG')
 
-
         await expect(page).toClick("input[value='15:30']");
         await expect(page).toClick(`li[data-value="18:00"]`);
-        await expect(page).not.toMatch('2145290 AERO ENG SEM I')
+        await page.waitForTimeout(500)
+        await expect(page).not.toMatch('2145490 AERO ENG SEM III')
         await expect(page).not.toMatch('0201172 SELF/CAREER MGT')
         await expect(page).not.toMatch('0201287 MAP APPLN')
         await expect(page).not.toMatch('2141491 RES METHODO')

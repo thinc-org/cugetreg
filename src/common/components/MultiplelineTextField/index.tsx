569f52c6304d1971c8418d2831ef3a1020a1ecc7
Saenyakorn <saneyakorn@gmail.com>
2021-11-03 00:57:21 +0700

    fix style inject error

--- src/common/components/MultiplelineTextField/index.tsx
+++ src/common/components/MultiplelineTextField/index.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { TextField } from '@material-ui/core'
 
 export const MultiplelineTextField = styled(TextField)`
-  & > *:not(p) {
+  & > div {
     min-height: 120px;
     display: flex;
     align-items: flex-start;

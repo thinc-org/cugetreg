ab90eb8ef72f5318f891da322adacf174e88700b
Anon-136 <anonjin44@gmail.com>
2021-11-05 09:24:27 +0700

    test: rename mock const

--- src/__mock__/overlap.ts
+++ src/__mock__/overlap.ts
@@ -1,5 +1,5 @@
 import { ExamClass } from '@/common/utils/types'
-import { CourseOverlap, ScheduleClass } from '@/modules/Schedule/components/Schedule/utils'
+import { CourseOverlap, CourseOverlapMap, ScheduleClass } from '@/modules/Schedule/components/Schedule/utils'
 
 export const courseTemplate: ScheduleClass = {
   courseNo: '2110316',
@@ -43,70 +43,92 @@ export const noOverlap: CourseOverlap = { hasOverlap: false, classes: [], exams:
  * 200006, 200007 are overlapping twice
  * 200010 are overlapping with each 200008 and 200009 twice
  */
-
-export const mockOverlappingCourses: Record<string, ScheduleClass> = {
-  '200000': { ...courseTemplate, courseNo: '200000' },
-  '200001': { ...courseTemplate, courseNo: '200001' },
-  '200002': { ...courseTemplate, courseNo: '200002' },
-  '200003': { ...courseTemplate, courseNo: '200003', hasOverlap: true, overlaps: ['200004'] },
-  '200004': { ...courseTemplate, courseNo: '200004', hasOverlap: true, overlaps: ['200003'] },
-  '200005': { ...courseTemplate, courseNo: '200005', hasOverlap: true, overlaps: ['200006', '200006'] },
-  '200006': { ...courseTemplate, courseNo: '200006', hasOverlap: true, overlaps: ['200005', '200005'] },
-  '200007': { ...courseTemplate, courseNo: '200007', hasOverlap: true, overlaps: ['200009', '200009'] },
-  '200008': { ...courseTemplate, courseNo: '200008', hasOverlap: true, overlaps: ['200009', '200009'] },
-  '200009': {
+export const mockNonOverlappingCourses: ScheduleClass[] = [
+  { ...courseTemplate, courseNo: '200000' },
+  { ...courseTemplate, courseNo: '200001' },
+  { ...courseTemplate, courseNo: '200002' },
+]
+export const mockTwoOverlappingCourses_1: ScheduleClass[] = [
+  { ...courseTemplate, courseNo: '200003', hasOverlap: true, overlaps: ['200004'] },
+  { ...courseTemplate, courseNo: '200004', hasOverlap: true, overlaps: ['200003'] },
+]
+export const mockTwoOverlappingCourses_2: ScheduleClass[] = [
+  { ...courseTemplate, courseNo: '200005', hasOverlap: true, overlaps: ['200006', '200006'] },
+  { ...courseTemplate, courseNo: '200006', hasOverlap: true, overlaps: ['200005', '200005'] },
+]
+export const mockThreeOverlappingCourses: ScheduleClass[] = [
+  { ...courseTemplate, courseNo: '200007', hasOverlap: true, overlaps: ['200009', '200009'] },
+  { ...courseTemplate, courseNo: '200008', hasOverlap: true, overlaps: ['200009', '200009'] },
+  {
     ...courseTemplate,
     courseNo: '200009',
     hasOverlap: true,
     overlaps: ['200007', '200007', '200008', '200008'],
   },
-}
+]
 
-export const expectedOverlappingCourses: Record<string, CourseOverlap> = {
+export const expectedNonOverlappingCourses: CourseOverlapMap = {
   '200000': { ...noOverlap },
   '200001': { ...noOverlap },
   '200002': { ...noOverlap },
+}
+export const expectedTwoOverlappingCourses_1: CourseOverlapMap = {
   '200003': { ...noOverlap, hasOverlap: true, classes: ['200004'] },
   '200004': { ...noOverlap, hasOverlap: true, classes: ['200003'] },
+}
+export const expectedTwoOverlappingCourses_2: CourseOverlapMap = {
   '200005': { ...noOverlap, hasOverlap: true, classes: ['200006'] },
   '200006': { ...noOverlap, hasOverlap: true, classes: ['200005'] },
+}
+export const expectedThreeOverlappingCourses: CourseOverlapMap = {
   '200007': { ...noOverlap, hasOverlap: true, classes: ['200009'] },
   '200008': { ...noOverlap, hasOverlap: true, classes: ['200009'] },
   '200009': { ...noOverlap, hasOverlap: true, classes: ['200007', '200008'] },
 }
-
 /**
  * 200000, 200001, 200002 are not overlapping
  * 200003, 200004 are overlapping exactly once
  * 200006, 200007 are overlapping twice
  * 200010 are overlapping with each 200008 and 200009 twice
  */
-export const mockOverlappingExams: Record<string, ExamClass> = {
-  '200000': { ...examTemplate, courseNo: '200000' },
-  '200001': { ...examTemplate, courseNo: '200001' },
-  '200002': { ...examTemplate, courseNo: '200002' },
-  '200003': { ...examTemplate, courseNo: '200003', hasOverlap: true, overlaps: ['200004'] },
-  '200004': { ...examTemplate, courseNo: '200004', hasOverlap: true, overlaps: ['200003'] },
-  '200005': { ...examTemplate, courseNo: '200005', hasOverlap: true, overlaps: ['200006', '200006'] },
-  '200006': { ...examTemplate, courseNo: '200006', hasOverlap: true, overlaps: ['200005', '200005'] },
-  '200007': { ...examTemplate, courseNo: '200007', hasOverlap: true, overlaps: ['200009', '200009'] },
-  '200008': { ...examTemplate, courseNo: '200008', hasOverlap: true, overlaps: ['200009', '200009'] },
-  '200009': {
+export const mockNonOverlappingExams: ExamClass[] = [
+  { ...examTemplate, courseNo: '200000' },
+  { ...examTemplate, courseNo: '200001' },
+  { ...examTemplate, courseNo: '200002' },
+]
+export const mockTwoOverlappingExams_1: ExamClass[] = [
+  { ...examTemplate, courseNo: '200003', hasOverlap: true, overlaps: ['200004'] },
+  { ...examTemplate, courseNo: '200004', hasOverlap: true, overlaps: ['200003'] },
+]
+export const mockTwoOverlappingExams_2: ExamClass[] = [
+  { ...examTemplate, courseNo: '200005', hasOverlap: true, overlaps: ['200006', '200006'] },
+  { ...examTemplate, courseNo: '200006', hasOverlap: true, overlaps: ['200005', '200005'] },
+]
+export const mockThreeOverlappingExams: ExamClass[] = [
+  { ...examTemplate, courseNo: '200007', hasOverlap: true, overlaps: ['200009', '200009'] },
+  { ...examTemplate, courseNo: '200008', hasOverlap: true, overlaps: ['200009', '200009'] },
+  {
     ...examTemplate,
     courseNo: '200009',
     hasOverlap: true,
     overlaps: ['200007', '200007', '200008', '200008'],
   },
-}
+]
 
-export const expectedOverlappingBoth: Record<string, CourseOverlap> = {
+export const expectedNonOverlappingClassesAndExams: CourseOverlapMap = {
   '200000': { ...noOverlap },
   '200001': { ...noOverlap },
   '200002': { ...noOverlap },
+}
+export const expectedTwoOverlappingClassesAndExams_1: CourseOverlapMap = {
   '200003': { hasOverlap: true, classes: ['200004'], exams: ['200004'] },
   '200004': { hasOverlap: true, classes: ['200003'], exams: ['200003'] },
+}
+export const expectedTwoOverlappingClassesAndExams_2: CourseOverlapMap = {
   '200005': { hasOverlap: true, classes: ['200006'], exams: ['200006'] },
   '200006': { hasOverlap: true, classes: ['200005'], exams: ['200005'] },
+}
+export const expectedThreeOverlappingClassesAndExams: CourseOverlapMap = {
   '200007': { hasOverlap: true, classes: ['200009'], exams: ['200009'] },
   '200008': { hasOverlap: true, classes: ['200009'], exams: ['200009'] },
   '200009': { hasOverlap: true, classes: ['200007', '200008'], exams: ['200007', '200008'] },
--- src/modules/Schedule/components/Schedule/utils/getOverlappingCourses/index.test.ts
+++ src/modules/Schedule/components/Schedule/utils/getOverlappingCourses/index.test.ts
@@ -1,8 +1,19 @@
 import {
-  expectedOverlappingBoth,
-  expectedOverlappingCourses,
-  mockOverlappingCourses,
-  mockOverlappingExams,
+  expectedNonOverlappingCourses,
+  expectedThreeOverlappingCourses,
+  expectedTwoOverlappingClassesAndExams_1,
+  expectedTwoOverlappingClassesAndExams_2,
+  expectedTwoOverlappingCourses_1,
+  expectedTwoOverlappingCourses_2,
+  mockNonOverlappingCourses,
+  mockNonOverlappingExams,
+  mockThreeOverlappingCourses,
+  mockThreeOverlappingExams,
+  mockTwoOverlappingCourses_1,
+  mockTwoOverlappingCourses_2,
+  mockTwoOverlappingExams_1,
+  mockTwoOverlappingExams_2,
+  expectedThreeOverlappingClassesAndExams,
 } from '@/__mock__/overlap'
 
 import { getOverlappingCourses } from '.'
@@ -14,128 +25,83 @@ describe('getOverlappingCourses', () => {
   })
   test('Course selected exactly once should return no overlap', () => {
     const result = getOverlappingCourses(
-      [mockOverlappingCourses['200000']],
-      [mockOverlappingExams['200000']],
-      [mockOverlappingExams['200000']]
+      [mockNonOverlappingCourses[0]],
+      [mockNonOverlappingExams[0]],
+      [mockNonOverlappingExams[0]]
     )
-    expect(result).toEqual({ '200000': expectedOverlappingCourses['200000'] })
+    expect(result).toEqual({ '200000': expectedNonOverlappingCourses['200000'] })
   })
   test('3 selected courses but not overlapping should return non-overlapping each ', () => {
-    const result = getOverlappingCourses(
-      [mockOverlappingCourses['200000'], mockOverlappingCourses['200001'], mockOverlappingCourses['200002']],
-      [],
-      []
-    )
-    expect(result).toEqual({
-      '200000': expectedOverlappingCourses['200000'],
-      '200001': expectedOverlappingCourses['200001'],
-      '200002': expectedOverlappingCourses['200002'],
-    })
+    const result = getOverlappingCourses(mockNonOverlappingCourses, [], [])
+    expect(result).toEqual(expectedNonOverlappingCourses)
   })
   test('2 classes are overlapping should return overlap for each other', () => {
-    const result = getOverlappingCourses([mockOverlappingCourses['200003'], mockOverlappingCourses['200004']], [], [])
-    expect(result).toEqual({
-      '200003': expectedOverlappingCourses['200003'],
-      '200004': expectedOverlappingCourses['200004'],
-    })
+    const result = getOverlappingCourses(mockTwoOverlappingCourses_1, [], [])
+    expect(result).toEqual(expectedTwoOverlappingCourses_1)
   })
   test('2 classes are overlapping but 1 is not should return overlap and no overlap', () => {
-    const result = getOverlappingCourses(
-      [mockOverlappingCourses['200000'], mockOverlappingCourses['200003'], mockOverlappingCourses['200004']],
-      [],
-      []
-    )
-    expect(result).toEqual({
-      '200000': expectedOverlappingCourses['200000'],
-      '200003': expectedOverlappingCourses['200003'],
-      '200004': expectedOverlappingCourses['200004'],
-    })
+    const result1 = getOverlappingCourses([mockNonOverlappingCourses[0], ...mockNonOverlappingCourses], [], [])
+    const result2 = getOverlappingCourses([...mockNonOverlappingCourses, mockNonOverlappingCourses[0]], [], [])
+    const expectedResult = { '200000': expectedNonOverlappingCourses['200000'], ...expectedNonOverlappingCourses }
+    expect(result1).toEqual(expectedResult)
+    expect(result2).toEqual(expectedResult)
   })
   test('2 classes are overlapping twice should return overlap only once', () => {
-    const result = getOverlappingCourses([mockOverlappingCourses['200005'], mockOverlappingCourses['200006']], [], [])
-    expect(result).toEqual({
-      '200005': expectedOverlappingCourses['200005'],
-      '200006': expectedOverlappingCourses['200006'],
-    })
+    const result = getOverlappingCourses(mockTwoOverlappingCourses_2, [], [])
+    expect(result).toEqual(expectedTwoOverlappingCourses_2)
   })
   test('A class is overlapping with 2 classes twice should return two unique overlaps', () => {
-    const result = getOverlappingCourses(
-      [mockOverlappingCourses['200007'], mockOverlappingCourses['200008'], mockOverlappingCourses['200009']],
-      [],
-      []
-    )
-    expect(result).toEqual({
-      '200007': expectedOverlappingCourses['200007'],
-      '200008': expectedOverlappingCourses['200008'],
-      '200009': expectedOverlappingCourses['200009'],
-    })
+    const result = getOverlappingCourses(mockThreeOverlappingCourses, [], [])
+    expect(result).toEqual(expectedThreeOverlappingCourses)
   })
   test('2 classes and midterm exams are overlapping should return both exam and class overlaps', () => {
-    const result = getOverlappingCourses(
-      [mockOverlappingCourses['200003'], mockOverlappingCourses['200004']],
-      [mockOverlappingExams['200003'], mockOverlappingExams['200004']],
-      []
-    )
-    expect(result).toEqual({
-      '200003': expectedOverlappingBoth['200003'],
-      '200004': expectedOverlappingBoth['200004'],
-    })
+    const result = getOverlappingCourses(mockTwoOverlappingCourses_1, mockTwoOverlappingExams_1, [])
+    expect(result).toEqual(expectedTwoOverlappingClassesAndExams_1)
   })
   test('2 classes and final exams are overlapping return both exam and class overlaps', () => {
-    const result = getOverlappingCourses(
-      [mockOverlappingCourses['200003'], mockOverlappingCourses['200004']],
-      [],
-      [mockOverlappingExams['200003'], mockOverlappingExams['200004']]
-    )
-    expect(result).toEqual({
-      '200003': expectedOverlappingBoth['200003'],
-      '200004': expectedOverlappingBoth['200004'],
-    })
+    const result = getOverlappingCourses(mockTwoOverlappingCourses_1, [], mockTwoOverlappingExams_1)
+    expect(result).toEqual(expectedTwoOverlappingClassesAndExams_1)
   })
   test('2 classes and both midterm and final exams are overlapping should return both exam and class overlaps only once', () => {
     const result = getOverlappingCourses(
-      [mockOverlappingCourses['200003'], mockOverlappingCourses['200004']],
-      [mockOverlappingExams['200003'], mockOverlappingExams['200004']],
-      [mockOverlappingExams['200003'], mockOverlappingExams['200004']]
+      mockTwoOverlappingCourses_1,
+      mockTwoOverlappingExams_1,
+      mockTwoOverlappingExams_1
     )
-    expect(result).toEqual({
-      '200003': expectedOverlappingBoth['200003'],
-      '200004': expectedOverlappingBoth['200004'],
-    })
+    expect(result).toEqual(expectedTwoOverlappingClassesAndExams_1)
   })
   test('2 classes and exams are overlapping but 1 is not should return overlap and no overlap', () => {
-    const result = getOverlappingCourses(
-      [mockOverlappingCourses['200000'], mockOverlappingCourses['200003'], mockOverlappingCourses['200004']],
-      [mockOverlappingExams['200000'], mockOverlappingExams['200003'], mockOverlappingExams['200004']],
-      [mockOverlappingExams['200000'], mockOverlappingExams['200003'], mockOverlappingExams['200004']]
+    const result1 = getOverlappingCourses(
+      [mockNonOverlappingCourses[0], ...mockTwoOverlappingCourses_1],
+      [mockNonOverlappingExams[0], ...mockTwoOverlappingExams_1],
+      [mockNonOverlappingExams[0], ...mockTwoOverlappingExams_1]
     )
-    expect(result).toEqual({
-      '200000': expectedOverlappingBoth['200000'],
-      '200003': expectedOverlappingBoth['200003'],
-      '200004': expectedOverlappingBoth['200004'],
-    })
+    const result2 = getOverlappingCourses(
+      [mockNonOverlappingCourses[0], ...mockTwoOverlappingCourses_1],
+      [...mockTwoOverlappingExams_1, mockNonOverlappingExams[0]],
+      [...mockTwoOverlappingExams_1, mockNonOverlappingExams[0]]
+    )
+    const expectedResult = {
+      '200000': expectedNonOverlappingCourses['200000'],
+      ...expectedTwoOverlappingClassesAndExams_1,
+    }
+    expect(result1).toEqual(expectedResult)
+    expect(result2).toEqual(expectedResult)
   })
   test('2 classes and exams are overlapping twice should return both exam and class overlaps only once', () => {
     const result = getOverlappingCourses(
-      [mockOverlappingCourses['200005'], mockOverlappingCourses['200006']],
-      [mockOverlappingExams['200005'], mockOverlappingExams['200006']],
-      [mockOverlappingExams['200005'], mockOverlappingExams['200006']]
+      mockTwoOverlappingCourses_2,
+      mockTwoOverlappingExams_2,
+      mockTwoOverlappingExams_2
     )
-    expect(result).toEqual({
-      '200005': expectedOverlappingBoth['200005'],
-      '200006': expectedOverlappingBoth['200006'],
-    })
+    expect(result).toEqual(expectedTwoOverlappingClassesAndExams_2)
   })
   test('A class and exam are both overlapping with 2 other classes twice should return two unique both exams and classes', () => {
     const result = getOverlappingCourses(
-      [mockOverlappingCourses['200007'], mockOverlappingCourses['200008'], mockOverlappingCourses['200009']],
-      [mockOverlappingExams['200007'], mockOverlappingExams['200008'], mockOverlappingExams['200009']],
-      [mockOverlappingExams['200007'], mockOverlappingExams['200008'], mockOverlappingExams['200009']]
+      mockThreeOverlappingCourses,
+      mockThreeOverlappingExams,
+      mockThreeOverlappingExams
     )
-    expect(result).toEqual({
-      '200007': expectedOverlappingBoth['200007'],
-      '200008': expectedOverlappingBoth['200008'],
-      '200009': expectedOverlappingBoth['200009'],
-    })
+    expect(result).toEqual(expectedThreeOverlappingClassesAndExams)
   })
 })

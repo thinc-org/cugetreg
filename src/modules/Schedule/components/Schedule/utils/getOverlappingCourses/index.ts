1d099d6e62f81afb94c55f5e03170602be135c72
Anos Posidos <51981658+Anon-136@users.noreply.github.com>
2021-11-05 13:52:15 +0700

    fix: some overlaps warning is missing (#246)
    * fix: overlaps warning is incomplete
    
    * fix: eslint warning
    
    * test: write test for getOverlapping
    
    * test: add notes and change test case format
    
    * test: rename mock const
    
    * test: fix grammar for test name

---
+++ src/modules/Schedule/components/Schedule/utils/getOverlappingCourses/index.ts
@@ -0,0 +1,48 @@
+import { uniq } from 'lodash'
+
+import { ExamClass } from '@/common/utils/types'
+
+import { ScheduleClass, CourseOverlapMap } from '..'
+
+/**
+ * get a course overlap map which tells that for each course (key),
+ * what other courses are overlapping with it in terms of classes and exams (value)
+ * for the purpose of showing overlaps warning
+ * @param classes a list of class which have been checked for overlapping
+ * @param midtermClasses a list of midterm exams which have been checked for overlapping
+ * @param finalClasses a list of final exams which have been checked for overlapping
+ * @returns A record of key `courseNo` and value `CourseOverlap`
+ */
+export function getOverlappingCourses(
+  classes: ScheduleClass[],
+  midtermClasses: ExamClass[],
+  finalClasses: ExamClass[]
+) {
+  const courses: CourseOverlapMap = {}
+  classes.forEach((it) => {
+    courses[it.courseNo] = {
+      hasOverlap: false,
+      classes: [],
+      exams: [],
+    }
+  })
+  classes.forEach((it) => {
+    courses[it.courseNo].classes = [...courses[it.courseNo].classes, ...it.overlaps]
+  })
+  midtermClasses.forEach((it) => {
+    if (it.hasOverlap === true) {
+      courses[it.courseNo].exams = [...courses[it.courseNo].exams, ...it.overlaps]
+    }
+  })
+  finalClasses.forEach((it) => {
+    if (it.hasOverlap === true) {
+      courses[it.courseNo].exams = [...courses[it.courseNo].exams, ...it.overlaps]
+    }
+  })
+  Object.entries(courses).forEach(([, course]) => {
+    course.classes = uniq(course.classes)
+    course.exams = uniq(course.exams)
+    course.hasOverlap = course.classes.length > 0 || course.exams.length > 0
+  })
+  return courses
+}

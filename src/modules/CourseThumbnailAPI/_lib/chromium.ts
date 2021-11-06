dff78a30dfc38837551dca767462a72b6058f365
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-11-06 09:45:35 +0700

    Wait until network idle to take screenshot

--- src/modules/CourseThumbnailAPI/_lib/chromium.ts
+++ src/modules/CourseThumbnailAPI/_lib/chromium.ts
@@ -26,7 +26,7 @@ async function getPage(isDev: boolean) {
 export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
   const page = await getPage(isDev)
   await page.setViewport({ width: 1200, height: 630 })
-  await page.setContent(html)
+  await page.setContent(html, { waitUntil: 'networkidle0' })
   const file = await page.screenshot({ type })
   return file
 }

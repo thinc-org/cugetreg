49c9d8d1b903a95023680b843e39ac088afd9f39
Anon-136 <anonjin44@gmail.com>
2021-11-15 20:52:45 +0700

    fix: rainbow order

--- src/modules/Schedule/components/ColorPicker/constants.ts
+++ src/modules/Schedule/components/ColorPicker/constants.ts
@@ -1,5 +1,15 @@
 export const GENED_COLORS = ['pink', 'secondary', 'purple', 'green'] as const
 export const OTHER_COLORS = ['orange', 'teal', 'blue', 'indigo', 'primary'] as const
-export const SCHEDULE_COLORS = [...GENED_COLORS, ...OTHER_COLORS] as const
+export const SCHEDULE_COLORS = [
+  'pink',
+  'orange',
+  'secondary',
+  'green',
+  'teal',
+  'blue',
+  'indigo',
+  'purple',
+  'primary',
+] as const
 
 export type ScheduleColor = typeof SCHEDULE_COLORS[number]

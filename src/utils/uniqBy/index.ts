3fab19096cf273624d1707327b60cc3c44ec3827
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-12-07 19:42:05 +0700

    Add tests for lodash replacement functions
    These tests are taken from lodash and adjusted for our simpler usage

--- src/utils/uniqBy/index.ts
+++ src/utils/uniqBy/index.ts
@@ -1,5 +1,5 @@
-export function uniqBy<T>(values: T[], classifier: (value: T) => string): T[] {
-  const map = new Map<string, T>()
+export function uniqBy<T, R>(values: T[], classifier: (value: T) => R): T[] {
+  const map = new Map<R, T>()
   for (const value of values) {
     const key = classifier(value)
     if (!map.has(key)) {

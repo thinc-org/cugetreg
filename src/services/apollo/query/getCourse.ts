f3e78450d9f2b6fd9d0c6e9d8628937875f495a2
Saenyakorn <saneyakorn@gmail.com>
2021-12-13 19:22:58 +0700

    delete unknow code

--- src/services/apollo/query/getCourse.ts
+++ src/services/apollo/query/getCourse.ts
@@ -21,35 +21,3 @@ query GetCourseInfo($courseNo: String!, $courseGroup: CourseGroupInput!) {
   }
 }
 `
-
-export type SectionThumbnailData = Pick<Section, 'genEdType' | 'closed' | 'capacity'>
-export type CourseThumbnailData = Pick<
-  Course,
-  'courseNo' | 'abbrName' | 'courseNameTh' | 'courseNameEn' | 'genEdType'
-> & {
-  sections: SectionThumbnailData[]
-}
-
-export interface GetCourseForThumbnailResponse {
-  course: CourseThumbnailData
-}
-
-export const GET_COURSE_FOR_THUMBNAIL = gql`
-  query GetCourseInfo($courseNo: String!, $courseGroup: CourseGroupInput!) {
-    course(courseNo: $courseNo, courseGroup: $courseGroup) {
-      courseNo
-      abbrName
-      courseNameTh
-      courseNameEn
-      genEdType
-      sections {
-        genEdType
-        closed
-        capacity {
-          current
-          max
-        }
-      }
-    }
-  }
-`

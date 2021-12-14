56d05e822ef68458c0796717d0a16ae36c0ea52e
Saenyakorn <saneyakorn@gmail.com>
2021-12-14 16:07:41 +0700

    fix user login after activate loginGuard

--- src/common/utils/loginGuard/index.tsx
+++ src/common/utils/loginGuard/index.tsx
@@ -3,13 +3,15 @@ import { userStore } from '@/store/userStore'
 
 const loginGuard = (onConfirm?: () => void) => {
   if (!userStore.isLoggedIn()) {
-    console.log('loginGuard')
     dialog({
       heading: 'คุณยังไม่ได้เข้าสู่ระบบ',
       content: 'คุณจะสามารถเขียนรีวิว กดไลก์ และรายงานรีวิวของผู้อื่นได้เมื่อเข้าสู่ระบบแล้วเท่านั้น',
       primaryButtonText: 'เข้าสู่ระบบ',
       secondaryButtonText: 'ยกเลิก',
-      onPrimaryButtonClick: onConfirm,
+      onPrimaryButtonClick: () => {
+        onConfirm?.()
+        userStore.login()
+      },
     })
     return false
   }

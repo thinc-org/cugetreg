0835b405f36f553513545535b05a93ac80e651f3
Thanayut T <b@thanayut.in.th>
2022-08-28 00:38:09 +0700

    Fix invalid ul in p tag causing hydration failed

--- apps/cugetreg-web/src/configs/theme/articletext.ts
+++ apps/cugetreg-web/src/configs/theme/articletext.ts
@@ -1,6 +1,11 @@
-import { Typography, styled } from '@mui/material'
+import { Box, Typography, styled } from '@mui/material'
 
 export const StyledArticleBody = styled(Typography)`
+  font-size: 18px;
+  line-height: 32px;
+`
+
+export const StyledArticleBox = styled(Box)`
   font-size: 18px;
   line-height: 32px;
 `

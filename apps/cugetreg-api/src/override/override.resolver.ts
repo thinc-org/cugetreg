06493f654d53c5ae635d435c301edf21c8b4f4f6
Nut Pinyo <bomb.np@gmail.com>
2022-08-30 14:58:43 +0700

    fix: update override schema to match scraper

--- apps/cugetreg-api/src/override/override.resolver.ts
+++ apps/cugetreg-api/src/override/override.resolver.ts
@@ -1,15 +1,23 @@
 import { UseGuards } from '@nestjs/common'
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
 import { StudyProgram } from '@thinc-org/chula-courses'
 
-import { AdminAuthGuard } from '../auth/admin.guard'
-import { OverrideInput } from '../graphql'
+import { AdminAuthGuard } from '@api/auth/admin.guard'
+import { OverrideInput } from '@api/graphql'
+import { Override } from '@api/schemas/override.schema'
+
 import { OverrideService } from './override.service'
 
 @Resolver('Override')
 export class OverrideResolver {
   constructor(private readonly overrideService: OverrideService) {}
+
+  @UseGuards(AdminAuthGuard)
+  @Query('overrides')
+  getOverrides(): Promise<Override[]> {
+    return this.overrideService.getOverrides()
+  }
 
   @UseGuards(AdminAuthGuard)
   @Mutation('createOrUpdateOverride')

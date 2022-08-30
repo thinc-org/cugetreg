3020542c41ffc0723ced44a97ccbdc0e1f9fc44f
samithiwat <samithiwat2544@gmail.com>
2022-08-30 12:35:00 +0700

    Create mock enum list

---
+++ apps/cugetreg-api/src/mocks/common/enums.ts
@@ -0,0 +1,51 @@
+import {
+  ClassType,
+  DayOfWeekEnum,
+  GenEdTypeEnum,
+  StudyProgramEnum,
+} from '@thinc-org/chula-courses'
+
+export const genEdTypes = [
+  GenEdTypeEnum.IN,
+  GenEdTypeEnum.HU,
+  GenEdTypeEnum.NO,
+  GenEdTypeEnum.SC,
+  GenEdTypeEnum.SO,
+]
+
+export const studyPrograms = [
+  StudyProgramEnum.Semester,
+  StudyProgramEnum.Trisemter,
+  StudyProgramEnum.International,
+]
+
+export const dayOfWeeks = [
+  DayOfWeekEnum.Monday,
+  DayOfWeekEnum.Tuesday,
+  DayOfWeekEnum.Wednesday,
+  DayOfWeekEnum.Thursday,
+  DayOfWeekEnum.Friday,
+  DayOfWeekEnum.Saturday,
+  DayOfWeekEnum.Sunday,
+  DayOfWeekEnum.AR,
+  DayOfWeekEnum.IA,
+]
+
+export const classTypes: ClassType[] = [
+  'AR',
+  'CLIN',
+  'DISC',
+  'FWK',
+  'IA',
+  'IDPS',
+  'IDVS',
+  'L/T',
+  'LAB',
+  'LECT',
+  'OTHER',
+  'PRAC',
+  'REFL',
+  'SMNA',
+  'STU',
+  'TUT',
+]

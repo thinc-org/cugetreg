d4444f7683bf0803227d6ab46cec048f9b1afa63
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-10-14 00:06:52 +0700

    fix: apollo in unit test

--- libs/codegen/src/generated/index.tsx
+++ libs/codegen/src/generated/index.tsx
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag';
 import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;

197f9fd12ecea664bdd57560e280381e71b468c0
Pipat Saengow <pay2630@gmail.com>
2023-01-12 01:28:46 +0700

    add log to file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,16 +6,22 @@ import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { WinstonModule } from "nest-winston"
+import * as winston from "winston"
 
 import * as cookieParser from 'cookie-parser'
 
 import { AppModule } from './app/app.module'
 import { Configuration, validateConfig } from './config/configuration'
-import { GelfLogger } from './logger'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: new GelfLogger(),
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({format: winston.format.simple()}),
+        new winston.transports.File({filename: "app.log"}),
+      ]
+    })
   })
 
   const configService = app.get<ConfigService<Configuration>>(ConfigService)

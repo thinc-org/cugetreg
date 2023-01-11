397be6d59cbecb35dc822f952d821c48c5359b99
Pipat Saengow <pay2630@gmail.com>
2023-01-12 01:17:20 +0700

    api:use elastic logging instead of graylog

--- apps/api/src/clientlogging/clientlogging.module.ts
+++ apps/api/src/clientlogging/clientlogging.module.ts
@@ -1,14 +1,31 @@
 import { HttpModule } from '@nestjs/axios'
-import { Module, forwardRef } from '@nestjs/common'
+import { Module, forwardRef, Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Client } from "@opensearch-project/opensearch"
 
 import { AuthModule } from '../auth/auth.module'
 import { ClientLoggingController } from './clientlogging.controller'
 import { ClientLoggingService } from './clientlogging.service'
 
+const elasticProvider: Provider<Client> = {
+  provide: Client,
+  useFactory: (configService: ConfigService) => new Client({
+    node: configService.get("elasticUrl"),
+    auth: {
+      username: configService.get("elasticUsername"),
+      password: configService.get("elasticPassword"),
+    },
+    ssl: {
+      rejectUnauthorized: false,
+    }
+  }),
+  inject: [ConfigService],
+}
+
 @Module({
-  providers: [ClientLoggingService],
+  providers: [ClientLoggingService, elasticProvider],
   controllers: [ClientLoggingController],
-  imports: [HttpModule, forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule)],
   exports: [ClientLoggingService],
 })
 export class ClientLoggingModule {}

024e1d6686d954c7ec585478d863f77974548739
Nut Pinyo <bomb.np@gmail.com>
2023-01-15 20:18:50 +0700

    use winston logger

--- apps/scraper/src/main.ts
+++ apps/scraper/src/main.ts
@@ -2,18 +2,36 @@ import { LogLevel, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
+import { WinstonModule } from 'nest-winston'
+import * as winston from 'winston'
+
 import { Configuration, validateConfig } from '@scraper/config/configuration'
 import { ScraperService } from '@scraper/scraper/scraper.service'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const logLevels: LogLevel[] = ['log', 'error', 'warn']
-  if (process.env.LOG_DEBUG === 'true') {
-    logLevels.push('debug')
-  }
   const app = await NestFactory.create(AppModule, {
-    logger: logLevels,
+    logger: WinstonModule.createLogger({
+      level: process.env.LOG_DEBUG === 'true' ? 'debug' : 'info',
+      handleExceptions: true,
+      handleRejections: true,
+      defaultMeta: {
+        app: 'cgr-scraper',
+      },
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize({ all: true }),
+            winston.format.simple()
+          ),
+        }),
+        new winston.transports.File({
+          filename: 'log/app.log',
+          format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        }),
+      ],
+    }),
   })
   const configService = app.get(ConfigService<Configuration>)
   validateConfig(configService)

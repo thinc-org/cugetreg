0adba28a3ca3b85310172593e4fa46bb04762b20
samithiwat <samithiwat2544@gmail.com>
2023-02-06 18:46:40 +0700

    feat: import elasticsearch module in search module

--- apps/api/src/search/search.module.ts
+++ apps/api/src/search/search.module.ts
@@ -1,8 +1,23 @@
-import { Module } from '@nestjs/common';
-import { SearchService } from './search.service';
-import { SearchResolver } from './search.resolver';
+import { Module } from '@nestjs/common'
+import { SearchService } from './search.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ElasticsearchModule } from '@nestjs/elasticsearch'
 
 @Module({
-  providers: [SearchResolver, SearchService]
+  imports: [
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get<string>('elasticUrl'),
+        auth: {
+          username: configService.get<string>('elasticUsername'),
+          password: configService.get<string>('elasticPassword'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [SearchService],
+  exports: [SearchService],
 })
 export class SearchModule {}

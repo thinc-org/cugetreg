81f608f42907ee8ddf4ba608288460bc37de299c
samithiwat <samithiwat2544@gmail.com>
2023-02-06 18:45:32 +0700

    chore(repo): remove unused files

--- apps/api/src/search/dto/create-search.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
-
-@InputType()
-export class CreateSearchInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}

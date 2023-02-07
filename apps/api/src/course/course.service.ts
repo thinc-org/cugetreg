628ffcaaeb8cec9dfec14d14109dad797bec6661
samithiwat <samithiwat2544@gmail.com>
2023-02-07 17:45:45 +0700

    chore: remove unused import

--- apps/api/src/course/course.service.ts
+++ apps/api/src/course/course.service.ts
@@ -1,26 +1,15 @@
-import { UserInputError } from '@nestjs/apollo'
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 
-import { DayOfWeek, GenEdType, Semester, StudyProgram } from '@thinc-org/chula-courses'
+import { Semester, StudyProgram } from '@thinc-org/chula-courses'
 import { Model } from 'mongoose'
 
 import { Course } from '../common/types/course.type'
 import { CourseDocument } from '../schemas/course.schema'
-import { QueryDslQueryContainer } from '@elastic/elasticsearch/lib/api/types'
-import { Period } from '@api/graphql'
-import { SearchService } from '@api/search/search.service'
-import { ConfigService } from '@nestjs/config'
-import { isTime } from '@api/util/functions'
-import { ICourseSearchDocument, ICourseSearchFilter } from './interface/course.interface'
 
 @Injectable()
 export class CourseService {
-  constructor(
-    @InjectModel('course') private courseModel: Model<CourseDocument>,
-    private readonly configService: ConfigService,
-    private readonly searchService: SearchService
-  ) {}
+  constructor(@InjectModel('course') private courseModel: Model<CourseDocument>) {}
 
   async findOne(
     courseNo: string,

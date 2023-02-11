7448cf4807c2820659c183aa05df35bfa026ea10
Joo <vijak13019@hotmail.com>
2023-02-11 09:10:11 +0700

    refactor: migrate approvedReviews to its own module

--- apps/admin-web/src/pages/approvedReviews/index.tsx
+++ apps/admin-web/src/pages/approvedReviews/index.tsx
@@ -1,6 +1,1 @@
-import { useGetReviewsQuery } from '@cgr/codegen'
-
-// Todo: get data from api
-export default function ApprovedReviewsPage() {
-  return <h1>Hello</h1>
-}
+export { ApprovedReviewsPage as default } from '@admin-web/module/approvedReviews'

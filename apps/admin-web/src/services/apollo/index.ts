68e806876a675449a2faa17703166dcf14c7edd8
Joo <vijak13019@hotmail.com>
2023-02-11 15:15:52 +0700

    refactor(apollo): remove course search cache from apollo client

--- apps/admin-web/src/services/apollo/index.ts
+++ apps/admin-web/src/services/apollo/index.ts
@@ -4,40 +4,6 @@ import { setContext } from '@apollo/client/link/context'
 
 import { ENVIRONMENT, ADMIN_ACCESS_TOKEN } from '@admin-web/env'
 import { apiUrl } from '@admin-web/services/httpClient'
-// import { uniqBy } from '@admin-web/utils/uniqBy'
-
-import { Course, SearchCourseQueryVariables } from '@cgr/codegen'
-
-const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        search: {
-          keyArgs: (args) => {
-            let hash = 'Search'
-            if (!args) return hash
-            const a = args as SearchCourseQueryVariables
-            hash += ':' + a.courseGroup.academicYear
-            hash += ':' + a.courseGroup.semester
-            hash += ':' + a.courseGroup.studyProgram
-            const filter = a.filter
-            hash += ':' + (filter.dayOfWeeks ? JSON.stringify(filter.dayOfWeeks) : '')
-            hash += ':' + (filter.genEdTypes ? JSON.stringify(filter.genEdTypes) : '')
-            hash += ':' + (filter.periodRange ? JSON.stringify(filter.periodRange) : '')
-            hash += ':' + (filter.keyword || '')
-            return hash
-          },
-          read(existing) {
-            return existing
-          },
-          // merge(existing: Course[] = [], incoming: Course[]) {
-          //   return uniqBy([...existing, ...incoming], (course) => course.courseNo)
-          // },
-        },
-      },
-    },
-  },
-})
 
 const createHttpLink = () =>
   new BatchHttpLink({
@@ -46,7 +12,6 @@ const createHttpLink = () =>
 
 const authLink = setContext(async (_, { headers }) => {
   const accessToken = ADMIN_ACCESS_TOKEN
-  console.log(`Access token: ${accessToken}`)
   return {
     headers: {
       ...headers,
@@ -57,7 +22,7 @@ const authLink = setContext(async (_, { headers }) => {
 
 export const client = new ApolloClient({
   link: authLink.concat(createHttpLink()),
-  cache: cache,
+  cache: new InMemoryCache(),
   connectToDevTools: ENVIRONMENT !== 'production',
 })
 

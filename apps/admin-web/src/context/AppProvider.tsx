cc4185dec91dbe691229d3802a7809d73d4aa8ed
Khajornritdadcha <vijak13019@hotmail.com>
2023-02-12 08:12:18 +0700

    feat: add themeProvider

--- apps/admin-web/src/context/AppProvider.tsx
+++ apps/admin-web/src/context/AppProvider.tsx
@@ -1,15 +1,20 @@
 import { ApolloProvider } from '@apollo/client'
 import { client } from '@admin-web/services/apollo'
+import { ThemeProvider } from '@mui/material'
+import { defaultTheme } from '@admin-web/config/theme/index'
 import Topbar from '@admin-web/common/Topbar/Topbar'
 
 interface AppProviderProps {
   children: React.ReactNode
 }
+
 export function AppProvider({ children }: AppProviderProps) {
   return (
     <ApolloProvider client={client}>
-      <Topbar />
-      {children}
+      <ThemeProvider theme={defaultTheme}>
+        <Topbar />
+        {children}
+      </ThemeProvider>
     </ApolloProvider>
   )
 }

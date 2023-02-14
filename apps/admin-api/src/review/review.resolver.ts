ac6f65240954dd06896bd0ba2b8e11fbeae2a9ef
nac <chotpisit080916c@gmail.com>
2023-02-14 21:05:25 +0700

    feat(admin-api): add get-all-reviews query

--- apps/admin-api/src/review/review.resolver.ts
+++ apps/admin-api/src/review/review.resolver.ts
@@ -1,3 +1,4 @@
+import { ReviewDocument } from '@admin-api/schemas/review.schema'
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
 import { Review, ReviewStatus } from '../graphql'
@@ -6,6 +7,11 @@ import { ReviewService } from './review.service'
 @Resolver('Review')
 export class ReviewResolver {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @Query('reviews')
+  async getReviews(): Promise<ReviewDocument[]> {
+    return this.reviewService.getReviews()
+  }
 
   // @UseGuards(AdminAuthGuard)
   @Query('pendingReviews')

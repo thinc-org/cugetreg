86d9fa119f901217a7555761e5f75e9b1473bbae
nac <chotpisit080916c@gmail.com>
2023-02-16 16:55:39 +0700

    add enum graphql

--- apps/admin-api/src/graphql.ts
+++ apps/admin-api/src/graphql.ts
@@ -1,3 +1,4 @@
+
 /** ------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
  * -------------------------------------------------------
@@ -6,89 +7,89 @@
 /* tslint:disable */
 /* eslint-disable */
 export enum DayOfWeek {
-  MO = 'MO',
-  TU = 'TU',
-  WE = 'WE',
-  TH = 'TH',
-  FR = 'FR',
-  SA = 'SA',
-  SU = 'SU',
-  IA = 'IA',
-  AR = 'AR',
+    MO = "MO",
+    TU = "TU",
+    WE = "WE",
+    TH = "TH",
+    FR = "FR",
+    SA = "SA",
+    SU = "SU",
+    IA = "IA",
+    AR = "AR"
 }
 
 export enum StudyProgram {
-  S = 'S',
-  T = 'T',
-  I = 'I',
+    S = "S",
+    T = "T",
+    I = "I"
 }
 
 export enum GenEdType {
-  SO = 'SO',
-  HU = 'HU',
-  SC = 'SC',
-  IN = 'IN',
-  NO = 'NO',
+    SO = "SO",
+    HU = "HU",
+    SC = "SC",
+    IN = "IN",
+    NO = "NO"
 }
 
 export enum ReviewInteractionType {
-  L = 'L',
-  D = 'D',
+    L = "L",
+    D = "D"
 }
 
 export enum ReviewStatus {
-  APPROVED = 'APPROVED',
-  REJECTED = 'REJECTED',
-  PENDING = 'PENDING',
+    APPROVED = "APPROVED",
+    REJECTED = "REJECTED",
+    PENDING = "PENDING"
 }
 
 export class PeriodRangeInput {
-  start: string
-  end: string
+    start: string;
+    end: string;
 }
 
 export class FilterInput {
-  keyword?: string
-  genEdTypes?: GenEdType[]
-  dayOfWeeks?: DayOfWeek[]
-  periodRange?: PeriodRangeInput
-  limit?: number
-  offset?: number
+    keyword?: string;
+    genEdTypes?: GenEdType[];
+    dayOfWeeks?: DayOfWeek[];
+    periodRange?: PeriodRangeInput;
+    limit?: number;
+    offset?: number;
 }
 
 export class CourseGroupInput {
-  semester: string
-  academicYear: string
-  studyProgram: StudyProgram
+    semester: string;
+    academicYear: string;
+    studyProgram: StudyProgram;
 }
 
 export class GenEdOverrideInput {
-  genEdType: GenEdType
-  sections: string[]
+    genEdType: GenEdType;
+    sections: string[];
 }
 
 export class OverrideInput {
-  courseNo: string
-  studyProgram: StudyProgram
-  semester: string
-  academicYear: string
-  genEd?: GenEdOverrideInput
+    courseNo: string;
+    studyProgram: StudyProgram;
+    semester: string;
+    academicYear: string;
+    genEd?: GenEdOverrideInput;
 }
 
 export class CreateReviewInput {
-  rating: number
-  courseNo: string
-  semester: string
-  academicYear: string
-  studyProgram: StudyProgram
-  content?: string
+    rating: number;
+    courseNo: string;
+    semester: string;
+    academicYear: string;
+    studyProgram: StudyProgram;
+    content?: string;
 }
 
 export class EditReviewInput {
-  rating?: number
-  semester?: string
-  academicYear?: string
-  content?: string
+    rating?: number;
+    semester?: string;
+    academicYear?: string;
+    content?: string;
 }
 
 export class Period {
@@ -96,61 +97,17 @@ export class Period {
   end: string
 }
 
-export class ExamPeriod {
-  date: string
-  period: Period
-}
+    abstract course(courseNo: string, courseGroup: CourseGroupInput): Course | Promise<Course>;
 
-export class Capacity {
-  current: number
-  max: number
-}
+    abstract search(filter: FilterInput, courseGroup: CourseGroupInput): Course[] | Promise<Course[]>;
 
-export class Class {
-  type: string
-  dayOfWeek?: DayOfWeek
-  period?: Period
-  building?: string
-  room?: string
-  teachers: string[]
-}
+    abstract overrides(): Override[] | Promise<Override[]>;
 
-export class Section {
-  sectionNo: string
-  closed: boolean
-  capacity: Capacity
-  note?: string
-  classes: Class[]
-  genEdType: GenEdType
-}
+    abstract reviews(courseNo: string, studyProgram: StudyProgram): Review[] | Promise<Review[]>;
 
-export class Course {
-  studyProgram: StudyProgram
-  semester: string
-  academicYear: string
-  courseNo: string
-  courseDescTh?: string
-  courseDescEn?: string
-  abbrName: string
-  courseNameTh: string
-  courseNameEn: string
-  faculty: string
-  department: string
-  credit: number
-  creditHours: string
-  courseCondition: string
-  genEdType: GenEdType
-  midterm?: ExamPeriod
-  final?: ExamPeriod
-  sections: Section[]
-  rating?: string
-  courseDesc?: string
-}
+    abstract myPendingReviews(courseNo: string, studyProgram: StudyProgram): Review[] | Promise<Review[]>;
 
-export class CourseNosOutput {
-  S: string[]
-  T: string[]
-  I: string[]
+    abstract pendingReviews(): Review[] | Promise<Review[]>;
 }
 
 export abstract class IQuery {
@@ -173,36 +130,30 @@ export abstract class IQuery {
 }
 
 export class GenEdOverride {
-  genEdType: GenEdType
-  sections: string[]
+    genEdType: GenEdType;
+    sections: string[];
 }
 
 export class Override {
-  courseNo: string
-  studyProgram: StudyProgram
-  semester: string
-  academicYear: string
-  genEd?: GenEdOverride
+    courseNo: string;
+    studyProgram: StudyProgram;
+    semester: string;
+    academicYear: string;
+    genEd?: GenEdOverride;
 }
 
 export abstract class IMutation {
-  abstract createOrUpdateOverride(override: OverrideInput): Override | Promise<Override>
+    abstract createOrUpdateOverride(override: OverrideInput): Override | Promise<Override>;
 
-  abstract deleteOverride(
-    courseNo: string,
-    courseGroup: CourseGroupInput
-  ): Override | Promise<Override>
+    abstract deleteOverride(courseNo: string, courseGroup: CourseGroupInput): Override | Promise<Override>;
 
-  abstract createReview(createReviewInput: CreateReviewInput): Review | Promise<Review>
+    abstract createReview(createReviewInput: CreateReviewInput): Review | Promise<Review>;
 
-  abstract removeReview(reviewId: string): Review | Promise<Review>
+    abstract removeReview(reviewId: string): Review | Promise<Review>;
 
-  abstract editMyReview(reviewId: string, review: EditReviewInput): Review | Promise<Review>
+    abstract editMyReview(reviewId: string, review: EditReviewInput): Review | Promise<Review>;
 
-  abstract setReviewInteraction(
-    reviewId: string,
-    interactionType: ReviewInteractionType
-  ): Review | Promise<Review>
+    abstract setReviewInteraction(reviewId: string, interactionType: ReviewInteractionType): Review | Promise<Review>;
 
   abstract setReviewStatus(
     reviewId: string,
@@ -212,19 +163,19 @@ export abstract class IMutation {
 }
 
 export class Review {
-  _id: string
-  rating: number
-  courseNo: string
-  semester: string
-  academicYear: string
-  studyProgram: StudyProgram
-  content?: string
-  likeCount: number
-  dislikeCount: number
-  myInteraction?: ReviewInteractionType
-  status?: ReviewStatus
-  rejectionReason?: string
-  isOwner: boolean
+    _id: string;
+    rating: number;
+    courseNo: string;
+    semester: string;
+    academicYear: string;
+    studyProgram: StudyProgram;
+    content?: string;
+    likeCount: number;
+    dislikeCount: number;
+    myInteraction?: ReviewInteractionType;
+    status?: ReviewStatus;
+    rejectionReason?: string;
+    isOwner: boolean;
 }
 
 /** ------------------------------------------------------

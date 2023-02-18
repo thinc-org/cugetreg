ccd945b99df61152b9fac64b0ff4fbaa5a21ff11
nac <chotpisit080916c@gmail.com>
2023-02-19 00:03:31 +0700

    fix env and config

--- apps/admin-api/src/config/configuration.ts
+++ apps/admin-api/src/config/configuration.ts
@@ -7,11 +7,10 @@ export interface Configuration {
   origin: string
   googleOAuthId: string
   googleOAuthSecret: string
-  backendPublicUrl: string
   jwtSecret: string
   adminToken: string
   clientLoggerUrl: string
-  slackWebhookUrl: string
+  dashBoardUrl: string
   env: string
 }
 
@@ -22,22 +21,15 @@ export const configuration = (): Configuration => {
     origin: process.env.CORS_ORIGIN || '*',
     googleOAuthId: process.env.GOOGLE_OAUTH_ID,
     googleOAuthSecret: process.env.GOOGLE_OAUTH_SECRET,
-    backendPublicUrl: process.env.BACKEND_PUBLIC_URL,
     jwtSecret: process.env.JWT_SECRET,
     adminToken: process.env.ADMIN_TOKEN,
     clientLoggerUrl: process.env.CLIENT_LOGGER_URL,
-    slackWebhookUrl: process.env.SLACK_WEBHOOK_URL,
+    dashBoardUrl: process.env.DASHBOARD_URL,
     env: process.env.ENV || 'development',
   }
 }
 
-const requiredConfigs = [
-  'googleOAuthId',
-  'googleOAuthSecret',
-  'jwtSecret',
-  'adminToken',
-  'backendPublicUrl',
-]
+const requiredConfigs = ['googleOAuthId', 'googleOAuthSecret', 'jwtSecret', 'adminToken']
 
 export function validateConfig(configService: ConfigService<Configuration>): void {
   const logger = new Logger('ConfigService')

d71868fe9cde69dfc92357559e7cbe47fb962a1d
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-02-19 10:47:38 +0700

    chore: strong type prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
-module.exports = {
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
   singleQuote: true,
   semi: false,
   printWidth: 100,
   tabWidth: 2,
   trailingComma: 'es5',
 }
+
+module.exports = config

21122935616b92007d032ab71f3bcf57dffffb86
Pipat Saengow <pay2630@gmail.com>
2023-03-22 17:20:51 +0700

    Normalize stack field in log entry (#558)
    * stringify stacktrace
    
    * add changeset
    
    * format
    
    * rename customlogger to logger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,33 +8,14 @@ import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 
 import cookieParser from 'cookie-parser'
-import { WinstonModule } from 'nest-winston'
-import * as winston from 'winston'
 
 import { AppModule } from './app/app.module'
 import { Configuration, validateConfig } from './config/configuration'
+import { createLogger } from './util/logger'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: WinstonModule.createLogger({
-      handleExceptions: true,
-      handleRejections: true,
-      defaultMeta: {
-        app: 'cgr-api',
-      },
-      transports: [
-        new winston.transports.Console({
-          format: winston.format.combine(
-            winston.format.colorize({ all: true }),
-            winston.format.simple()
-          ),
-        }),
-        new winston.transports.File({
-          filename: 'log/app.log',
-          format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-        }),
-      ],
-    }),
+    logger: createLogger(),
   })
 
   const configService = app.get<ConfigService<Configuration>>(ConfigService)

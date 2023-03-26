70ddcac4851591272b67d7a6cf2498bb45afdbf8
Chanatip Pattanapen <lovels2906@gmail.com>
2023-03-27 01:39:24 +0700

    feat: upper Review page

--- apps/admin-web/src/module/pendingReviews/components/pendingReviewList.tsx
+++ apps/admin-web/src/module/pendingReviews/components/pendingReviewList.tsx
@@ -24,49 +24,52 @@ export default function PendingReviewsList() {
 
           <Grid item>
             <Grid container gap={3}>
-              <Grid item>x</Grid>
-              <Grid item>y</Grid>
-              <Grid item>z</Grid>
+              <Grid item>
+                <FormControl
+                  size="small"
+                  sx={{
+                    width: 120,
+                    height: 40,
+                  }}
+                >
+                  <InputLabel>Year</InputLabel>
+                  <Select label="Year">
+                    <MenuItem value={2564}>2564</MenuItem>
+                    <MenuItem value={2565}>2565</MenuItem>
+                    <MenuItem value={2566}>2566</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <FormControl
+                  size="small"
+                  sx={{
+                    width: 120,
+                    height: 40,
+                    //marginLeft: 3,
+                  }}
+                >
+                  <InputLabel>Sem</InputLabel>
+                  <Select label="Semester">
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="outlined-basic"
+                  label="Search"
+                  variant="outlined"
+                  size="small"
+                  sx={{
+                    width: 240,
+                    height: 40,
+                    // marginLeft: 3,
+                  }}
+                />
+              </Grid>
             </Grid>
-            {/* <FormControl
-              size="small"
-              sx={{
-                width: 120,
-                height: 40,
-              }}
-            >
-              <InputLabel>Year</InputLabel>
-              <Select label="Year">
-                <MenuItem value={2564}>2564</MenuItem>
-                <MenuItem value={2565}>2565</MenuItem>
-                <MenuItem value={2566}>2566</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl
-              size="small"
-              sx={{
-                width: 120,
-                height: 40,
-                //marginLeft: 3,
-              }}
-            >
-              <InputLabel>Sem</InputLabel>
-              <Select label="Semester">
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField
-              id="outlined-basic"
-              label="Search"
-              variant="outlined"
-              size="small"
-              sx={{
-                width: 240,
-                height: 40,
-                // marginLeft: 3,
-              }}
-            /> */}
           </Grid>
         </Grid>
 

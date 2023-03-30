760f9aa92a6d24be0f81c020cbb02495e416843c
vijak khajornritdacha <vijak13019@hotmail.com>
2023-03-30 19:17:57 +0700

    feat: add login page and generate token page

---
+++ apps/admin-web/src/pages/login.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router'
+import path from 'path'
+import { useEffect } from 'react'
+
+export default function LoginPage() {
+  const router = useRouter()
+  useEffect(() => {
+    const auth_url = 'https://auth.internal.cugetreg.com/application/o/authorize/?'
+    const response_type = 'code'
+    const client_id = process.env.NEXT_PUBLIC_CLIENT_ID || ''
+    const scope = 'openid+profile+email'
+    const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI || ''
+    const params = new URLSearchParams({ response_type, client_id, scope, redirect_uri })
+    const url = auth_url + params
+    console.log(url)
+    router.push(url)
+  }, [])
+}

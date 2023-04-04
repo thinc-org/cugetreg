054d716f7bd54eb5e7cf9783b954ae02c378a35c
Chanatip Pattanapen <lovels2906@gmail.com>
2023-04-05 00:37:24 +0700

    fix: update design to final WIP

--- apps/admin-web/src/module/pendingReviews/components/pendingReviewList.tsx
+++ apps/admin-web/src/module/pendingReviews/components/pendingReviewList.tsx
@@ -6,6 +6,7 @@ import {
   TableHead,
   TableRow,
   TableBody,
+  Stack,
 } from '@mui/material'
 import { useGetPendingReviewsQuery } from '@cgr/codegen'
 import SinglePendingReview from './singlePendingReview'
@@ -16,45 +17,21 @@ export default function PendingReviewsList() {
   console.log(reviewQuery.loading)
   return (
     <>
-      <Container>
+      <Container disableGutters>
         <PendingReviewHeader />
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ padding: '24px' }}>
-                <Typography fontWeight={700}>CourseNo.</Typography>
-              </TableCell>
-              <TableCell sx={{ padding: '24px' }}>
-                <Typography fontWeight={700}>Year</Typography>
-              </TableCell>
-              <TableCell sx={{ padding: '24px' }}>
-                <Typography fontWeight={700}>Semester</Typography>
-              </TableCell>
-              <TableCell sx={{ padding: '24px' }}>
-                <Typography fontWeight={700}>Star</Typography>
-              </TableCell>
-              <TableCell sx={{ width: '100%', padding: '24px' }}></TableCell>
-              <TableCell sx={{ padding: '24px' }}>
-                <Typography fontWeight={700}>Approval</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {reviewQuery.loading
-              ? // Todo: Reduce gaps between each skeleton
-                // Todo: Add more skeleton (dynamically?)
-                // <Grid spacing={0} direction="column">
-                //   <Skeleton height={200} />
-                //   <Skeleton height={200} />
-                //   <Skeleton height={200} />
-                // </Grid>
-                // TODO: THiS Cause ERROR
-                null
-              : reviewQuery.data?.pendingReviews.map((data) => (
-                  <SinglePendingReview key={data._id} data={data} />
-                ))}
-          </TableBody>
-        </Table>
+        {reviewQuery.loading
+          ? // Todo: Reduce gaps between each skeleton
+            // Todo: Add more skeleton (dynamically?)
+            // <Grid spacing={0} direction="column">
+            //   <Skeleton height={200} />
+            //   <Skeleton height={200} />
+            //   <Skeleton height={200} />
+            // </Grid>
+            // TODO: THiS Cause ERROR
+            null
+          : reviewQuery.data?.pendingReviews.map((data) => (
+              <SinglePendingReview key={data._id} data={data} />
+            ))}
       </Container>
     </>
   )

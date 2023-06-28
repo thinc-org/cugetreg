f886c4a8dd92a962224bf6f4af3432eaebc49b28
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-06-28 23:30:46 +0700

    chore: update code

--- apps/web/src/common/utils/getCurrentTerm/index.ts
+++ apps/web/src/common/utils/getCurrentTerm/index.ts
@@ -1,8 +1,13 @@
 import { tempHardCodedCurrentTerm } from '@web/common/constants/terms'
 import { Term } from '@web/common/types/term'
 
+const { academicYear, semester } = tempHardCodedCurrentTerm
+
 export function getCurrentTerm(): Term {
   // TODO: dynamic year and semester
   // Must sync with SearchCourse's SSR
-  return tempHardCodedCurrentTerm
+  return {
+    academicYear,
+    semester,
+  }
 }

c7cca7cc02e8eb2cf6083ee5ca80014d78ae20c9
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-09-12 13:49:59 +0700

    feat(web): not reg chula warning

--- apps/web/src/common/components/TopBar/components/AnnouncementBar/announcements.ts
+++ apps/web/src/common/components/TopBar/components/AnnouncementBar/announcements.ts
@@ -1,9 +1,20 @@
-import { AnnoucementItem } from '@web/common/components/TopBar/components/AnnouncementBar/types'
+import { AnnouncementItem } from '@web/common/components/TopBar/components/AnnouncementBar/types'
 
-// TODO Admin should b able to change the annoucement
-export function getCurrentAnnoucement(): AnnoucementItem {
+// TODO Admin should be able to change the annoucement
+
+export function getAnnouncement1(): AnnouncementItem {
   return {
     id: 'annoucement_1',
     label: 'CU Get Reg เป็นเว็บไซต์ที่จัดทำโดยนิสิต ซึ่งได้รับการสนับสนุนเซิฟเวอร์จาก Reg Chula',
+    severity: 'info',
+  }
+}
+
+export function getNotRegChulaWarning(): AnnouncementItem {
+  return {
+    id: 'not_reg_chula',
+    label:
+      'CU Get Reg เป็นเพียงเครื่องมือที่ช่วยให้การวางแผนลงทะเบียนเรียนง่ายขึ้น แต่ไม่ใช่การลงทะเบียนเรียนจริง คุณสามารถลงทะเบียนเรียนได้ที่ https://www2.reg.chula.ac.th/ เพียงช่องทางเดียวเท่านั้น',
+    severity: 'warning',
   }
 }

29cdb81955658d226411f9b58881a0e9d5fb2e4c
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2024-06-03 00:32:30 +0700

    chore: setup ui

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import { MyCounterButton } from '@repo/ui';
+	import { Button } from '@repo/ui/button';
 </script>
 
 <h1>Web</h1>
-<MyCounterButton />
+
+<Button>Button</Button>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

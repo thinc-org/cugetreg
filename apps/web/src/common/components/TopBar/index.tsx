90eecbefa8c02a122c8dbe4574d416b2a1c6709e
Nutthapat Pongtanyavichai <59821765+leomotors@users.noreply.github.com>
2024-06-09 09:07:39 +0700

    fix: build and lint error

--- apps/web/src/common/components/TopBar/index.tsx
+++ apps/web/src/common/components/TopBar/index.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from 'next/router'
 
 import { AnnouncementBar } from './components/AnnouncementBar'
-import { getAnnouncement1, getNotRegChulaWarning, term67Issue } from './components/AnnouncementBar/announcements'
+import {
+  getAnnouncement1,
+  getNotRegChulaWarning,
+  term67Issue,
+} from './components/AnnouncementBar/announcements'
 import { ConfigBar } from './components/ConfigBar'
 import { ConfigBarLayout } from './components/ConfigBar/styled'
 import { NavBar } from './components/NavBar'

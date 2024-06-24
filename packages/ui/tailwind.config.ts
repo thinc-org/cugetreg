2f0ab2189d3de9ff1a5f9b16f4f09c0ed8f885ab
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-06-24 12:12:31 +0700

    feat: update color design token

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -18,70 +18,59 @@ export const config: Config = {
     },
     extend: {
       colors: {
-        border: 'hsl(var(--border) / <alpha-value>)',
-        input: 'hsl(var(--input) / <alpha-value>)',
-        ring: 'hsl(var(--ring) / <alpha-value>)',
-        background: 'hsl(var(--background) / <alpha-value>)',
-        foreground: 'hsl(var(--foreground) / <alpha-value>)',
         primary: {
           DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
           hover: 'hsl(var(--primary-hover) / <alpha-value>)',
+          low: {
+            DEFAULT: 'hsl(var(--primary-low) / <alpha-value>)',
+            hover: 'hsl(var(--primary-low-hover) / <alpha-value>)',
+          },
           container: 'hsl(var(--primary-container) / <alpha-value>)',
-          foreground: 'hsl(var(--primary-foreground) / <alpha-value>)',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
-          container: 'hsl(var(--secondary-container) / <alpha-value>)',
           hover: 'hsl(var(--secondary-hover) / <alpha-value>)',
-          foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)',
+          container: 'hsl(var(--secondary-container) / <alpha-value>)',
         },
         error: {
           DEFAULT: 'hsl(var(--error) / <alpha-value>)',
           hover: 'hsl(var(--error-hover) / <alpha-value>)',
           container: 'hsl(var(--error-container) / <alpha-value>)',
         },
-        success: {
-          DEFAULT: 'hsl(var(--success) / <alpha-value>)',
-          hover: 'hsl(var(--success-hover) / <alpha-value>)',
-          container: 'hsl(var(--success-container) / <alpha-value>)',
-        },
         warning: {
           DEFAULT: 'hsl(var(--warning) / <alpha-value>)',
           hover: 'hsl(var(--warning-hover) / <alpha-value>)',
           container: 'hsl(var(--warning-container) / <alpha-value>)',
         },
+        success: {
+          DEFAULT: 'hsl(var(--success) / <alpha-value>)',
+          hover: 'hsl(var(--success-hover) / <alpha-value>)',
+          container: 'hsl(var(--success-container) / <alpha-value>)',
+        },
         surface: {
+          DEFAULT: 'hsl(var(--surface) / <alpha-value>)',
           dim: 'hsl(var(--surface-dim) / <alpha-value>)',
           bright: 'hsl(var(--surface-bright) / <alpha-value>)',
-          container: 'hsl(var(--surface-container) / <alpha-value>)',
-          'container-low': 'hsl(var(--surface-container-low) / <alpha-value>)',
-          'container-lowest':
-            'hsl(var(--surface-container-lowest) / <alpha-value>)',
-          'container-high':
-            'hsl(var(--surface-container-high) / <alpha-value>)',
-          'container-highest':
-            'hsl(var(--surface-container-highest) / <alpha-value>)',
+          container: {
+            DEFAULT: 'hsl(var(--surface-container) / <alpha-value>)',
+            low: 'hsl(var(--surface-container-low) / <alpha-value>)',
+            lowest: 'hsl(var(--surface-container-lowest) / <alpha-value>)',
+            high: 'hsl(var(--surface-container-high) / <alpha-value>)',
+            highest: 'hsl(var(--surface-container-highest) / <alpha-value>)',
+          },
         },
-        muted: {
-          DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
-          foreground: 'hsl(var(--muted-foreground) / <alpha-value>)',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
-          foreground: 'hsl(var(--accent-foreground) / <alpha-value>)',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
-          foreground: 'hsl(var(--popover-foreground) / <alpha-value>)',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card) / <alpha-value>)',
-          foreground: 'hsl(var(--card-foreground) / <alpha-value>)',
-        },
+        monday: 'hsl(var(--monday) / <alpha-value>)',
+        tuesday: 'hsl(var(--tuesday) / <alpha-value>)',
+        wednesday: 'hsl(var(--wednesday) / <alpha-value>)',
+        thursday: 'hsl(var(--thursday) / <alpha-value>)',
+        friday: 'hsl(var(--friday) / <alpha-value>)',
+        saturday: 'hsl(var(--saturday) / <alpha-value>)',
+        sunday: 'hsl(var(--sunday) / <alpha-value>)',
         on: {
           primary: {
             DEFAULT: 'hsl(var(--on-primary) / <alpha-value>)',
             hover: 'hsl(var(--on-primary-hover) / <alpha-value>)',
+            low: 'hsl(var(--on-primary-low) / <alpha-value>)',
             container: 'hsl(var(--on-primary-container) / <alpha-value>)',
           },
           secondary: {
@@ -94,29 +83,36 @@ export const config: Config = {
             hover: 'hsl(var(--on-error-hover) / <alpha-value>)',
             container: 'hsl(var(--on-error-container) / <alpha-value>)',
           },
-          success: {
-            DEFAULT: 'hsl(var(--on-success) / <alpha-value>)',
-            hover: 'hsl(var(--on-success-hover) / <alpha-value>)',
-            container: 'hsl(var(--on-success-container) / <alpha-value>)',
-          },
           warning: {
             DEFAULT: 'hsl(var(--on-warning) / <alpha-value>)',
             hover: 'hsl(var(--on-warning-hover) / <alpha-value>)',
             container: 'hsl(var(--on-warning-container) / <alpha-value>)',
           },
+          success: {
+            DEFAULT: 'hsl(var(--on-success) / <alpha-value>)',
+            hover: 'hsl(var(--on-success-hover) / <alpha-value>)',
+            container: 'hsl(var(--on-success-container) / <alpha-value>)',
+          },
           surface: {
             DEFAULT: 'hsl(var(--on-surface) / <alpha-value>)',
             var: 'hsl(var(--on-surface-var) / <alpha-value>)',
+            disabled: 'hsl(var(--on-surface-disabled) / <alpha-value>)',
+            placeholder: 'hsl(var(--on-surface-placeholder) / <alpha-value>)',
           },
+          monday: 'hsl(var(--on-monday) / <alpha-value>)',
+          tuesday: 'hsl(var(--on-tuesday) / <alpha-value>)',
+          wednesday: 'hsl(var(--on-wednesday) / <alpha-value>)',
+          thursday: 'hsl(var(--on-thursday) / <alpha-value>)',
+          friday: 'hsl(var(--on-friday) / <alpha-value>)',
+          saturday: 'hsl(var(--on-saturday) / <alpha-value>)',
+          sunday: 'hsl(var(--on-sunday) / <alpha-value>)',
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        button: '0.625rem',
       },
       fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
+        sans: ['"FC Orbit"', ...fontFamily.sans],
       },
     },
   },

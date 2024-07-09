99aa53b2861dc439da348bac63d36df138a3b196
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-07-09 16:05:27 +0700

    input component styles

--- packages/ui/src/components/input/input.svelte
+++ packages/ui/src/components/input/input.svelte
@@ -18,7 +18,7 @@
 
 <input
   class="{cn(
-    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+    'flex w-full h-10 rounded-button bg-surface-container-lowest px-4 text-button2 placeholder:text-on-surface hover:outline-none hover:ring-1 hover:ring-primary focus-visible:outline-none focus-visible:ring-1.5 focus-visible:ring-primary disabled:cursor-not-allowed placeholder:disabled:text-on-surface-disabled disabled:hover:ring-0',
     className,
   )}"
   bind:value

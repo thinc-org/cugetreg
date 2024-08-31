7872114c3495855fcbf1d2c40eb4f295bb1ba73a
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-08-31 17:51:28 +0700

    feat: add subject details component

---
+++ packages/ui/src/components/subject-details/description.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { getContext } from 'svelte'
+  const variant = getContext<'left' | 'center' | 'right' | 'mobile'>(
+    'subject-details-variant',
+  )
+
+  const textStyle = variant === 'mobile' ? 'p-2' : 'p-4'
+  export let className: string = '' // Additional classes
+  export let text: string = '' // Optional text prop, can be overridden by slot
+</script>
+
+<div
+  class="{`text-on-surface font-normal text-body1 ${textStyle} ${className}`}"
+>
+  <slot>{text}</slot>
+</div>

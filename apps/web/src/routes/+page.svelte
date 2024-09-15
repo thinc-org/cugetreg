1f0c48f5ba0880218f25ba8746079f579802fbcd
neennera <neennera@gmail.com>
2024-09-15 14:52:15 +0700

    add: select input

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Button } from '@repo/ui/button'
   import { Input } from '@repo/ui/input'
+  import * as Select from '@repo/ui/input-select'
 
   let counter = 0
 
@@ -37,6 +38,21 @@
     desc="ฟอร์มนี้ใช้ไม่ได้"
     state="disable"
   />
+</div>
+<div
+  class="flex flex-col space-x-0 space-y-10 w-auto h-auto p-10 my-5 border-b-2 border-t-2"
+>
+  <h1>Select</h1>
+  <Select.Root>
+    <Select.Trigger class="w-[350px]">
+      <Select.Value placeholder="Theme" />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Item value="light">Light</Select.Item>
+      <Select.Item value="dark">Dark</Select.Item>
+      <Select.Item value="system">System</Select.Item>
+    </Select.Content>
+  </Select.Root>
 </div>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation

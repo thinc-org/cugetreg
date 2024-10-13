a9e4837197cf481b5935c13d4be374fff99f0531
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-10-13 18:28:50 +0700

    feat(v2): add radio-group components

---
+++ packages/ui/src/components/radio-group/radio-group-item.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Label, RadioGroup as RadioGroupPrimitive } from 'bits-ui'
+  import Circle from 'lucide-svelte/icons/circle'
+
+  import { cn } from '../../utils'
+
+  type $$Props = RadioGroupPrimitive.ItemProps & {
+    label: string
+  }
+  type $$Events = RadioGroupPrimitive.ItemEvents
+
+  let className: $$Props['class'] = undefined
+  export let value: $$Props['value']
+  export let id: $$Props['id']
+  export let label: $$Props['label'] = 'label'
+  export { className as class }
+</script>
+
+<div class="flex items-center space-x-2">
+  <RadioGroupPrimitive.Item
+    {value}
+    class="{cn(
+      'border-on-surface-placeholder data-[state=checked]:border-primary  text-primary ring-offset-background focus-visible:ring-ring aspect-square h-5 w-5 rounded-full border-2 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+      className,
+    )}"
+    {...$$restProps}
+    on:click
+  >
+    <div class="flex items-center justify-center">
+      <RadioGroupPrimitive.ItemIndicator>
+        <Circle class="h-2.5 w-2.5 fill-primary text-current" />
+      </RadioGroupPrimitive.ItemIndicator>
+    </div>
+  </RadioGroupPrimitive.Item>
+  <Label.Root for="{id}">{label}</Label.Root>
+</div>

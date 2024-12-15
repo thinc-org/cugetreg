ffcc560e88baa9c5b06e6db9b7d357dc33583b95
Phumsirii <phumsirisumativit@gmail.com>
2024-12-15 23:32:36 +0700

    init

---
+++ packages/ui/src/components/comment/comment.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let content = "This is a comment"; // Example prop
+</script>
+
+<style>
+  .comment {
+    width: 968px;
+    height: 444px;
+    border: 1px solid #D6D7E1; /* 1px border */
+    padding: 16px; /* Optional padding */
+    box-sizing: border-box; /* Ensure padding doesn't affect dimensions */
+  }
+</style>
+
+<div class="comment">
+  <p>{content}</p>
+</div>

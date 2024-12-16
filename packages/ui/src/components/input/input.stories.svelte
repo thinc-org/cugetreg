b572a46c9456dfbfe81b41941353dcd4efac67cb
Patthapol Kittikun <patthapol.k@gmail.com>
2024-12-16 23:25:26 +0700

    input storybook

---
+++ packages/ui/src/components/input/input.stories.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+  import type { Meta } from '@storybook/svelte'
+
+  import { Input } from './index.js'
+
+  export const meta = {
+    title: 'Atom/Input',
+    component: Input,
+    tags: ['autodocs'],
+    argTypes: {
+      state: {
+        control: {
+          type: 'select',
+        },
+        options: ['default', 'error', 'success'],
+        defaultValue: 'default',
+      },
+    },
+  } satisfies Meta<Input>
+</script>
+
+<script lang="ts">
+  import { Story, Template } from '@storybook/addon-svelte-csf'
+</script>
+
+<!--👇 We create a “template” of how args map to rendering -->
+
+<Template let:args>
+  <Input {...args} />
+</Template>
+
+<!-- 👇 Each story then reuses that template -->
+
+<Story name="Default" args="{{ state: 'default', placeholder: 'รหัสผ่าน' }}" />

9937c06c844d9ad6124d7516562dfc0e93f0ccde
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-12-19 23:35:14 +0700

    fix: course-card responsive style

--- packages/ui/src/components/course-card/course-card.svelte
+++ packages/ui/src/components/course-card/course-card.svelte
@@ -40,7 +40,7 @@
 
 <div
   class="{cn(
-    'flex flex-col w-[334px] h-[170px] lg:w-[440px] lg:h-[200px] ring-2 ring-neutral-100 rounded-xl py-5 px-4 lg:p-6 justify-between relative',
+    'flex flex-col w-[334px] h-[164px] md:w-[440px] md:h-[194px] px-4 py-5 md:p-6 gap-3 ring-2 ring-neutral-100 rounded-xl relative',
     className,
   )}"
   {...$$restProps}
@@ -51,7 +51,9 @@
   <div class="flex flex-row justify-between items-center">
     <div class="flex flex-col">
       <div class="font-medium text-caption">{course.code}</div>
-      <div class="font-medium text-subtitle">{course.name}</div>
+      <div class="font-medium sm:text-body2 md:text-body1">
+        {course.name}
+      </div>
     </div>
     <div class="flex gap-1">
       {#each course.gened as gened}
@@ -63,10 +65,10 @@
     <div
       class="font-normal text-caption text-neutral-400 flex flex-row items-center"
     >
-      {course.credit} หน่วยกิต <Dot color="#EDEDF1" /> ที่นั่ง GenEd {course.seat}
+      {course.credit} หน่วยกิต <Dot color="#EDEDF1" size="12" /> ที่นั่ง GenEd {course.seat}
       /
       {course.maxseat}
-      <Dot color="#EDEDF1" />
+      <Dot color="#EDEDF1" size="12" />
       {course.review} รีวิว
     </div>
     <div class="flex gap-2">
@@ -76,7 +78,10 @@
     </div>
   </div>
   <div class="flex flex-row justify-between items-center">
-    <Button variant="outlined" size="lg" color="neutral" class="w-36 lg:w-48"
+    <Button
+      variant="outlined"
+      color="neutral"
+      class="w-36 h-7 md:w-48 md:h-9 text-caption md:text-body2"
       >เลือกเซคชัน</Button
     >
     {#if !selected}
@@ -84,12 +89,17 @@
         variant="outlined"
         color="primary"
         on:click="{onButtonClick}"
-        class="w-36 lg:w-48"
+        class="w-36 h-7 md:w-48 md:h-9 text-caption md:text-body2"
+        size="sm"
       >
         เลือก <Plus size="16" strokeWidth="3" />
       </Button>
     {:else}
-      <Button color="primary" on:click="{onButtonClick}" class="w-36 lg:w-48">
+      <Button
+        color="primary"
+        on:click="{onButtonClick}"
+        class="w-36 h-7 md:w-48 md:h-9 text-caption md:text-body2"
+      >
         เลือก <Check size="16" strokeWidth="3" />
       </Button>
     {/if}

aee0dca41c2075eb40e6ef5ad0042e2921730a56
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-12-20 23:02:22 +0700

    feat: responsive

--- packages/ui/src/components/collapsible/collapsible.svelte
+++ packages/ui/src/components/collapsible/collapsible.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Collapsible } from 'bits-ui'
   import { ChevronDown } from 'lucide-svelte'
+  import { slide } from 'svelte/transition'
 
   export let name = undefined
   let collapseItems = ['TEST1', 'TEST2', 'TEST3']
@@ -8,13 +9,14 @@
 
 <Collapsible.Root class="relative">
   <div class="flex flex-row gap-2">
-    <p class="text-primary text-lg font-medium">{name}</p>
+    <p class="text-primary text-lg">{name}</p>
     <Collapsible.Trigger>
       <ChevronDown color="#4A70C6" />
     </Collapsible.Trigger>
   </div>
   <Collapsible.Content
     class="absolute top-8 right-0 w-28 bg-surface-container border-b-neutral-400 rounded-md"
+    transition="{slide}"
   >
     {#each collapseItems as item}
       <div class="p-2">{item}</div>
--- packages/ui/src/components/navbar/navbar.svelte
+++ packages/ui/src/components/navbar/navbar.svelte
@@ -1,5 +1,3 @@
-<!-- To be implemented: add props (isLoggedIn, ...) -->
-<!-- To be implemented: make responsive -->
 <script lang="ts">
   import { Moon, Search } from 'lucide-svelte'
 
@@ -18,25 +16,25 @@
 </script>
 
 <div
-  class="py-3 px-10 flex justify-between items-center z-50 border-b-2 border-surface-container-low"
+  class=" h-20 py-3 px-5 md:py-3 md:px-10 flex justify-between items-center z-50 border-b-2 border-surface-container-low"
 >
-  <div class="flex flex-row gap-6">
-    <Logo class="w-32 h-10" />
-    <div class="relative h-full">
+  <div class="flex flex-row gap-6 items-center">
+    <Logo class="w-24 h-8 md:w-32 md:h-10" />
+    <div class="hidden md:flex relative">
       <Search
-        class="absolute my-auto right-4 top-[32%]"
+        class="absolute right-4 top-[32%]"
         size="16"
         color="#898EA7"
         strokeWidth="3.5"
       />
       <Input
         placeholder="ค้นหาวิชา"
-        class="w-56 placeholder:text-neutral-400"
+        class="w-56 bg-surface-container-lowest placeholder:text-neutral-400"
       />
     </div>
   </div>
   <div
-    class="absolute left-1/2 transform -translate-x-1/2 flex flex-row justify-center items-center gap-4"
+    class="hidden absolute left-1/2 transform -translate-x-1/2 md:flex flex-row justify-center items-center gap-4"
   >
     <!-- To be implemented: add page from navItems-->
     {#each navItems as item}
@@ -57,16 +55,21 @@
       href="https://github.com/thinc-org/cugetreg"
       target="_blank"
       rel="noreferrer"
+      class="hidden md:flex"
     >
-      <GitHubMark class="w-8 h-8 text-neutral-500" />
+      <GitHubMark class="w-8 h-8 text-neutral-500 " />
     </a>
-    <IconButton color="neutral"><Moon strokeWidth="3" size="16" /></IconButton>
+    <IconButton color="neutral" class="hidden md:flex"
+      ><Moon strokeWidth="3" size="16" /></IconButton
+    >
     {#if isLoggedIn}
       <!-- To be implemented: Collapsible component -->
       <Collapsible {name} />
     {:else}
       <!-- To be implemented: add real href in Button -->
-      <Button href="login" class="font-medium w-28">เข้าสู่ระบบ</Button>
+      <Button href="login" class="w-24 md:w-28"
+        ><p class="text-xs font-light md:text-base">เข้าสู่ระบบ</p></Button
+      >
     {/if}
   </div>
 </div>

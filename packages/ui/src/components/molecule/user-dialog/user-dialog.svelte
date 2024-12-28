754376c550466da7272d66113b1e03ac1288be96
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-12-28 23:17:36 +0700

    refactor: img & loops

--- packages/ui/src/components/molecule/user-dialog/user-dialog.svelte
+++ packages/ui/src/components/molecule/user-dialog/user-dialog.svelte
@@ -29,18 +29,20 @@
 
 <div class="w-60 rounded-lg border-2 border-[#EDEDF1]">
   <div class="flex flex-col justify-center items-center p-4 gap-4">
-    <img src={imageUrl} alt="Profile" class="rounded-full w-[72px] h-20" />
+    <img
+      src={imageUrl}
+      alt="Profile"
+      class="rounded-full w-[72px] h-20 object-cover"
+    />
     <div class="flex flex-col gap-2 items-center">
       <p class="text-on-surface text-h3 font-bold">{shortenedName}</p>
       <p class="text-on-surface text-body2 font-medium">ID: {id}</p>
     </div>
   </div>
-  <div class="flex flex-row p-4 gap-3 items-center border-t border-[#EDEDF1]">
-    <Settings size="16" color="#353745" strokeWidth="2.5" />
-    <p class="text-on-surface font-medium text-button2">ตั้งค่า</p>
-  </div>
-  <div class="flex flex-row p-4 gap-3 items-center border-t border-[#EDEDF1]">
-    <DoorOpen size="16" color="#353745" strokeWidth="2.5" />
-    <p class="text-on-surface font-medium text-button2">ออกจากระบบ</p>
-  </div>
+  {#each items as { icon: Icon, name }}
+    <div class="flex flex-row p-4 gap-3 items-center border-t border-[#EDEDF1]">
+      <Icon size="16" color="#353745" strokeWidth="2.5" />
+      <p class="text-on-surface font-medium text-button2">{name}</p>
+    </div>
+  {/each}
 </div>

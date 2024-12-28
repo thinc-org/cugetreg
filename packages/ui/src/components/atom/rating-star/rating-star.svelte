ab83819236d0983863f6a1f8772d94e80243b077
Phumsirii <phumsirisumativit@gmail.com>
2024-12-28 16:51:59 +0700

    move component code to rating-star

--- packages/ui/src/components/atom/rating-star/rating-star.svelte
+++ packages/ui/src/components/atom/rating-star/rating-star.svelte
@@ -7,3 +7,29 @@
   let hasHalfStar = $derived(rating % 1 !== 0) // Determine if there's a half star
   let emptyStars = $derived(totalStars - filledStars - (hasHalfStar ? 1 : 0))
 </script>
+
+<div class="flex flex-row text-h3 text-primary gap-1.5">
+  <!-- Render filled stars -->
+  {#each Array(filledStars) as _, i}
+    <Star fill="currentColor" />
+  {/each}
+
+  <!-- Optionally render a half star (if applicable) -->
+  {#if hasHalfStar}
+    <div class="flex gap-0">
+      <div class="relative inline-block w-6 h-6 overflow-hidden max-w-3">
+        <Star fill="currentColor" />
+      </div>
+      <div
+        class="relative inline-block w-6 h-6 overflow-hidden max-w-3 transform scale-x-[-1]"
+      >
+        <Star />
+      </div>
+    </div>
+  {/if}
+
+  <!-- Render unfilled stars -->
+  {#each Array(emptyStars) as _, __}
+    <Star />
+  {/each}
+</div>

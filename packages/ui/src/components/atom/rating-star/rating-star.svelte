19c563aba7aae6f5e0db087d6c4e7a68157f48d6
Phumsirii <phumsirisumativit@gmail.com>
2024-12-28 16:51:10 +0700

    move parameters from commentCard to rating-star

--- packages/ui/src/components/atom/rating-star/rating-star.svelte
+++ packages/ui/src/components/atom/rating-star/rating-star.svelte
@@ -1,3 +1,9 @@
 <script>
   import { Star } from 'lucide-svelte'
+  let rating = $props()
+  // Calculate the number of filled and unfilled stars
+  const totalStars = 5
+  let filledStars = $derived(Math.floor(rating)) // Number of fully filled stars
+  let hasHalfStar = $derived(rating % 1 !== 0) // Determine if there's a half star
+  let emptyStars = $derived(totalStars - filledStars - (hasHalfStar ? 1 : 0))
 </script>
--- packages/ui/src/components/molecule/comment/comment.svelte
+++ packages/ui/src/components/molecule/comment/comment.svelte
@@ -2,11 +2,6 @@
   import { ThumbsUp, ThumbsDown, Star } from 'lucide-svelte'
 
   let { content, semester, rating, likesCount, dislikesCount } = $props()
-  // Calculate the number of filled and unfilled stars
-  const totalStars = 5
-  let filledStars = $derived(Math.floor(rating)) // Number of fully filled stars
-  let hasHalfStar = $derived(rating % 1 !== 0) // Determine if there's a half star
-  let emptyStars = $derived(totalStars - filledStars - (hasHalfStar ? 1 : 0))
   let isExpanded = $state(false)
 </script>
 

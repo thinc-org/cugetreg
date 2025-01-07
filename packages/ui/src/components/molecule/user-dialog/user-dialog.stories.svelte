160aa3c631f26f29a7fb257d853ec814cdd18518
K.Chanatipz <chanatipzaza2003@gmail.com>
2025-01-07 12:51:30 +0700

    fix: image url example

--- packages/ui/src/components/molecule/user-dialog/user-dialog.stories.svelte
+++ packages/ui/src/components/molecule/user-dialog/user-dialog.stories.svelte
@@ -25,7 +25,7 @@
   name="Example"
   args={{
     imageUrl:
-      'https://s3-alpha-sig.figma.com/img/9700/7ce5/9fb43f41dfa4552821c5c7bc837a7e84?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UWp0COPKxbiGnZUbN7-EQF0v8PznPXZzvPZxc3mryo-Ht7j~6qUJDaq3uzCuh-z-baS3x6SeJ8omqtDT6ECz6P9KLdXGXtSex5x-2zgezbPQAGBym2aQEuZ4bfn9hCQ6SsImZaG30l5c9Xw1wQxAiuTdPfXP3uPIfZJXesdXB73tKGKNGPv93MDrVpE2ZSaA5hhkkFyhhbqgiRpNEuZ4GMqatcVzjcl5xyhgbRHR2o03eXbvHUsFgzCD-pTFnofOB6pYYzqsroWM3pmCu9NsDXLCDgRNdsGgSczwTwj6vLoWIMuvmEkTcVcHc-0JRGzo4XRpfrJ2X0jd-hTU41H0kQ__',
+      'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg',
     name: 'Wanrudee Kkk',
     id: '6XXXXXXXXX',
   }}

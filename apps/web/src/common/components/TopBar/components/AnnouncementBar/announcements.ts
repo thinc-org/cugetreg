08de7d9c1152ef051135a76ab66ffb3789e1c161
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-01-15 08:51:13 +0700

    update announcement

--- apps/web/src/common/components/TopBar/components/AnnouncementBar/announcements.ts
+++ apps/web/src/common/components/TopBar/components/AnnouncementBar/announcements.ts
@@ -2,28 +2,11 @@ import { AnnouncementItem } from '@web/common/components/TopBar/components/Annou
 
 // TODO Admin should be able to change the annoucement
 
-export function getAnnouncement1(): AnnouncementItem {
-  return {
-    id: 'annoucement_1',
-    label: 'CU Get Reg เป็นเว็บไซต์ที่จัดทำโดยนิสิต ซึ่งได้รับการสนับสนุนเซิฟเวอร์จาก Reg Chula',
-    severity: 'info',
-  }
-}
-
 export function getNotRegChulaWarning(): AnnouncementItem {
   return {
     id: 'not_reg_chula',
     label:
-      'CU Get Reg เป็นเพียงเครื่องมือที่ช่วยให้การวางแผนลงทะเบียนเรียนง่ายขึ้น แต่ไม่ใช่การลงทะเบียนเรียนจริง คุณสามารถลงทะเบียนเรียนได้ที่ https://www2.reg.chula.ac.th/ เพียงช่องทางเดียวเท่านั้น',
+      'CU Get Reg เป็นเว็บไซต์ที่จัดทำโดยนิสิต ซึ่งได้รับการสนับสนุนเซิฟเวอร์จาก Reg Chula โดยเป็นเพียงเครื่องมือที่ช่วยให้การวางแผนลงทะเบียนเรียนง่ายขึ้น แต่ไม่ใช่การลงทะเบียนเรียนจริง คุณสามารถลงทะเบียนเรียนได้ที่ https://www2.reg.chula.ac.th/ เพียงช่องทางเดียวเท่านั้น',
     severity: 'warning',
-  }
-}
-
-export function term67Issue(): AnnouncementItem {
-  return {
-    id: 'term_67_issue',
-    label:
-      'ข้อมูลของปีการศึกษา 2567 จะยังมีไม่ครบ เนื่องจากปัญหาของการเชื่อมต่อระหว่าง CU Get Reg กับเว็บไซต์ของทะเบียนจุฬาฯ ขออภัยในความไม่สะดวก',
-    severity: 'error',
   }
 }
--- apps/web/src/common/components/TopBar/index.tsx
+++ apps/web/src/common/components/TopBar/index.tsx
@@ -1,11 +1,7 @@
 import { useRouter } from 'next/router'
 
 import { AnnouncementBar } from './components/AnnouncementBar'
-import {
-  getAnnouncement1,
-  getNotRegChulaWarning,
-  term67Issue,
-} from './components/AnnouncementBar/announcements'
+import { getNotRegChulaWarning } from './components/AnnouncementBar/announcements'
 import { ConfigBar } from './components/ConfigBar'
 import { ConfigBarLayout } from './components/ConfigBar/styled'
 import { NavBar } from './components/NavBar'
@@ -15,9 +11,7 @@ import { StickyContainer, StickySpace, TopBarLayout } from './styled'
 export function AnnouncementSection() {
   return (
     <>
-      <AnnouncementBar announcement={getAnnouncement1()} />
       <AnnouncementBar announcement={getNotRegChulaWarning()} />
-      <AnnouncementBar announcement={term67Issue()} />
     </>
   )
 }

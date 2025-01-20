e630fcc8b4ef65282f73d5f616a81e6327410167
Phumsirii <phumsirisumativit@gmail.com>
2025-01-20 09:48:30 +0700

    start implementing with mock data

---
+++ packages/ui/src/components/organism/sectionDetails/sectionDetails.stories.svelte
@@ -0,0 +1,41 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  import { sectionDetails } from './index.js'
+
+  const { Story } = defineMeta<typeof sectionDetails>({
+    title: 'Organism/sectionDetails',
+    component: sectionDetails,
+    tags: ['autodocs'],
+    argTypes: {
+      isLoggedIn: {
+        control: 'boolean',
+      },
+      name: {
+        control: 'text',
+      },
+    },
+  })
+</script>
+
+<Story
+  name="Desktop"
+  parameters={{ viewport: { defaultViewport: 'tablet' } }}
+/>
+
+<Story
+  name="Desktop & Logged In"
+  parameters={{ viewport: { defaultViewport: 'tablet' } }}
+  args={{ isLoggedIn: true, name: 'Testname testname' }}
+/>
+
+<Story
+  name="Mobile"
+  parameters={{ viewport: { defaultViewport: 'mobile2' } }}
+/>
+
+<Story
+  name="Mobile & Loggin In"
+  parameters={{ viewport: { defaultViewport: 'mobile2' } }}
+  args={{ isLoggedIn: true, name: 'Testname testname' }}
+/>
---
+++ packages/ui/src/components/organism/sectionDetails/sectionDetails.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import * as Table from '../../atom/table/index.ts'
+  import { Button } from '../../atom/button/index.ts'
+</script>
+
+<div>
+  <Table.Root>
+    <Table.Header>
+      <Table.Row>
+        <Table.Head>เซคชั่น</Table.Head>
+        <Table.Head>จำนวนที่รับ</Table.Head>
+        <Table.Head>ผู้สอน</Table.Head>
+        <Table.Head>วันเวลาที่เรียน</Table.Head>
+        <Table.Head>ห้องเรียน</Table.Head>
+        <Table.Head>รูปแบบ</Table.Head>
+        <Table.Head>&nbsp;</Table.Head>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      <Table.Row class="border-b-0">
+        <Table.Cell data-rowspan={true} rowspan={2}>1</Table.Cell>
+        <Table.Cell>28/28</Table.Cell>
+        <Table.Cell>NNN</Table.Cell>
+        <Table.Cell>THU 21:00 - 24:00</Table.Cell>
+        <Table.Cell>ENG4 18-16</Table.Cell>
+        <Table.Cell>LECT</Table.Cell>
+        <Table.Cell><Button>เลือก +</Button></Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>28/28</Table.Cell>
+        <Table.Cell>NNN</Table.Cell>
+        <Table.Cell>FRI 21:00 - 24:00</Table.Cell>
+        <Table.Cell>ENG4 18-16</Table.Cell>
+        <Table.Cell>LECT</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table.Root>
+</div>

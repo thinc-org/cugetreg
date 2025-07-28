ee76155ec02870656752077ed6ee568462efe096
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-07-28 16:48:07 +0700

    feat(api): openai moderation

---
+++ apps/api/src/common/services/discord.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
+import type { MessageCreateOptions } from 'discord.js'
+
+import { Configuration } from '@api/config/configuration'
+
+@Injectable()
+export class DiscordService {
+  private webhookUrl: string | null = null
+
+  constructor(private configService: ConfigService<Configuration>) {
+    this.webhookUrl = this.configService.get<string>('slackWebhookUrl')
+  }
+
+  isAvailable(): boolean {
+    return this.webhookUrl !== null
+  }
+
+  async sendMessage(message: MessageCreateOptions) {
+    const response = await fetch(this.webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Failed to send message to Discord: ${response.statusText}`)
+    }
+
+    return await response.json()
+  }
+}
--- apps/api/src/review/review.module.ts
+++ apps/api/src/review/review.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
+import { CommonModule } from '@api/common/common.module'
+
 import { ModelName, ReviewSchema } from '@cgr/schema'
 
 import { ReviewResolver } from './review.resolver'
 import { ReviewService } from './review.service'
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: ModelName.Review, schema: ReviewSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: ModelName.Review, schema: ReviewSchema }]),
+    CommonModule,
+  ],
   providers: [ReviewResolver, ReviewService],
   exports: [ReviewService],
 })

131fd61dc72bef2a7214550a32b8a0084e2e87f5
Coded5 <phoenixanimation24@gmail.com>
2025-12-19 14:28:59 +0700

    feat: atom/rating-star

---
+++ frontend/src/lib/components/atoms/rating-star/rating-star.stories.svelte
@@ -0,0 +1,28 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  import { RatingStar } from './index.js'
+
+  const { Story } = defineMeta<typeof RatingStar>({
+    title: 'Atom/Rating Star',
+    component: RatingStar,
+    tags: ['autodocs'],
+    argTypes: {
+      rating: {
+        control: {
+          type: 'range',
+          min: 0,
+          max: 5,
+          step: 0.5,
+        },
+      },
+    },
+  })
+</script>
+
+<Story
+  name="Default"
+  args={{
+    rating: 3.5,
+  }}
+/>
---
+++ frontend/src/lib/components/atoms/rating-star/rating-star.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { Star, StarHalf } from "lucide-svelte";
+    let { rating }: { rating: number } = $props();
+    // Calculate the number of filled and unfilled stars
+    const totalStars: number = 5;
+    let filledStars: number = $derived(Math.floor(rating)); // Number of fully filled stars
+    let hasHalfStar: boolean = $derived(rating % 1 !== 0); // Determine if there's a half star
+</script>
+
+<div class="flex flex-row text-h3 text-primary gap-1.5">
+    <div class="absolute inset-0 flex flex-row">
+        {#each Array(totalStars) as _}
+            <Star />
+        {/each}
+    </div>
+    <div class="absolute inset-0 flex flex-row">
+        {#each Array(filledStars) as _}
+            <Star fill="currentColor" />
+        {/each}
+
+        <!-- Optionally render a half star (if applicable) -->
+        {#if hasHalfStar}
+            <StarHalf fill="currentColor" />
+        {/if}
+    </div>
+</div>

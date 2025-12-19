1d874c57bcfc7eb0ae84b7c02ac10eba37796fb8
Coded5 <phoenixanimation24@gmail.com>
2025-12-19 15:58:01 +0700

    feat: atoms/accordion components

---
+++ frontend/src/lib/components/atoms/accordion/accordion-trigger.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {
+        Accordion as AccordionPrimitive,
+        type AccordionTriggerProps,
+    } from "bits-ui";
+    import { ChevronDown } from "lucide-svelte";
+    import type { Snippet } from "svelte";
+
+    import { cn } from "../../../../utils";
+
+    interface Props {
+        class?: AccordionTriggerProps["class"];
+        level?: AccordionPrimitive.HeaderProps["level"];
+        children?: Snippet;
+        [key: string]: unknown;
+    }
+
+    let {
+        class: className = undefined,
+        level = 3,
+        children,
+        ...rest
+    }: Props = $props();
+</script>
+
+<AccordionPrimitive.Header {level} class="flex">
+    <AccordionPrimitive.Trigger
+        class={cn(
+            "flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180 px-2",
+            className,
+        )}
+        {...rest}
+    >
+        {@render children?.()}
+        <ChevronDown class="h-6 w-6 transition-transform duration-200" />
+    </AccordionPrimitive.Trigger>
+</AccordionPrimitive.Header>

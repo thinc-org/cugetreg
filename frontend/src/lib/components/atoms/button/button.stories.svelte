ea518ca7d9fe13f45fac1dc282c2b5fc6fc2e291
Coded5 <phoenixanimation24@gmail.com>
2025-12-20 14:13:36 +0700

    fix: typos

--- frontend/src/lib/components/atoms/button/button.stories.svelte
+++ frontend/src/lib/components/atoms/button/button.stories.svelte
@@ -12,7 +12,7 @@
                 control: {
                     type: "select",
                 },
-                options: ["solid", "outline"],
+                options: ["solid", "outlined"],
                 defaultValue: "solid",
             },
             size: {

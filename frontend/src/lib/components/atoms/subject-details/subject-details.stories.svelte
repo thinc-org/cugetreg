42cb077c024e1b114d8b901ce182a6902cbbd9c0
Coded5 <phoenixanimation24@gmail.com>
2025-12-20 14:19:06 +0700

    feat: atoms/subject-detail components

---
+++ frontend/src/lib/components/atoms/subject-details/subject-details.stories.svelte
@@ -0,0 +1,22 @@
+<script module lang="ts">
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+
+    import {
+        SubjectDetails,
+        SubjectDetailsDescription,
+        SubjectDetailsLabel,
+    } from "./index";
+
+    const { Story } = defineMeta({
+        title: "Atom/Subject Detail",
+    });
+
+    //NOTE: This component appear to be unfinished
+</script>
+
+<Story name="Default">
+    <SubjectDetails>
+        <SubjectDetailsLabel>COM PORG</SubjectDetailsLabel>
+        <SubjectDetailsDescription>Something</SubjectDetailsDescription>
+    </SubjectDetails>
+</Story>

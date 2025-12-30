a51c68110fa5e3a36b8a9540db74f0d5b236b937
Coded5 <phoenixanimation24@gmail.com>
2025-12-30 23:44:31 +0700

    feat: add types and constants

---
+++ frontend/src/types.ts
@@ -0,0 +1,34 @@
+import { courseColorVariants } from "./constants";
+
+export type ColorVariant = keyof typeof courseColorVariants;
+export type Day = "MO" | "TU" | "WE" | "TH" | "FR" | "SA" | "SU";
+export type GenEdType = "SC" | "SO" | "HU" | "IN";
+
+export type Period = {
+    day: Day;
+    startTime: number;
+    duration: number;
+    building: string;
+    room: string;
+}
+
+export type Course = {
+    name: string;
+    code: string;
+    credit: number;
+    gened: GenEdType[];
+    seat: number;
+    maxseat: number;
+    // review: number;
+    sections: { [key: number]: Period[] };
+}
+
+export type CourseSchedule = {
+    id: number;
+    course: Course;
+    selectedSection: number;
+    hidden: boolean;
+    colorVariant?: ColorVariant;
+}
+
+export type ScheduleData = CourseSchedule[];

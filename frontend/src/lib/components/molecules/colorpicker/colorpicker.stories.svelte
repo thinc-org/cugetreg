74dce0d8802f4b7827496ab71902304aa67ec132
Coded5 <phoenixanimation24@gmail.com>
2025-12-30 23:45:33 +0700

    update: Use colors from constants

--- frontend/src/lib/components/molecules/colorpicker/colorpicker.stories.svelte
+++ frontend/src/lib/components/molecules/colorpicker/colorpicker.stories.svelte
@@ -2,19 +2,7 @@
     import { defineMeta } from "@storybook/addon-svelte-csf";
 
     import { ColorPicker } from ".";
-
-    const placeholder_option = [
-        "bg-neutral-100 border-neutral-500 text-neutral-700",
-        "bg-pink-300 border-pink-500 text-pink-700",
-        "bg-tangerine-300 border-tangerine-500 text-tangerine-700",
-        "bg-orange-300 border-orange-500 text-orange-700",
-        "bg-yellow-300 border-yellow-500 text-yellow-700",
-        "bg-green-300 border-green-500 text-green-700",
-        "bg-teal-300 border-teal-500 text-teal-700",
-        "bg-sky-300 border-sky-500 text-sky-700",
-        "bg-indigo-300 border-indigo-500 text-indigo-700",
-        "bg-purple-300 border-purple-500 text-purple-700",
-    ];
+    import { courseColorVariants } from "../../../../constants";
 
     const { Story } = defineMeta({
         title: "Molecule/Color Picker",
@@ -29,13 +17,14 @@
             },
             options: {
                 control: false,
-                defaultValue: placeholder_option,
+                defaultValue: courseColorVariants,
             },
         },
         args: {
-            options: placeholder_option,
+            options: courseColorVariants,
             onCancel: () => alert("Cancel"),
             onConfirmSelected: (option) => alert(option),
+            class: "w-[350px]",
         },
     });
 </script>
--- frontend/src/lib/components/molecules/colorpicker/colorpicker.svelte
+++ frontend/src/lib/components/molecules/colorpicker/colorpicker.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang="ts" generics="Key extends string">
     import type { ClassValue } from "clsx";
     import { cn } from "../../../../utils";
     import { Button } from "../../atoms/button";
@@ -8,75 +8,55 @@
         onChange?: (option: string) => void;
         onCancel?: () => void;
         onConfirmSelected?: (option: string) => void;
-        options: string[];
+        options: Record<Key, string>;
+        value: Key;
     }
 
-    const {
+    let {
         class: className = undefined,
         onCancel = () => {},
         onConfirmSelected = () => {},
         onChange = () => {},
         options,
+        value = $bindable(),
     }: ColorPickerProps = $props();
 
-    const halfLength = $derived(Math.trunc(options.length / 2));
-
-    let selected = $state(0);
-
-    $effect(() => {
-        onChange(options[selected]);
-    });
+    function handleCancel() {
+        onCancel();
+    }
 </script>
 
-<div
-    class={cn(
-        className,
-        "border border-neutral-200 rounded-lg p-5 min-w-50 max-w-75",
-    )}
->
+<div class={cn(className, "border border-neutral-200 rounded-lg p-5")}>
     <div class="font-bold text-lg">เลือกสีในตาราง</div>
 
-    <div class="flex justify-between mt-2">
-        {#each options.slice(0, halfLength) as color, i}
+    <div class="grid grid-cols-5 my-5">
+        {#each Object.keys(options) as Key[] as option}
             <Button
                 variant="solid"
                 color="primary"
                 class={cn(
                     "aspect-square hover:ring-0 border m-2",
-                    color,
-                    i === selected &&
+                    options[option],
+                    value === option &&
                         "outline-[1.5px]! outline-offset-4 outline-primary",
-                    i === selected &&
+                    value === option &&
                         "hover:outline-[1.5px]! hover:outline-solid!",
                 )}
-                onclick={() => (selected = i)}
-            />
-        {/each}
-    </div>
-    <div class="flex justify-between mb-5">
-        {#each options.slice(halfLength, options.length) as color, i}
-            <Button
-                variant="solid"
-                color="primary"
-                class={cn(
-                    "aspect-square hover:ring-0 border m-2",
-                    color,
-                    i + halfLength === selected &&
-                        "outline-[1.5px]! outline-offset-4 outline-primary",
-                    i + halfLength === selected &&
-                        "hover:outline-[1.5px]! hover:outline-solid!",
-                )}
-                onclick={() => (selected = i + halfLength)}
+                onclick={() => (value = option)}
             />
         {/each}
     </div>
 
     <div class="flex justify-between space-x-5">
-        <Button class="flex-1" onclick={() => onCancel()}>ยกเลิก</Button>
+        <Button class="flex-1" onclick={handleCancel}>ยกเลิก</Button>
         <Button
             class="flex-1"
             variant="outlined"
-            onclick={() => onConfirmSelected(options[selected])}>เลืิอก</Button
+            onclick={() => {
+                onConfirmSelected(options[value]);
+            }}
         >
+            เลืิอก
+        </Button>
     </div>
 </div>

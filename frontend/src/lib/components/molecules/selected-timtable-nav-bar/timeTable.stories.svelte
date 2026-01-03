e382bb0e50cd71148955168fb190763dd7581403
Passton Jindawong <passton8362@gmail.com>
2026-01-03 21:52:58 +0700

    fixed color
    fixed color

---
+++ frontend/src/lib/components/molecules/selected-timtable-nav-bar/timeTable.stories.svelte
@@ -0,0 +1,20 @@
+<script module lang="ts">
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+
+    import { timeTable } from "./index";
+
+    const { Story } = defineMeta<typeof timeTable>({
+        title: "Molecule/timetable",
+        component: timeTable,
+        tags: ["autodocs"],
+        argTypes: {
+            class: {
+                control: false,
+            },
+        },
+    });
+</script>
+
+<!--👇 Each story then reuses that template-->
+
+<Story name="Default" />

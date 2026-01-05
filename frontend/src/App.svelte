866bb9e287c2d7fc3a966d41df20833a98faf45c
Thames <chiratt.thames@gmail.com>
2026-01-05 19:38:36 +0700

    Feat: Added template files

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,4 +1,21 @@
-<script>
+<script lang="ts">
+  import { Router, Route, Link } from 'svelte-routing'
+
+  import Home from './pages/Home.svelte'
+  import About from './pages/About.svelte'
+  import NotFound from './pages/NotFound.svelte'
 </script>
 
-<h1>CUGetReg</h1>
+<Router>
+  <main>
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="*">
+      <NotFound />
+    </Route>
+  </main>
+</Router>
---
+++ frontend/src/pages/NotFound.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { Navbar } from '../lib/components/organisms/navbar'
+</script>
+
+<Navbar />
+<h1 class="text-2xl font-bold">404</h1>
+<p>Page not found.</p>

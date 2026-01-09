4fca4b6d6c9f15d29232bf90db37a5b44b6d023c
Coded5 <phoenixanimation24@gmail.com>
2026-01-09 12:04:14 +0700

    chore: use semicolon for prettier

--- apps/frontend/prettier.config.js
+++ apps/frontend/prettier.config.js
@@ -1,9 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: [
-    'prettier-plugin-svelte',
-    'prettier-plugin-tailwindcss'
-  ],
-  semi: false,
-  singleQuote: true
+    plugins: [
+        'prettier-plugin-svelte',
+        'prettier-plugin-tailwindcss'
+    ],
+    semi: true,
+    singleQuote: true
 }

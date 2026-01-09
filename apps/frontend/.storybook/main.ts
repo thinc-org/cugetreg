39f3ed433bbafbd38b82aed63101b899de4e7b88
Coded5 <phoenixanimation24@gmail.com>
2026-01-09 11:18:26 +0700

    fix: fix storybook to work with sveltekit

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -1,18 +1,40 @@
-import type { StorybookConfig } from '@storybook/svelte-vite';
+import type { StorybookConfig } from '@storybook/sveltekit';
 
+import { dirname } from "path"
+
+import { fileURLToPath } from "url"
+import { join } from 'path';
+import { searchForWorkspaceRoot } from 'vite';
+
+/**
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string): any {
+    return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
 const config: StorybookConfig = {
     "stories": [
         "../src/**/*.mdx",
         "../src/**/*.stories.@(js|ts|svelte)"
     ],
     "addons": [
-        "@storybook/addon-svelte-csf",
-        "@chromatic-com/storybook",
-        "@storybook/addon-vitest",
-        "@storybook/addon-a11y",
-        "@storybook/addon-docs",
-        "@storybook/addon-styling-webpack"
+        getAbsolutePath('@storybook/addon-svelte-csf'),
+        getAbsolutePath('@chromatic-com/storybook'),
+        getAbsolutePath('@storybook/addon-vitest'),
+        getAbsolutePath('@storybook/addon-a11y'),
+        getAbsolutePath('@storybook/addon-docs'),
+        getAbsolutePath('@storybook/addon-styling-webpack')
     ],
-    "framework": "@storybook/svelte-vite"
+    "framework": getAbsolutePath('@storybook/sveltekit'),
+    "staticDirs": ['../static'],
+    viteFinal: async (config) => {
+        if (config.server && config.server.fs) {
+            config.server.fs.allow = [
+                searchForWorkspaceRoot(process.cwd()),
+            ];
+        }
+        return config;
+    },
 };
 export default config;
--- apps/frontend/.storybook/vitest.setup.ts
+++ apps/frontend/.storybook/vitest.setup.ts
@@ -1,5 +1,5 @@
 import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
-import { setProjectAnnotations } from '@storybook/svelte-vite';
+import { setProjectAnnotations } from '@storybook/sveltekit';
 import * as projectAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.

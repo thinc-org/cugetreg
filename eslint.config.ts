7ea86c7f495097086515b7df5de86976b86d9669
Coded5 <phoenixanimation24@gmail.com>
2026-01-09 12:05:54 +0700

    feat: move eslint config to config package

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,25 +1,3 @@
-// @ts-check
+import { eslint } from '@cugetreg/configs/lint';
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-
-export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    {
-        rules: {
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "warn"
-        }
-    },
-    {
-        ignores: [
-            '**/build',
-            '**/.svelte-kit',
-            '**/package',
-            '**/storybook-static',
-            '**/dist',
-        ],
-    }
-);
+export default eslint;

f1dd8dc242b87d92d3e8a38afa87f5e7edb99082
TeriyakiThames <chiratt.thames@gmail.com>
2026-01-11 15:43:02 +0700

    Fix: Storybook configuration using svelte-vite from addon

--- apps/cugetreg/.storybook/main.ts
+++ apps/cugetreg/.storybook/main.ts
@@ -1,37 +1,27 @@
 import type { StorybookConfig } from '@storybook/sveltekit';
 import type { UserConfig } from 'vite';
-
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { searchForWorkspaceRoot } from 'vite';
 
-function getAbsolutePath(value: string): string {
-    return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
-}
-
 const config: StorybookConfig = {
-    stories: [
-        '../src/**/*.mdx',
-        '../src/**/*.stories.@(js|ts|svelte)',
-    ],
-    addons: [
-        getAbsolutePath('@storybook/addon-svelte-csf'),
-        getAbsolutePath('@chromatic-com/storybook'),
-        getAbsolutePath('@storybook/addon-vitest'),
-        getAbsolutePath('@storybook/addon-a11y'),
-        getAbsolutePath('@storybook/addon-docs'),
-        getAbsolutePath('@storybook/addon-styling-webpack'),
-    ],
-    framework: getAbsolutePath('@storybook/sveltekit'),
-    staticDirs: ['../static'],
-    viteFinal: async (config: UserConfig) => {
-        if (config.server && config.server.fs) {
-            config.server.fs.allow = [
-                searchForWorkspaceRoot(process.cwd()),
-            ];
-        }
-        return config;
-    },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+  addons: [
+    '@storybook/addon-svelte-csf',
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+  ],
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {},
+  },
+  staticDirs: ['../static'],
+  viteFinal: async (config: UserConfig) => {
+    if (config.server?.fs) {
+      config.server.fs.allow = [searchForWorkspaceRoot(process.cwd())];
+    }
+    return config;
+  },
 };
 
 export default config;

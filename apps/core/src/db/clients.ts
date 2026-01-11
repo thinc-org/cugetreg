5df3ae214eca25e0c9661c1271156c7e32e1694d
TeriyakiThames <chiratt.thames@gmail.com>
2026-01-11 16:13:21 +0700

    Chore: Removed old apps/core folder

--- apps/core/src/db/clients.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "../generated/prisma/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-export const prisma = new PrismaClient({ adapter });
--- apps/core/src/routes/admin.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { prisma } from "../db/clients.js";
-
-const admin = new Hono()
-
-export default admin

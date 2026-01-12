bd1bca85b704b8edec07b49955f47811b2121a77
TeriyakiThames <chiratt.thames@gmail.com>
2026-01-12 18:35:12 +0700

    Chore: Move backend to apps/core and frontend to apps/cugetreg

---
+++ apps/cugetreg/.storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/sveltekit'
+import type { UserConfig } from 'vite'
+import { searchForWorkspaceRoot } from 'vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+  addons: [
+    '@storybook/addon-svelte-csf',
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+  ],
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {},
+  },
+  staticDirs: ['../static'],
+  viteFinal: async (config: UserConfig) => {
+    if (config.server?.fs) {
+      config.server.fs.allow = [searchForWorkspaceRoot(process.cwd())]
+    }
+    return config
+  },
+}
+
+export default config

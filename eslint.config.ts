41aa33a49572651b74d06a9539cfeb9ade73c223
Coded5 <phoenixanimation24@gmail.com>
2026-01-13 22:30:07 +0700

    chore: update package

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,1 +1,4 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 export { default } from "@cugetreg/configs/linter";

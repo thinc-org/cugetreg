b5f096b9caf43ee248b4091c7a1a5f874555e259
Coded5 <phoenixanimation24@gmail.com>
2026-01-19 22:13:22 +0700

    feat: update eslint configuration

--- packages/configs/eslint.ts
+++ packages/configs/eslint.ts
@@ -1,26 +1,25 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    {
-        rules: {
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "warn"
-        }
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
     },
-    {
-        ignores: [
-            '**/build',
-            '**/.svelte-kit',
-            '**/package',
-            '**/storybook-static',
-            '**/dist',
-        ],
-    }
+  },
+  {
+    ignores: [
+      "**/build",
+      "**/.svelte-kit",
+      "**/package",
+      "**/storybook-static",
+      "**/dist",
+      "**/.storybook",
+    ],
+  },
 );
-

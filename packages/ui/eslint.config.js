40dcae846af0c81ceb0ec85ec243488d1ef72edd
Coded5 <phoenixanimation24@gmail.com>
2026-01-23 21:15:16 +0700

    chore: remove unused import

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,7 +1,6 @@
 import prettier from 'eslint-config-prettier';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';

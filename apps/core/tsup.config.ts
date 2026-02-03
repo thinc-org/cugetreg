7df3bc77ad8c57aed3b788359a8b5c60933e1568
Coded5 <phoenixanimation24@gmail.com>
2026-02-03 19:57:00 +0700

    fix: formatting

--- apps/core/tsup.config.ts
+++ apps/core/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ["src/index.ts"],
   format: "esm",
   splitting: false,
   sourcemap: false,
   clean: true,
-  dts: true
+  dts: true,
 });

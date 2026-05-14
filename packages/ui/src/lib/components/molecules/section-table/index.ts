b6514ff77535d6f4759cd6d0aaf56f9b82982ccb
Coded5 <phoenixanimation24@gmail.com>
2026-05-14 17:26:13 +0700

    feat: update section-table making each section could have multiple class

--- packages/ui/src/lib/components/molecules/section-table/index.ts
+++ packages/ui/src/lib/components/molecules/section-table/index.ts
@@ -1,1 +1,14 @@
 export { default as SectionTable } from './section-table.svelte';
+
+export interface SectionTableData {
+	section: string;
+	seats: string;
+	classes: ClassInfo[];
+}
+
+export interface ClassInfo {
+	teacher: string;
+	schedule: string;
+	room: string;
+	type: string;
+}

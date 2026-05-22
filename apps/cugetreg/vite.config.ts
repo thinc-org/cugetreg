d7c9084c1d4523f5be0b4bd2131d73a19826b944
Coded5 <phoenixanimation24@gmail.com>
2026-05-22 10:54:10 +0700

    chore: add tailwind/typography and svelte-markdown

--- apps/cugetreg/vite.config.ts
+++ apps/cugetreg/vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
-}
+  assetsInclude: ['**/*.md'],
+})

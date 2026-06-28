40e311342b72114f415ca220422a382229d5dbbc
Narongwas Benjalux <darkwar008@gmail.com>
2026-06-28 18:25:58 +0700

    fix: lint

--- packages/zod-schemas/courses.schema.ts
+++ packages/zod-schemas/courses.schema.ts
@@ -37,7 +37,7 @@ export const CourseSchema = z.object({
   academicYear: z.number().int(),
   semester: z.string(),
   courseNo: z.string(),
-  courseCondition: z.string().nullish().default('-'),
+  courseCondition: z.string().nullish().default("-"),
   genEdType: z.string(),
   midtermStart: z.string().datetime().nullable(),
   midtermEnd: z.string().datetime().nullable(),

0cab6cb881595ce2a7341db7344759cb3418f7cc
Narongwas Benjalux <darkwar008@gmail.com>
2026-07-03 02:29:48 +0700

    feat: schedule-list can scroll if has many items

--- packages/ui/src/lib/components/organisms/schedule-list/schedule-list.svelte
+++ packages/ui/src/lib/components/organisms/schedule-list/schedule-list.svelte
@@ -127,7 +127,9 @@
 			</p>
 		</div>
 	{/if}
-	<div class="mt-4 flex flex-col gap-4">
+	<div
+		class={`mt-4 flex flex-col gap-4 ${filteredItems.length > 4 ? 'lg:max-h-[calc(100vh-100px)] max-h-144 overflow-y-auto sm:max-h-[calc(100vh-530px)]' : ''}`}
+	>
 		{#if showSpinner}
 			<div class="flex flex-col items-center justify-center gap-3 py-10">
 				<LoaderCircle size={80} class="animate-spin text-blue-500" />
@@ -171,12 +173,12 @@
 				</div>
 			{/each}
 		{/if}
-		<button
-			type="button"
-			class="md:text-md inline-flex w-full cursor-pointer justify-center rounded-2xl bg-blue-100 px-8 py-2 text-center text-sm font-medium text-blue-900 hover:bg-blue-200"
-			onclick={() => onClickButton?.()}
-		>
-			เพิ่มตารางเรียน
-		</button>
 	</div>
+	<button
+		type="button"
+		class="md:text-md mt-4 inline-flex w-full cursor-pointer justify-center rounded-2xl bg-blue-100 px-8 py-2 text-center text-sm font-medium text-blue-900 hover:bg-blue-200"
+		onclick={() => onClickButton?.()}
+	>
+		เพิ่มตารางเรียน
+	</button>
 </div>

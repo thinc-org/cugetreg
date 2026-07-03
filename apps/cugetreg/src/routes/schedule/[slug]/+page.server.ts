7caa8929329594e5d567d4d863a704d211ccf2d9
Coded5 <phoenixanimation24@gmail.com>
2026-07-03 11:32:55 +0700

    feat: public schedule page

--- apps/cugetreg/src/routes/schedule/[slug]/+page.server.ts
+++ apps/cugetreg/src/routes/schedule/[slug]/+page.server.ts
@@ -4,26 +4,14 @@ import { tryCatch } from '$lib/async-handler';
 
 import { error as svelteError } from '@sveltejs/kit';
 
-import type { SemesterType } from '@cugetreg/utils/types';
 import { PublicCartDetailResponseSchema } from '@cugetreg/zod-schemas/public-carts-response';
 
 import type { PageServerLoad } from './$types';
 
-const toSemesterType = (studyProgram: string): SemesterType => {
-  switch (studyProgram) {
-    case 'S':
-      return 'Semester';
-    case 'I':
-      return 'Inter';
-    case 'T':
-      return 'Trimester';
-    default:
-      return 'Semester';
-  }
-};
-
 export const load: PageServerLoad = async ({ params, fetch }) => {
-  const API_BASE = privateEnv.API_URL ? `${privateEnv.API_URL}/api/v1` : env.PUBLIC_API_URL;
+  const API_BASE = privateEnv.API_URL
+    ? `${privateEnv.API_URL}/api/v1`
+    : env.PUBLIC_API_URL;
   const API_URL = `${API_BASE}/public/carts/`;
   const cartId = params.slug;
 
@@ -35,8 +23,8 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
 
   const resData = await response.json();
   const data = PublicCartDetailResponseSchema.parse(resData).data;
+
   return {
-    data: data,
-    semesterType: toSemesterType(data.cart.studyProgram),
+    data,
   };
 };

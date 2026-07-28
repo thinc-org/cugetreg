e06c0d327f51791d921f79ef38311ec76503d627
Coded5 <phoenixanimation24@gmail.com>
2026-07-28 13:21:42 +0700

    feat(util): add faculty data

---
+++ packages/utils/faculty.ts
@@ -0,0 +1,206 @@
+export const FACULTIES = {
+  "01": {
+    en: "THE SIRINDHORN THAI LANGUAGE INSTITUTE",
+    en_short: "Sirindhorn Thai Language",
+    th: "สถาบันภาษาไทยสิรินธร",
+    th_short: "ภาษาไทยสิรินธร",
+  },
+  "02": {
+    en: "OFFICE OF ACADEMIC AFFAIRS",
+    en_short: "Academic Affairs",
+    th: "ศูนย์การศึกษาทั่วไป",
+    th_short: "ศึกษาทั่วไป",
+  },
+  "20": {
+    en: "COLLEGE OF INTERDISCIPLINARY AND INTEGRATIVE STUDIES CHULALONGKORN UNIVERSITY",
+    en_short: "Interdisciplinary and Integrative Studies",
+    th: "วิทยาลัยสหศาสตร์บูรณาการแห่งจุฬาลงกรณ์มหาวิทยาลัย",
+    th_short: "สหศาสตร์",
+  },
+  "21": {
+    en: "FACULTY OF ENGINEERING",
+    en_short: "Engineering",
+    th: "คณะวิศวกรรมศาสตร์",
+    th_short: "วิศวะ",
+  },
+  "22": {
+    en: "FACULTY OF ARTS",
+    en_short: "Arts",
+    th: "คณะอักษรศาสตร์",
+    th_short: "อักษร",
+  },
+  "23": {
+    en: "FACULTY OF SCIENCE",
+    en_short: "Science",
+    th: "คณะวิทยาศาสตร์",
+    th_short: "วิทยา",
+  },
+  "24": {
+    en: "FACULTY OF POLITICAL SCIENCE",
+    en_short: "Political Science",
+    th: "คณะรัฐศาสตร์",
+    th_short: "รัฐศาสตร์",
+  },
+  "25": {
+    en: "FACULTY OF ARCHITECTURE",
+    en_short: "Architecture",
+    th: "คณะสถาปัตยกรรมศาสตร์",
+    th_short: "สถาปัตย์",
+  },
+  "26": {
+    en: "FACULTY OF COMMERCE AND ACCOUNTANCY",
+    en_short: "Commerce and Accountancy",
+    th: "คณะพาณิชยศาสตร์และการบัญชี",
+    th_short: "บัญชี",
+  },
+  "27": {
+    en: "FACULTY OF EDUCATION",
+    en_short: "Education",
+    th: "คณะครุศาสตร์",
+    th_short: "ครุ",
+  },
+  "28": {
+    en: "FACULTY OF COMMUNICATION ARTS",
+    en_short: "Communication Arts",
+    th: "คณะนิเทศศาสตร์",
+    th_short: "นิเทศ",
+  },
+  "29": {
+    en: "FACULTY OF ECONOMICS",
+    en_short: "Economics",
+    th: "คณะเศรษฐศาสตร์",
+    th_short: "เศรษฐศาสตร์",
+  },
+  "30": {
+    en: "FACULTY OF MEDICINE",
+    en_short: "Medicine",
+    th: "คณะแพทยศาสตร์",
+    th_short: "แพทย์",
+  },
+  "31": {
+    en: "FACULTY OF VETERINARY SCIENCE",
+    en_short: "Veterinary Science",
+    th: "คณะสัตวแพทยศาสตร์",
+    th_short: "สัตวแพทย์",
+  },
+  "32": {
+    en: "FACULTY OF DENTISTRY",
+    en_short: "Dentistry",
+    th: "คณะทันตแพทยศาสตร์",
+    th_short: "ทันตะ",
+  },
+  "33": {
+    en: "FACULTY OF PHARMACEUTICAL SCIENCES",
+    en_short: "Pharmaceutical Sciences",
+    th: "คณะเภสัชศาสตร์",
+    th_short: "เภสัช",
+  },
+  "34": {
+    en: "FACULTY OF LAW",
+    en_short: "Law",
+    th: "คณะนิติศาสตร์",
+    th_short: "นิติ",
+  },
+  "35": {
+    en: "FACULTY OF FINE AND APPLIED ARTS",
+    en_short: "Fine and Applied Arts",
+    th: "คณะศิลปกรรมศาสตร์",
+    th_short: "ศิลปกรรม",
+  },
+  "36": {
+    en: "FACULTY OF NURSING",
+    en_short: "Nursing",
+    th: "คณะพยาบาลศาสตร์",
+    th_short: "พยาบาล",
+  },
+  "37": {
+    en: "FACULTY OF ALLIED HEALTH SCIENCES",
+    en_short: "Allied Health Sciences",
+    th: "คณะสหเวชศาสตร์",
+    th_short: "สหเวช",
+  },
+  "38": {
+    en: "FACULTY OF PSYCHOLOGY",
+    en_short: "Psychology",
+    th: "คณะจิตวิทยา",
+    th_short: "จิตวิทยา",
+  },
+  "39": {
+    en: "FACULTY OF SPORTS SCIENCE",
+    en_short: "Sports Science",
+    th: "คณะวิทยาศาสตร์การกีฬา",
+    th_short: "วิทย์กีฬา",
+  },
+  "40": {
+    en: "FACULTY OF INTEGRATED AGRICULTURE",
+    en_short: "Integrated Agriculture",
+    th: "คณะเกษตรศาสตร์บูรณาการ",
+    th_short: "เกษตรบูรณาการ",
+  },
+  "49": {
+    en: "COLLEGE OF EXTENSION AND LIFELONG LEARNING FOR THE PEOPLE CHULALONGKORN UNIVERSITY",
+    en_short: "Lifelong Learning",
+    th: "วิทยาลัยส่งเสริมการเรียนรู้ตลอดชีวิตเพื่อประชาชนแห่งจุฬาลงกรณ์มหาวิทยาลัย",
+    th_short: "การเรียนรู้ตลอดชีวิต",
+  },
+  "51": {
+    en: "COLLEGE OF POPULATION STUDIES",
+    en_short: "Population Studies",
+    th: "วิทยาลัยประชากรศาสตร์",
+    th_short: "ประชากรศาสตร์",
+  },
+  "53": {
+    en: "COLLEGE OF PUBLIC HEALTH SCIENCES",
+    en_short: "Public Health Sciences",
+    th: "วิทยาลัยวิทยาศาสตร์สาธารณสุข",
+    th_short: "สาธารณสุข",
+  },
+  "55": {
+    en: "LANGUAGE INSTITUTE",
+    en_short: "Language",
+    th: "สถาบันภาษา",
+    th_short: "ภาษา",
+  },
+  "56": {
+    en: "CHULALONGKORN SCHOOL OF INTEGRATED INNOVATION",
+    en_short: "Integrated Innovation",
+    th: "สถาบันนวัตกรรมบูรณาการแห่งจุฬาลงกรณ์มหาวิทยาลัย",
+    th_short: "นวัตกรรมบูรณาการ",
+  },
+  "58": {
+    en: "SASIN GRADUATE INSTITUTE OF BUSINESS ADMINISTION",
+    en_short: "Sasin",
+    th: "สถาบันบัณฑิตบริหารธุรกิจ ศศินทร์ฯ",
+    th_short: "ศศินทร์",
+  },
+  "63": {
+    en: "THE PETROLEUM AND PETROCHEMICAL COLLEGE",
+    en_short: "Petroleum and Petrochemical",
+    th: "วิทยาลัยปิโตรเลียมและปิโตรเคมี",
+    th_short: "ปิโตรเลียมและปิโตรเคมี",
+  },
+  "92": {
+    en: "POLICE NURSING COLLEGE",
+    en_short: "Police Nursing",
+    th: "วิทยาลัยพยาบาลตำรวจ",
+    th_short: "พยาบาลตำรวจ",
+  },
+  "99": {
+    en: "OTHER UNIVERSITY",
+    en_short: "Other University",
+    th: "มหาวิทยาลัยอื่น",
+    th_short: "มหาวิทยาลัยอื่น",
+  },
+} satisfies Record<
+  string,
+  { en: string; en_short: string; th: string; th_short: string }
+>;
+
+export const UNKOWN_FACULTY = {
+  en: "UNKOWN",
+  en_short: "UNKOWN",
+  th: "ไม่ทราบ",
+  th_short: "ไม่ทราบ",
+};
+
+export type FacultyId = keyof typeof FACULTIES;
